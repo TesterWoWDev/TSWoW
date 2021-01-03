48efce4b030546e89f4bbcf8c27f85097eae3a44
Connor <connorkilbourne@gmail.com>
2021-01-03 10:10:29 -0500

    finally working arrays+kill counter

--- modules/tester-mod/scripts/tester_mod_scripts.ts
+++ modules/tester-mod/scripts/tester_mod_scripts.ts
@@ -1,13 +1,44 @@
+import { kill } from "process";
 import { ID } from "./ID";
+
+class TestClass  {
+	guid: uint32 = 0
+	kills: int16 = 1
+}
+class TestContainer {
+    inner: TestClass = new TestClass();
+}
+
+let arr : TSArray<TestContainer> = [];
 
 export function Main(events: TSEventHandlers) {
 //kill tracker/reward system
 	events.Player.OnPVPKill((killer,killed)=>{
-		const PVPTOKEN = 20880
+		let PVPTOKEN = 20880
 		let AMOUNT = 5
 		killer.AddItem(PVPTOKEN,AMOUNT)
-		 killer.SendBroadcastMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood.")
-	});
+		let foundIndex = -1
+		console.log(arr.length)
+		for(let i=0;i<arr.length;i++){
+			console.log(arr.get(i).inner.guid)
+			console.log(killer.GetGUIDLow())
+			if(arr.get(i).inner.guid == killer.GetGUIDLow()){
+				foundIndex = i
+				arr.get(i).inner.kills++
+			}
+			if(arr.get(i).inner.guid == killed.GetGUIDLow()){
+				foundIndex = i
+				arr.get(i).inner.kills = 0
+			}
+		}
+		if(foundIndex == -1){
+			let holder = new TestContainer();
+			holder.inner.guid = killer.GetGUIDLow()
+			arr.push(holder)
+			
+		}
+		 killer.SendBroadcastMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+arr.get(foundIndex).inner.kills)
+		});
 
 //reset cds on duel
 	events.Player.OnDuelStart((player1,player2)=>{
