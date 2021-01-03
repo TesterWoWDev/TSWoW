32dcb9393f2f1efc4afb2e846d09fd9b7a7c4117
Connor <connorkilbourne@gmail.com>
2021-01-02 20:47:56 -0500

    minions

--- modules/tester-mod/data/Quest_Tests/Quest1.ts
+++ modules/tester-mod/data/Quest_Tests/Quest1.ts
@@ -5,19 +5,19 @@ import { ZOMBIE } from "../EnemyCreature";
 const killQuest = std.Quests.create('my-module', 'kill-zombies-quest')
 
 // Set the quest log title
-.Text.Title.set({ enGB: 'Kill the zombies' })
+killQuest.Text.Title.set({ enGB: 'Kill the zombies' })
 
 // Set the longer quest description 
-.Text.Description.set({ enGB: 'I sure have absolutely no idea where those zombies came from, but they are beginning to become a little more than an annoyance at this point. Please clear them out for us, will you?' })
+killQuest.Text.Description.set({ enGB: 'I sure have absolutely no idea where those zombies came from, but they are beginning to become a little more than an annoyance at this point. Please clear them out for us, will you?' })
 
 // Set the short objective description (shown above the concrete objectives)
-.Text.Objective.set({ enGB: 'Kill ten zombies, then report back to Dane Winslow.' })
+killQuest.Text.Objective.set({ enGB: 'Kill ten zombies, then report back to Dane Winslow.' })
 
 // Makes Dane Winslow both starter and ender of the quest
-.Questgiver.addBoth(6373)
+killQuest.Questgiver.addBoth(6373)
 
 // Adds 10 zombies as the single objective of this quest
 // If a creature is used for this, it's presumed you should kill them, 
 // whereas for gameobjects it's presumed you should just activate them.
-.Objectives.Entity.add(ZOMBIE.ID, 10)
-.Rewards.money.set(10000)
+killQuest.Objectives.Entity.add(ZOMBIE.ID, 10)
+killQuest.Rewards.money.set(10000)
