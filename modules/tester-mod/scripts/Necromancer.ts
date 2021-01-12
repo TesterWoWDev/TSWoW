6ccc8a10b4a665c326bbe0aeaba4ed400ecd8331
Connor <connorkilbourne@gmail.com>
2021-01-12 10:59:11 -0500

    updated the spell creature a bit

--- modules/tester-mod/scripts/Necromancer.ts
+++ modules/tester-mod/scripts/Necromancer.ts
@@ -15,12 +15,9 @@ export function NecromancerClass(events: TSEventHandlers) {
 	
 	//test build out for summon creature that goes forward 20 yards
 	events.SpellID.OnCast(53307,(spell)=>{
-		const distance = 20
-		const creatureID = ID.TESTER_MOD_SKILLSHOT_CREATURE
-		const despawnTime = 3000
 		const player = spell.GetCaster().ToPlayer()
-		const TSPosition = player.GetRelativePoint(distance,0)
-		const creature = player.SpawnCreature(creatureID,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),3,despawnTime)
+		const TSPosition = player.GetRelativePoint(20,0)
+		const creature = player.SpawnCreature(ID.TESTER_MOD_SKILLSHOT_CREATURE,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),3,3000)
 		creature.CastSpell(creature,ID.TESTER_MOD_SKILLSHOT_CAST,true)
 		creature.MoveTo(1,TSPosition.x,TSPosition.y,TSPosition.z,true)
 	});
