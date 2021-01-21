fcba645e97802c1fef3be72c2a82c5572b7b2913
Connor <connorkilbourne@gmail.com>
2021-01-21 14:48:39 -0500

    more death script modification

--- modules/tester-mod/scripts/death.ts
+++ modules/tester-mod/scripts/death.ts
@@ -1,16 +1,17 @@
 export function onDeath(events: TSEventHandlers) {
     events.Player.OnPlayerKilledByCreature((creature,player)=>{
         const chestID = 75299
-        const despawnTime = 60//seconds
+        const despawnTime = 60//seconds till despawn
         const chest = player.SummonGameObject(chestID,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),despawnTime)
-        let item = player.GetItemByPos(0,0)
-        let itemEntry = item.GetEntry()
-        let itemCount = item.GetCount()
+        let item = new TSItem()
+        let itemEntry = 0
+        let itemCount = 0
         chest.GetLoot().Clear()//maybe?
-        chest.GetLoot().AddLooter(player.GetGUIDLow())
-        chest.GetLoot().SetMoney(player.GetMoney())
-        player.ModifyMoney(-999999999)//idk
-        for (let x=19;x<=22;x++){//equipped bags inside slots
+        chest.GetLoot().AddLooter(player.GetGUIDLow())//prolly work?
+        const charmoney = player.GetMoney()
+        chest.GetLoot().SetMoney(charmoney)
+        player.ModifyMoney(-1*charmoney)
+        for (let x=19;x<=22;x++){//items inside inventory bags
             for (let i = 0; i <= 35; ++i){
                 item = player.GetItemByPos(x,i)
                 itemEntry = item.GetEntry()
@@ -19,7 +20,7 @@ export function onDeath(events: TSEventHandlers) {
                 player.RemoveItem(item,itemCount,itemEntry)
             }
         }
-        for (let x=67;x<=74;x++){//equipped bags inside bank slots
+        for (let x=67;x<=74;x++){//items inside bank bags
             for (let i = 0; i <= 35; ++i){
                 item = player.GetItemByPos(x,i)
                 itemEntry = item.GetEntry()
@@ -28,7 +29,7 @@ export function onDeath(events: TSEventHandlers) {
                 player.RemoveItem(item,itemCount,itemEntry)
             }
         }
-        for(let i=0;i<=118;i++){//equip/equip bags/backpack/bank main/bank bags/keyring
+        for(let i=0;i<=118;i++){//equipment/inventory equipped bags/backpack(base 16)/bank main area/bank equipped bags/keyring
             item = player.GetItemByPos(255,i)
             itemEntry = item.GetEntry()
             itemCount = item.GetCount()
@@ -38,40 +39,6 @@ export function onDeath(events: TSEventHandlers) {
     });
 
     events.Player.OnPVPKill((killer,player)=>{
-        const chestID = 75299
-        const despawnTime = 60//seconds
-        const chest = player.SummonGameObject(chestID,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),despawnTime)
-        let item = player.GetItemByPos(0,0)
-        let itemEntry = item.GetEntry()
-        let itemCount = item.GetCount()
-        chest.GetLoot().Clear()//maybe?
-        chest.GetLoot().AddLooter(player.GetGUIDLow())
-        chest.GetLoot().SetMoney(player.GetMoney())
-        player.ModifyMoney(-999999999)//idk
-        for (let x=19;x<=22;x++){//equipped bags inside slots
-            for (let i = 0; i <= 35; ++i){
-                item = player.GetItemByPos(x,i)
-                itemEntry = item.GetEntry()
-                itemCount = item.GetCount()
-                chest.GetLoot().AddItem(CreateLootItem(itemEntry,0,100,0,false,0,itemCount,itemCount))
-                player.RemoveItem(item,itemCount,itemEntry)
-            }
-        }
-        for (let x=67;x<=74;x++){//equipped bags inside bank slots
-            for (let i = 0; i <= 35; ++i){
-                item = player.GetItemByPos(x,i)
-                itemEntry = item.GetEntry()
-                itemCount = item.GetCount()
-                chest.GetLoot().AddItem(CreateLootItem(itemEntry,0,100,0,false,0,itemCount,itemCount))
-                player.RemoveItem(item,itemCount,itemEntry)
-            }
-        }
-        for(let i=0;i<=118;i++){//equip/equip bags/backpack/bank main/bank bags/keyring
-            item = player.GetItemByPos(255,i)
-            itemEntry = item.GetEntry()
-            itemCount = item.GetCount()
-            chest.GetLoot().AddItem(CreateLootItem(itemEntry,0,100,0,false,0,itemCount,itemCount))
-            player.RemoveItem(item,itemCount,itemEntry)
-        }
+        //copy the above code here?
     });
 }
