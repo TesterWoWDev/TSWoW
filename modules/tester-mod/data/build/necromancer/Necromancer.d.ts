b84d7a83309e29c750880020ce85bea7dcc8f2e7
Connor Kilbourne <connorkilbourne@gmail.com>
2021-01-26 20:32:12 -0500

    fix for data build folder being removed

--- modules/tester-mod/data/build/necromancer/Necromancer.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const NECROMANCER_CLASS: import("tswow-stdlib/Class/Class").Class;
-export declare const NECROMANCER_ROBE: (value: string[]) => import("wotlkdata/dbc/types/ItemDisplayInfo").ItemDisplayInfoRow;
-export declare const NECROMANCY_SKILL: import("tswow-stdlib/SkillLines/SkillLine").SkillLine;
-export declare const PESTILENCE_SKILL: import("tswow-stdlib/SkillLines/SkillLine").SkillLine;
--- modules/tester-mod/data/create-db-tables.ts
+++
@@ -1,3 +0,0 @@
-import { SQL } from 'wotlkdata';
-
-SQL.Databases.auth.write('CREATE TABLE IF NOT EXISTS `vip_list` (`accID` int(11) NOT NULL,`multiplier` float(0) NOT NULL,PRIMARY KEY (`accID`))');
