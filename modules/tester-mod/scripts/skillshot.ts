adc95506cbdf7337b9b2a5f0c96223cc4172097a
Connor Kilbourne <connorkilbourne@gmail.com>
2021-01-27 19:27:26 -0500

    added new display to creature

--- modules/tester-mod/scripts/skillshot.ts
+++ modules/tester-mod/scripts/skillshot.ts
@@ -8,6 +8,8 @@ export function Skillshot(events: TSEventHandlers) {
 		const creature = player.SpawnCreature(ID.TESTER_MOD_SKILLSHOT_CREATURE,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),3,3000)
 		creature.SetLevel(player.GetLevel())
 		creature.CastSpell(creature,ID.TESTER_MOD_SKILLSHOT_CAST,true)
+		creature.CastSpell(creature,51019,true)
 		creature.MoveTo(1,TSPosition.x,TSPosition.y,TSPosition.z,true)
+		creature.SetDisplayId(11686)
 	});
 }
