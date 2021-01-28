855aaad9b910f7c5cc433b4b193a5e39f2cd2655
Connor <connorkilbourne@gmail.com>
2021-01-28 11:11:26 -0500

    rename

--- modules/tester-mod/scripts/tester_mod_scripts.ts
+++ modules/tester-mod/scripts/tester_mod_scripts.ts
@@ -1,15 +1,15 @@
-import { onDeath } from "./death";
-import { NecromancerClass } from "./Necromancer";
-import { PvP } from "./PvP";
-import { QoLChanges } from "./QoLChanges";
-import { Skillshot } from "./skillshot";
-import { VIP } from "./VIP"
+// import { onDeath } from "./death";
+// import { NecromancerClass } from "./Necromancer";
+// import { PvP } from "./PvP";
+// import { QoLChanges } from "./QoLChanges";
+// import { Skillshot } from "./skillshot";
+// import { VIP } from "./VIP"
 
 export function Main(events: TSEventHandlers) {
-	VIP(events)
-	QoLChanges(events)
-	NecromancerClass(events)
-	PvP(events)
-	Skillshot(events)
-	onDeath(events)
+	// VIP(events)
+	// QoLChanges(events)
+	// NecromancerClass(events)
+	// PvP(events)
+	// Skillshot(events)
+	// onDeath(events)
 }
