0a5ae91a97fca9d56fd4c1504218c5ebe39a8686
Connor Kilbourne <connorkilbourne@gmail.com>
2021-01-27 20:24:01 -0500

    rework dante's module to compile

--- modules/dante-mod/data/dante-mod-data.ts
+++ modules/dante-mod/data/dante-mod-data.ts
@@ -1,10 +1,4 @@
 
 import { std } from "tswow-stdlib";
-//This is a test comment to show that I can create notes to give information.
-const WARRIOR_CLASS = std.Classes.load(1)
-WARRIOR_CLASS.Name.enGB.set('Adventurer');
-WARRIOR_CLASS.UI.Description.set('Adventurers adventure the world!');
-WARRIOR_CLASS.Stats.MeleeAttackPower.set("level*1337");
-console.log(WARRIOR_CLASS.Stats.objectify())
-WARRIOR_CLASS.Stats.RangedAttackPower.set('200')
-WARRIOR_CLASS.Name.enGB.set('Warrior')
+
+console.log("Hello from dante-mod data script!");
