5d084ca31dc566bd04a8437fef390b86f7bd7b51
Connor <connorkilbourne@gmail.com>
2021-01-29 18:25:57 -0500

    added quest2

--- modules/tester-mod/data/quests/Starting-Quests.ts
+++ modules/tester-mod/data/quests/Starting-Quests.ts
@@ -12,3 +12,14 @@ startQuest1.Rewards.ChoiceItem.add(1,20)//health pot
 startQuest1.Rewards.ChoiceItem.add(1,20)//mana pot
 startQuest1.Rewards.Item.add(1,1)//draft papers
 
+const startQuest2 = std.Quests.create("tester-mod", "startQuest2")
+startQuest2.QuestLevel.set(1)
+startQuest2.Text.Title.enGB.set("Call for Action")
+startQuest2.Text.Objective.enGB.set("fill me")
+startQuest2.Text.Description.enGB.set("The letter you obtained from Gregor Victus summons you to the Greenway Lodge where you are greeted by famine, disease, and strife amidst a suffering population. You enter the main building and speak to Archbishop Fairway about a mysterious plague that suddenly befell the people of Greenway.")
+startQuest2.Questgiver.addStarter(1)
+startQuest2.Questgiver.addEnder(1)
+startQuest2.Objectives.Item.add(1,1)//draft paper
+startQuest2.Rewards.Money.set(21000)
+startQuest2.Rewards.Item.add(1,1)//blank papers
+startQuest2.Rewards.Item.add(1,5)//empty vials
