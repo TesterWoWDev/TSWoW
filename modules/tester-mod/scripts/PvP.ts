2a40c51d7715ec8786d9e6258e41c5eb38752f1b
Connor <connorkilbourne@gmail.com>
2021-01-29 20:36:13 -0500

    rewrite killtracker with ORM system

--- modules/tester-mod/scripts/PvP.ts
+++ modules/tester-mod/scripts/PvP.ts
@@ -1,41 +1,40 @@
-class TestContainer {
-	guid: uint32 = 0
-	kills: int16 = 1
+const TABLE_NAME = "player_killstreak";
+
+@CharactersTable
+class PlayerKillstreak extends DBTable {
+    constructor(playerGUID: uint32) {
+        super();
+        this.playerGUID = playerGUID;
+	}
+	
+    @PrimaryKey
+	playerGUID: uint32 = 0;
+	
+    @Field
+    killCount: int32 = 0;
 }
 
-let arr : TSArray<TestContainer> = [];
+export function Killstreaks(events: TSEventHandlers) {
+    events.Player.OnLogin((player,first)=>{
+        const guid = player.GetGUIDLow()
+        const rows = LoadRows(PlayerKillstreak,`player = ${guid}`)
+        const tableData = rows.length > 0 ? rows.get(0) : new PlayerKillstreak(guid);
+        player.GetData().SetObject(ModID(),TABLE_NAME,tableData);
+    });
 
-export function PvP(events: TSEventHandlers) {
-//reset cds on duel
-		events.Player.OnDuelStart((player1,player2)=>{
-			player1.ResetAllCooldowns()
-			player2.ResetAllCooldowns()
-		});
-//kill tracker/reward
-		events.Player.OnPVPKill((killer,killed)=>{
-			let PVPTOKEN = 20880
-			let AMOUNT = 5
-			killer.AddItem(PVPTOKEN,AMOUNT)
-			let foundIndex = -1
-			let kills = 1
-			for(let i=0;i<arr.length;i++){
-				console.log(arr.get(i).guid)
-				console.log(killer.GetGUIDLow())
-				if(arr.get(i).guid == killer.GetGUIDLow()){
-					foundIndex = i
-					arr.get(i).kills++
-				}
-				if(arr.get(i).guid == killed.GetGUIDLow()){
-					arr.get(i).kills = 0
-				}
-			}
-			if(foundIndex > -1){
-				kills = arr.get(foundIndex).kills
-			}else{
-				let holder = new TestContainer();
-				holder.guid = killer.GetGUIDLow()
-				arr.push(holder)
-			}
-			killer.SendBroadcastMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+kills)
-		});
-	}
+	events.Player.OnPVPKill((killer,killed)=>{
+		const killCount = killer.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(killer.GetGUIDLow())).killCount++
+		killed.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(killed.GetGUIDLow())).killCount = 0
+		//replace with global message
+		killer.SendBroadcastMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+killCount)
+		killed.SendBroadcastMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+killCount)
+	});
+
+    events.Player.OnSave((player)=>{
+        player.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(player.GetGUIDLow())).save();
+	});
+
+	events.Player.OnLogout((player)=>{
+        player.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(player.GetGUIDLow())).save();
+    });
+}
