4295ef04d844efeee82a99a15b7dfcf6769b1c06
Connor <connorkilbourne@gmail.com>
2021-01-30 14:21:03 -0500

    added in-method saving and loading form database

--- modules/tester-mod/scripts/Killstreak.ts
+++ modules/tester-mod/scripts/Killstreak.ts
@@ -11,24 +11,19 @@ class PlayerKillstreak extends DBTable {
 	playerGUID: uint32 = 0;
 	
     @Field
-    killCount: int32 = 1;
+    killCount: int32 = 0;
 }
 
 export function Killstreaks(events: TSEventHandlers) {
-    events.Player.OnLogin((player,first)=>{
-        const guid = player.GetGUIDLow()
-        const rows = LoadRows(PlayerKillstreak,`playerGUID = ${guid}`)
-        const tableData = rows.length > 0 ? rows.get(0) : new PlayerKillstreak(guid);
-        player.GetData().SetObject(ModID(),TABLE_NAME,tableData);
-    });
-
 	events.Player.OnPVPKill((killer,killed)=>{
-		const killCount = killer.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(killer.GetGUIDLow())).killCount++
+		let killCount = killer.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>{
+            const guid = killer.GetGUIDLow()
+            const rows = LoadRows(PlayerKillstreak,`playerGUID = ${guid}`)
+            return rows.length > 0 ? rows.get(0) : new PlayerKillstreak(guid);
+        }).killCount++
 		killed.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(killed.GetGUIDLow())).killCount = 0
-        SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+killCount)
-	});
-
-    events.Player.OnSave((player)=>{
-        player.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(player.GetGUIDLow())).save();
-	});
+        SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+(killCount+1))
+        killer.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(killer.GetGUIDLow())).save();
+        killed.GetData().GetObject<PlayerKillstreak>(ModID(),TABLE_NAME,()=>new PlayerKillstreak(killed.GetGUIDLow())).save();
+    });
 }
