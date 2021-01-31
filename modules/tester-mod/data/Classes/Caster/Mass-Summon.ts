ec38822e6d19373b37cb2f585738f6c720e9630f
Connor <connorkilbourne@gmail.com>
2021-01-31 10:45:24 -0500

    added healthLink spell + script

--- modules/tester-mod/data/Classes/Caster/Mass-Summon.ts
+++ modules/tester-mod/data/Classes/Caster/Mass-Summon.ts
@@ -1,11 +1,11 @@
 import { std } from "tswow-stdlib";
 import {DEATH_SKILL} from "./Caster"
-export const DEATH_BOLT = std.Spells.create('tswow-introduction','death-bolt',686)
+export const DEATH_BOLT = std.Spells.create('tester-mod','death-bolt',686)
 DEATH_BOLT.Name.enGB.set('Death Bolt')
 DEATH_BOLT.Description.enGB.set('A bolt of death!');
 DEATH_BOLT.SkillLines.add(DEATH_SKILL.ID).setAutolearn();
 
-export const MINION_SUMMON = std.Spells.create('tswow-introduction','minion-summon',66842)
+export const MINION_SUMMON = std.Spells.create('tester-mod','minion-summon',66842)
 MINION_SUMMON.Name.enGB.set('Mass Reanimation')
 MINION_SUMMON.Description.enGB.set('Simultaneously summon up to 4 minions specified in the totem bar');
 MINION_SUMMON.Icon.set(std.Spells.load(12020).Icon.get())
--- modules/tester-mod/scripts/tester_mod_scripts.ts
+++ modules/tester-mod/scripts/tester_mod_scripts.ts
@@ -1,9 +1,10 @@
 import { onDeath } from "./death";
-import { MultiPetAttack } from "././MultiPetAttack";
+import { MultiPetAttack } from "./caster/MultiPetAttack";
 import { Killstreaks } from "./Killstreak";
 import { QoLChanges } from "./QoLChanges";
 import { Skillshot } from "./skillshot";
 import { VIP } from "./VIP"
+import { HealthLink } from "./caster/HealthLink";
 
 export function Main(events: TSEventHandlers) {
 	VIP(events)
@@ -12,4 +13,5 @@ export function Main(events: TSEventHandlers) {
 	Killstreaks(events)
 	Skillshot(events)
 	onDeath(events)
+	HealthLink(events)
 }
