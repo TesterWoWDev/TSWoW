40797923782e82e6e44e08a98e3013e1c39c274d
Connor <connorkilbourne@gmail.com>
2021-01-31 14:06:24 -0500

    rename flag to castFlag

--- modules/tester-mod/scripts/caster/DoubleCast.ts
+++ modules/tester-mod/scripts/caster/DoubleCast.ts
@@ -2,7 +2,7 @@ import { ID } from "../ID";
 const TABLE_NAME_DOUBLE = "doubleCast"
 class PlayerDoublecast {
 	playerGUID: uint32 = 0;
-    flag: int32 = 0;
+    castFlag: int32 = 0;
     spawnedGUID: int32 = 0;
 }
 
@@ -13,20 +13,20 @@ export function DoubelCast(events: TSEventHandlers) {
         const creatureID = 31746
         const mineDespawn = 10000
         const caster = spell.GetCaster().ToPlayer()
-        let flag = caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE,()=>new PlayerDoublecast()).flag
+        let flag = caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE,()=>new PlayerDoublecast()).castFlag
         if(flag == 0){
-            caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).flag = 1
+            caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).castFlag = 1
             const TSPosition = caster.GetRelativePoint(20,0)
             const creature = caster.SpawnCreature(creatureID,caster.GetX(),caster.GetY(),caster.GetZ(),caster.GetO(),3,mineDespawn)
             creature.MoveTo(1,TSPosition.x,TSPosition.y,TSPosition.z,true)
             caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).spawnedGUID = creature.GetGUIDLow()
 
             caster.GetTasks().AddTimer(ModID(),"reset_doublecast",mineDespawn,1,(timer,entity,del,can)=>{
-                entity.ToPlayer().GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).flag = 0
+                entity.ToPlayer().GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).castFlag = 0
             });
         }
         else{
-            caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).flag = 0
+            caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).castFlag = 0
             const creatureGUID = caster.GetData().GetObject<PlayerDoublecast>(ModID(),TABLE_NAME_DOUBLE).spawnedGUID
             const spawned = caster.GetCreaturesInRange(100,creatureID,0,0)
             for(let i=0;i<spawned.length;i++){
