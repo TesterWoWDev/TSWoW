814da4c963cde56b896b104ed8476d4f415b31a7
Connor <connorkilbourne@gmail.com>
2021-01-31 10:58:05 -0500

    helath link+health swap spells fixed up

--- modules/tester-mod/scripts/caster/HealthLink.ts
+++ modules/tester-mod/scripts/caster/HealthLink.ts
@@ -5,8 +5,8 @@ export function HealthLink(events: TSEventHandlers) {
             const caster = spell.GetCaster().ToUnit()
             const target = spell.GetTarget().ToUnit()
             const averagePCT = (caster.GetHealthPct()+target.GetHealthPct())/200//200 because its 2*100, 2 for average, 100 to turn 100% into multiplier of 1
+            const tarHP = target.GetMaxHealth()*averagePCT
             caster.SetHealth(caster.GetMaxHealth()*averagePCT)
-            target.SetHealth(target.GetMaxHealth()*averagePCT)
-     
+            target.SetHealth(tarHP)
         })
     }
