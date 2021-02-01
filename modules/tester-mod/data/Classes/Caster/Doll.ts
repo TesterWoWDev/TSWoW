592e72429da421377c72ba1d0a0dc71744d9f1ee
Connor <connorkilbourne@gmail.com>
2021-01-31 22:09:32 -0500

    fix module name on doll

--- modules/tester-mod/data/Classes/Caster/Doll.ts
+++ modules/tester-mod/data/Classes/Caster/Doll.ts
@@ -1,7 +1,7 @@
 import { std } from "tswow-stdlib";
 import { CASTER_CLASS } from "./Caster";
 
-export const CASTER_TOTEM = std.Items.create('my-mod', 'caster-totem-item',46978)
+export const CASTER_TOTEM = std.Items.create('tester-mod', 'caster-totem-item',46978)
 CASTER_TOTEM.Name.set({enGB:"Caster's Doll"})
 CASTER_TOTEM.Description.set({enGB:"The doll required to conjure any of a Caster's minions"})
 CASTER_TOTEM.DisplayID.set(std.Items.load(19816).DisplayID.get())
