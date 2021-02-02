5728893bb49e0e87a8ed34d91060d3b5dc99d0dd
Connor <connorkilbourne@gmail.com>
2021-02-01 21:23:48 -0500

    refactor

--- modules/classes-mod/data/Classes/Caster/FireSpells.ts
+++ modules/classes-mod/data/Classes/Caster/FireSpells.ts
@@ -1,10 +1,8 @@
 import { std } from "tswow-stdlib";
 import { FIRE_SKILL } from "./Caster";
 
-export const fire1 = std.Spells.create('classes-mod','fireball-rank1',133)
-fire1.SkillLines.add(FIRE_SKILL.ID).setAutolearn();
-export const fire2 = std.Spells.create('classes-mod','fireball-rank2',143)
-fire2.SkillLines.add(FIRE_SKILL.ID).AcquireMethod.set(0);
+export const fire1 = std.Spells.create('classes-mod','fireball-rank1',133).SkillLines.add(FIRE_SKILL.ID).setAutolearn().end;
+export const fire2 = std.Spells.create('classes-mod','fireball-rank2',143).SkillLines.add(FIRE_SKILL.ID).AcquireMethod.set(0).end;
 std.Spells.create('classes-mod','fireball-rank3',145).SkillLines.add(FIRE_SKILL.ID).AcquireMethod.set(0);
 std.Spells.create('classes-mod','fireball-rank4',3140).SkillLines.add(FIRE_SKILL.ID).AcquireMethod.set(0);
 std.Spells.create('classes-mod','fireball-rank5',8400).SkillLines.add(FIRE_SKILL.ID).AcquireMethod.set(0);
