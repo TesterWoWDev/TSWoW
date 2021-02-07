23c63ba0e9880dcfd7c5977a58d4ac7e4336862e
Connor K <connorkilbourne@gmail.com>
2021-02-07 10:57:48 -0500

    more code refactors

--- modules/tester-mod/scripts/skillshot.ts
+++ modules/tester-mod/scripts/skillshot.ts
@@ -6,10 +6,10 @@ export function Skillshot(events: TSEventHandlers) {
 		const player = spell.GetCaster().ToPlayer()
 		const TSPosition = player.GetRelativePoint(20,0)
 		const creature = player.SpawnCreature(ID.TESTER_MOD_SKILLSHOT_CREATURE,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),3,3000)
-		creature.SetLevel(player.GetLevel())
-		creature.CastSpell(creature,ID.TESTER_MOD_SKILLSHOT_CAST,true)
-		creature.CastSpell(creature,51019,true)
-		creature.MoveTo(1,TSPosition.x,TSPosition.y,TSPosition.z,true)
-		creature.SetDisplayId(11686)
+			creature.SetLevel(player.GetLevel())
+			creature.CastSpell(creature,ID.TESTER_MOD_SKILLSHOT_CAST,true)
+			creature.CastSpell(creature,51019,true)
+			creature.MoveTo(1,TSPosition.x,TSPosition.y,TSPosition.z,true)
+			creature.SetDisplayId(11686)
 	});
 }
