7145b34dede1bbc702863c3dec6ddd9d8fa55478
Connor K <connorkilbourne@gmail.com>
2021-02-09 18:55:25 -0500

    updated addon

--- modules/tester-mod/addons/tester-mod-addon.ts
+++ modules/tester-mod/addons/tester-mod-addon.ts
@@ -2,8 +2,14 @@ import { Events, SendToServer } from "./events";
 import { WrapperMessage } from "../shared/testMessage";
 
 // All we need to set up an event listener is a frame with a unique name.
-const frame = CreateFrame('Frame','UniqueName');
-
+const frame = CreateFrame('Frame','UniqueName',UIParent);
+frame.SetWidth(128)
+frame.SetHeight(64)
+const texture = frame.CreateTexture("texture1",'BACKGROUND')
+texture.SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
+texture.SetAllPoints(frame)
+frame.SetPoint("CENTER",0,0)
+frame.Hide()
 // Registers a listener for "ExampleMessage" packets
 Events.AddOns.OnMessage(frame,WrapperMessage,(msg)=>{
 	console.log("Addon Message: " + msg.inner.str)
@@ -11,4 +17,5 @@ Events.AddOns.OnMessage(frame,WrapperMessage,(msg)=>{
 	let serverpacket = new WrapperMessage()
 	serverpacket.inner.str = "from client"
 	SendToServer(serverpacket);
+    frame.Show()
 });
