1a3b8f9c0e218d20ccc986f346fdda6eef10571f
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-02-26 23:24:24 -0500

    Finalized Fire elemental

--- modules/classes-mod/data/Caster/Spells/Fire/Fire Elemental.ts
+++ modules/classes-mod/data/Caster/Spells/Fire/Fire Elemental.ts
@@ -13,17 +13,17 @@ entity.Scripts.onJustSummoned().Target.setSelf().Action.setCast(29166,0,0)
 entity.Scripts.onUpdateOoc(1,1,1,1).Target.setSelf().Action.setCast(29166,0,0)
 
 //(Timed create event)ID,initial min timer, initial max timer, repeated min timer, repeated max timer, chance
-entity.Scripts.onUpdateIc(0,0,0,0).Action.setCreateTimedEvent(0,0,0,2000,6000,100)
-entity.Scripts.onUpdateOoc(0,0,0,0).Action.setRemoveTimedEvent(0)
-entity.Scripts.onUpdateIc(0,0,0,0).Action.setCreateTimedEvent(1,0,0,5000,5000,100)
-entity.Scripts.onUpdateOoc(0,0,0,0).Action.setRemoveTimedEvent(1)
-entity.Scripts.onUpdateIc(0,0,0,0).Action.setCreateTimedEvent(2,0,0,30000,45000,100)
-entity.Scripts.onUpdateOoc(0,0,0,0).Action.setRemoveTimedEvent(2)
+entity.Scripts.onUpdateIc(0,0,0,0).Action.setCreateTimedEvent(0,0,0,2000,6000,100).row.event_flags.set(1)
+entity.Scripts.onUpdateOoc(0,0,0,0).Action.setRemoveTimedEvent(0).row.event_flags.set(1)
+entity.Scripts.onUpdateIc(0,0,0,0).Action.setCreateTimedEvent(1,0,0,5000,5000,100).row.event_flags.set(1)
+entity.Scripts.onUpdateOoc(0,0,0,0).Action.setRemoveTimedEvent(1).row.event_flags.set(1)
+entity.Scripts.onUpdateIc(0,0,0,0).Action.setCreateTimedEvent(2,0,0,30000,45000,100).row.event_flags.set(1)
+entity.Scripts.onUpdateOoc(0,0,0,0).Action.setRemoveTimedEvent(2).row.event_flags.set(1)
 
 //combat loop
 entity.Scripts.onTimedEventTriggered(0).Target.setClosestEnemy(40,0).Action.setCast(17195,0,0)
-//entity.Scripts.onTimedEventTriggered(1).Target.setClosestEnemy(40,0).Action.setCast(45902,2,7)
-//entity.Scripts.onTimedEventTriggered(2).Target.setClosestEnemy(40,0).Action.setCast(67037,2,7)
+entity.Scripts.onTimedEventTriggered(1).Target.setClosestEnemy(40,0).Action.setCast(55362,2,7)
+entity.Scripts.onTimedEventTriggered(2).Target.setClosestEnemy(40,0).Action.setCast(6725,2,7)
 
 const FIREELEMENTAL = std.Spells.create(MODNAME,'FireElemental',688).SkillLines.add(FIRE_SKILL.ID).setAutolearn().end;
 FIRE_SKILL_TRAINER_HORDE.Trainer.addSpell(FIREELEMENTAL.ID,10,69)//CHANGE LATER
@@ -38,5 +38,3 @@ FIREELEMENTAL.Effects.get(0).MiscValueA.set(entity.ID)
 FIREELEMENTAL.Effects.get(0).MiscValueB.set(61)
 FIREELEMENTAL.Effects.get(0).Radius.set(5,0,5)
 FIREELEMENTAL.Effects.get(0).ImplicitTargetA.setDestCasterSummon
-
-
