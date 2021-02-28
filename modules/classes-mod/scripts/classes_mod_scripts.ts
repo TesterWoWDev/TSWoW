a73be78b6525332f37a5546d10729e73da9db9c3
Connor K <connorkilbourne@gmail.com>
2021-02-28 17:43:07 -0500

    add ignite

--- modules/classes-mod/scripts/classes_mod_scripts.ts
+++ modules/classes-mod/scripts/classes_mod_scripts.ts
@@ -2,10 +2,12 @@ import { DoubleCast } from "./caster/DoubleCast"
 import { HealthLink } from "./caster/Spells/Death/HealthLink"
 import { HealthSwap } from "./caster/Spells/Death/HealthSwap"
 import { MultiPetAttack } from "./caster/Spells/Death/MultiPetAttack"
+import { Ignite } from "./caster/Spells/Fire/Ignite"
 
 export function Main(events: TSEventHandlers) {
 		MultiPetAttack(events)
 		HealthSwap(events)
 		HealthLink(events)
 		DoubleCast(events)
+        Ignite(events)
 }
