38d2ba0517056a9bbb6c855a0163fec0ad6ae759
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-02-28 19:52:27 -0500

    Ignite burn fixes

--- modules/classes-mod/scripts/caster/Spells/Fire/Ignite.ts
+++ modules/classes-mod/scripts/caster/Spells/Fire/Ignite.ts
@@ -1,14 +1,13 @@
 import { ID } from "../../../ID"
 
 export function Ignite(events: TSEventHandlers) {
-let firespells = [1,1,1,1,1]
+let firespells:TSArray<uint32> = [ID.CLASSES_MOD_FIREBALL_RANK7] //add fire spells here
 for(let i=0;i<firespells.length;i++){
     events.SpellID.OnCast(firespells[i],spell=>{
         spell.GetCaster().ToPlayer().CastSpell(spell.GetTarget().ToUnit(),ID.CLASSES_MOD_IGNITE_DOT,true)
     })
 }
     events.SpellID.OnTick(ID.CLASSES_MOD_IGNITE_DOT,aura=>{
-        console.log(aura.GetAmount())
-        //aura.SetAmount(aura.GetCaster().GetLevel()*3)
+        aura.SetAmount(aura.GetCaster().GetLevel()*3)
     })
 }
