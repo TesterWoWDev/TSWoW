c821093eb5443145c568d989f4e426c07b216952
Connor K <connorkilbourne@gmail.com>
2021-03-02 14:01:44 -0500

    more scrapper addon work

--- modules/item-addon/scripts/item_addon_scripts.ts
+++ modules/item-addon/scripts/item_addon_scripts.ts
@@ -1,45 +1,47 @@
-import { blagSlotMessage, frameCloseMessage } from "../shared/Messages";
+import { bagSlotMessage, frameCloseMessage } from "../shared/Messages";
 
 
 class ScrapperItems extends TSClass {
     // @ts-ignore
-    selectedItems: TSArray<blagSlotMessage>;
+    selectedItems: TSArray<bagSlotMessage>;
 }
 
 export function Main(events: TSEventHandlers) {
     
-    events.Addon.OnMessageID(blagSlotMessage,(player,msg)=>{
+    events.Addon.OnMessageID(bagSlotMessage,(player,msg)=>{
         let charItems = player.GetData().GetObject<ScrapperItems>(ModID(),"ScrapperItems",()=>new ScrapperItems());
         let selectedItems = charItems.selectedItems
         let found = false
         if(selectedItems.length < 12){
             for (let i=0;i<selectedItems.length;i++){
-                let bag:uint32 = selectedItems[i].Bag
-                let slot:uint32 = selectedItems[i].Slot
-                if(msg.Bag == bag && msg.Slot == slot){
+                if(msg.Bag == selectedItems[i].Bag && msg.Slot == selectedItems[i].Slot){
                     found = true
-                    //send packet saying item is already in scrapper(make items only stack to 1?)
+                    player.SendAreaTriggerMessage("That item is already in the scrapper!")
+                    break
                 }
             }
+            let pkt = new bagSlotMessage()
+            pkt.Bag = msg.Bag
+            pkt.Slot = msg.Slot
             if(!found){
-                    let pkt = new blagSlotMessage()
-                    pkt.Bag = msg.Bag
-                    pkt.Slot = msg.Slot
+                let item =player.GetItemByPos(msg.Bag,msg.Slot)
+                    //pkt.itemID = player.GetItemByPos(msg.Bag,msg.Slot).GetEntry();
+                    pkt.itemID = msg.itemID
                     selectedItems.push(pkt)
                     player.GetData().SetObject(ModID(),"ScrapperItems",charItems);
             }
+            player.SendData(pkt)
         }
         else{
-            //send packet saying its full
+            player.SendAreaTriggerMessage("You have already filled the scrapper!")
         }
     })
 
     events.Addon.OnMessageID(frameCloseMessage,(player,msg)=>{
-        let charItems = player.GetData().GetObject<ScrapperItems>(ModID(),"ScrapperItems",()=>new ScrapperItems());
-        let selectedItems = charItems.selectedItems
-            for (let i=0;i<selectedItems.length;i++){
-                console.log(selectedItems[i].Bag)
-                console.log(selectedItems[i].Slot)
-            }
+        let charItems = player.GetData().GetObject<ScrapperItems>(ModID(),"ScrapperItems",()=>new ScrapperItems())
+        console.log(charItems.selectedItems.length)
+        player.GetData().SetObject(ModID(),"ScrapperItems",new ScrapperItems());
+        charItems = player.GetData().GetObject<ScrapperItems>(ModID(),"ScrapperItems",()=>new ScrapperItems())
+        console.log(charItems.selectedItems.length)
     })
 }
