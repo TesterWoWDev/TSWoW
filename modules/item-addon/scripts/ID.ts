7455baf9b27557d5b8d0bac7d3a0e5719a8c7b83
Connor K <connorkilbourne@gmail.com>
2021-03-06 09:47:12 -0500

    add undead

--- modules/item-addon/scripts/ID.ts
+++ modules/item-addon/scripts/ID.ts
@@ -1,5 +1,6 @@
 export namespace ID {
-    export const ITEM_ADDON_BAGSLOTMESSAGE : uint32 = GetID("Messages","item-addon","bagSlotMessage");
+    export const ITEM_ADDON_BLAGSLOTMESSAGE : uint32 = GetID("Messages","item-addon","blagSlotMessage");
     export const ITEM_ADDON_FRAMECLOSEMESSAGE : uint32 = GetID("Messages","item-addon","frameCloseMessage");
+    export const ITEM_ADDON_BAGSLOTMESSAGE : uint32 = GetID("Messages","item-addon","bagSlotMessage");
     export const ITEM_ADDON_SCRAPMESSAGE : uint32 = GetID("Messages","item-addon","scrapMessage");
 };
