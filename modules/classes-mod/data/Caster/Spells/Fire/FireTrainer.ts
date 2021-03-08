ce0ed318ee366c451a910b433c89c2f6a7d0371a
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-03-08 00:05:59 -0500

    set fire trainer to neutral faction

--- modules/classes-mod/data/Caster/Spells/Fire/FireTrainer.ts
+++ modules/classes-mod/data/Caster/Spells/Fire/FireTrainer.ts
@@ -8,5 +8,6 @@ export const FIRE_SKILL_TRAINER_HORDE = std.CreatureTemplates.create(MODNAME, 'f
 	FIRE_SKILL_TRAINER_HORDE.Title.enGB.set('Fire Caster Trainer') // can be changed as with greeting
     FIRE_SKILL_TRAINER_HORDE.Trainer.Greeting.enGB.set('Welcome Young Fire Caster')
 	FIRE_SKILL_TRAINER_HORDE.Trainer.Class.set(CASTER_CLASS.ID);
+	FIRE_SKILL_TRAINER_HORDE.FactionTemplate.set(35)
 	FIRE_SKILL_TRAINER_HORDE.GossipID.set(0);
 	FIRE_SKILL_TRAINER_HORDE.spawn('MODNAME','Mulgore',Pos(1,-2925.737793,-248.827667,53.565315,4.401661))
