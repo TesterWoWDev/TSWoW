056844651dbff3ac4fcc006feff20a3fd5432fe6
Connor K <connorkilbourne@gmail.com>
2021-03-07 22:08:05 -0500

    more atlas work

--- modules/atlas-addon/scripts/atlas_addon_scripts.ts
+++ modules/atlas-addon/scripts/atlas_addon_scripts.ts
@@ -1,3 +1,25 @@
+import { creatureNamePacket, creatureNoExistPacket, itemLootFinishPacket, itemLootPacket } from "../shared/Messages";
+
 export function Main(events: TSEventHandlers) {
-    // Register your events here!
+    events.Addon.OnMessageID(creatureNamePacket,(player,msg)=>{ 
+        let entry = msg.entry
+        if(msg.isEntry == 0){
+            let query1 = QueryWorld('SELECT entry FROM creature_template WHERE name=' + msg.entry + ';');
+            while (query1.GetRow()){
+                entry = query1.GetString(0)
+            }
+        }
+        let query = QueryWorld('SELECT * FROM creature_loot_template WHERE entry=' + msg.entry + ';');
+        let i=0;
+        while(query.GetRow()) {
+            let pkt = new itemLootPacket()
+            player.SendData(pkt)
+            i = i+1;
+        }
+        if(i > 0){
+        player.SendData(new itemLootFinishPacket())
+        }else{
+            player.SendData(new creatureNoExistPacket())
+        }
+    })
 }
