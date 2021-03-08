82873eb9e8da4b5b107484e51ea06543a720ceaa
Connor K <connorkilbourne@gmail.com>
2021-03-08 12:26:20 -0500

    more atlas work

--- modules/atlas-addon/scripts/ID.ts
+++ modules/atlas-addon/scripts/ID.ts
@@ -1,5 +1,4 @@
 export namespace ID {
-    export const ATLAS_ADDON_BUTTONIDMESSAGE : uint32 = GetID("Messages","atlas-addon","buttonIDMessage");
     export const ATLAS_ADDON_CREATURENAMEPACKET : uint32 = GetID("Messages","atlas-addon","creatureNamePacket");
     export const ATLAS_ADDON_ITEMLOOTPACKET : uint32 = GetID("Messages","atlas-addon","itemLootPacket");
     export const ATLAS_ADDON_ITEMLOOTFINISHPACKET : uint32 = GetID("Messages","atlas-addon","itemLootFinishPacket");
--- modules/atlas-addon/scripts/atlas_addon_scripts.ts
+++ modules/atlas-addon/scripts/atlas_addon_scripts.ts
@@ -4,27 +4,38 @@ export function Main(events: TSEventHandlers) {
     events.Addon.OnMessageID(creatureNamePacket,(player,msg)=>{ 
         let entry = msg.entry
         if(msg.isName == 1){
-            let query1 = QueryWorld('SELECT entry FROM creature_template WHERE name=' + msg.entry + ';');
+            let query1 = QueryWorld('SELECT COUNT(*) FROM creature_template WHERE name=' + entry + ';');
             while (query1.GetRow()){
-                entry = query1.GetString(0)
+                if(query1.GetUInt32(0) > 0){
+                    let query2 = QueryWorld('SELECT entry FROM creature_template WHERE name=' + msg.entry + ';');
+                    while(query2.GetRow()){
+                        entry = query2.GetString(0)
+                    }
+                }
+                else{
+                    player.SendData(new creatureNoExistPacket())
+                    return;
+                }
             }
         }
-        let query = QueryWorld('SELECT * FROM creature_loot_template WHERE item=' + msg.entry + ';');
-        let i=false;
-        while(query.GetRow()) {
-            let pkt = new itemLootPacket()
-            pkt.itemID = ToUInt32(msg.entry);
-            pkt.dropChance = query.GetInt8(2);
-            pkt.itemCountMin = query.GetInt8(4);
-            pkt.itemCountMax = query.GetInt8(5);
-            
-            player.SendData(pkt)
-            i = true;
+        let queryCheck = QueryWorld('SELECT COUNT(*) FROM creature_loot_template WHERE Entry=' + entry + ';');
+        while(queryCheck.GetRow()){
+            if(queryCheck.GetUInt32(0) > 0){
+                let query = QueryWorld('SELECT * FROM creature_loot_template WHERE Entry=' + entry + ';');
+                while(query.GetRow()) {
+                    let pkt = new itemLootPacket()
+                        pkt.itemID = ToUInt32(entry);
+                        pkt.dropChance = query.GetInt8(2);
+                        pkt.itemCountMin = query.GetInt8(4);
+                        pkt.itemCountMax = query.GetInt8(5);
+                    player.SendData(pkt)
+                }
+            }
+            else{
+                player.SendData(new creatureNoExistPacket())
+                return;
+            }
         }
-        if(i){
-        player.SendData(new itemLootFinishPacket())
-        }else{
-            player.SendData(new creatureNoExistPacket())
-        }
+        player.SendData(new itemLootFinishPacket())  
     })
 }
