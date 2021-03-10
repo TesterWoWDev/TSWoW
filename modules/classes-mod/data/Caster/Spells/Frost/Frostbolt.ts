55ccde2c110f02566b475740388f81267d9ff555
Paxonym <rezamzinn@gmail.com>
2021-03-10 12:24:39 -0500

    Frostbolt Final

--- modules/classes-mod/data/Caster/Spells/Frost/Frostbolt.ts
+++ modules/classes-mod/data/Caster/Spells/Frost/Frostbolt.ts
@@ -6,26 +6,29 @@ import { CASTER_SKILL_TRAINER_HORDE } from "../CasterTrainer";
 const FROSTBOLT = std.Spells.create (MODNAME,'Frostbolt',11).SkillLines.add(FROST_SKILL.ID).setAutolearn().end; //Frostbolt casts will add stacks of frostbite.
 FROSTBOLT.Name.enGB.set('Frostbolt')
 FROSTBOLT.Subtext.enGB.set('Basic Spell')
+FROSTBOLT.AuraDescription.enGB.set('Freezing...')
 FROSTBOLT.Description.enGB.set('Hurls a bolt of frost at your enemy, causing $s1 damage and slowing their movement speed by 15% (up to a maximum of 30%). Victims succumb to frostbite and take an additional 15% damage for every frost bolt up to a maximum of 100% increased damage taken.')
-//FROSTBOLT.Icon.set('Interface\\Icons\\spell_fire_incinerate.blp')
+FROSTBOLT.Icon.set('Interface\\Icons\\spell_frost_chillingblast.blp')
 
 const FROSTBITE = std.Spells.create (MODNAME,'Frostbite',11).SkillLines.add(FROST_SKILL.ID).end; //When players stack to 2 stacks of frostbite, they will become a frostbomb.
 FROSTBITE.Name.enGB.set('Frostbite')
 FROSTBITE.Subtext.enGB.set('Passive')
+FROSTBOLT.AuraDescription.enGB.set('Movement speed decreased by $s1%.')
 FROSTBITE.Description.enGB.set('Freezing... this enemies movement is slowed, and they are taking increased damage from frost spells.')
-//FROSTBITE.Icon.set('Interface\\Icons\\spell_fire_incinerate.blp')
+FROSTBITE.Icon.set('Interface\\Icons\\spell_shadow_darkritual.blp')
 
 const FROSTBOMB = std.Spells.create (MODNAME,'Frostbomb',11).SkillLines.add(FROST_SKILL.ID).end; //Frostbomb will stack 4 times (up to +60% increased damage taken), so 15% per frostbolt.
 FROSTBOMB.Name.enGB.set('Frostbomb')
 FROSTBOMB.Subtext.enGB.set('Passive')
+FROSTBOLT.AuraDescription.enGB.set('Damage taken increased by $s1%.')
 FROSTBOMB.Description.enGB.set('This target has become frozen, shards of ice shatter and deal damage to surrounding allies whenever they are hit by a frostbolt.')
-//FROSTBOMB.Icon.set('Interface\\Icons\\spell_fire_incinerate.blp')
+FROSTBOMB.Icon.set('Interface\\Icons\\spell_frostward.blp')
 
 const SHATTER = std.Spells.create (MODNAME,'Shatter',11).SkillLines.add(FROST_SKILL.ID).end; //Shatter is the explosion from Frostbomb at 4 stacks that does mass AoE Damage.
 SHATTER.Name.enGB.set('Shatter')
 SHATTER.Subtext.enGB.set('Passive')
 SHATTER.Description.enGB.set('A shard of ice fractured from a frostbomb, this deals substantial aoe damage.')
-//SHATTER.Icon.set('Interface\\Icons\\spell_fire_incinerate.blp')
+SHATTER.Icon.set('Interface\\Icons\\spell_frost_icefloes.blp')
 
 FROSTBOLT.Effects.clearAll()
 FROSTBOLT.Effects.add()
@@ -45,4 +48,20 @@ FROSTBITE.Effects.clearAll()
         FROSTBITE.Effects.get(0).BasePoints.set(-16)
         FROSTBITE.Effects.get(0).DieSides.set(1)
 
+FROSTBOMB.Effects.clearAll()
+        FROSTBOMB.Stacks.set(4)
+        FROSTBOMB.Effects.add()
+            FROSTBOMB.Effects.get(0).EffectType.setApplyAura()
+            FROSTBOMB.Duration.set(10000,0,10000)
+            FROSTBOMB.Effects.get(0).AuraType.setModDamagePercentTaken()
+            FROSTBOMB.Effects.get(0).BasePoints.set(14)
+            FROSTBOMB.Effects.get(0).DieSides.set(1)
+
+SHATTER.Effects.clearAll()
+    SHATTER.Effects.add()
+        SHATTER.Effects.get(0).EffectType.setSchoolDamage()
+        SHATTER.Effects.get(0).BasePoints.set(25)
+        SHATTER.Effects.get(0).DieSides.set(5)
+        SHATTER.Effects.get(0).ImplicitTargetA.setUnitDestAreaEnemy()
+        SHATTER.Effects.get(0).Radius.set(10,0,10)
 
