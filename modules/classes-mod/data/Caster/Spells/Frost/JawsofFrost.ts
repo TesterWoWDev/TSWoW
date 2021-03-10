f5d3a0fecc7ba71e6b29d107f4e2199963fa7fc8
Paxonym <rezamzinn@gmail.com>
2021-03-10 18:39:53 -0500

    Frost Spells

---
+++ modules/classes-mod/data/Caster/Spells/Frost/JawsofFrost.ts
@@ -0,0 +1,28 @@
+import { std } from "tswow-stdlib";
+import { MODNAME } from "../../../moduleName";
+import { FROST_SKILL } from "../../Caster";
+import { CASTER_SKILL_TRAINER_HORDE } from "../CasterTrainer";
+
+const JAWSFROST = std.Spells.create (MODNAME,'JawsofFrost',11).SkillLines.add(FROST_SKILL.ID).setAutolearn().end; //Jaws of Frost is a targeted freezing attack, aimed to freeze the enemy and deal minor damage.
+JAWSFROST.Name.enGB.set('Jaws of Frost')
+JAWSFROST.Subtext.enGB.set('Basic Spell')
+JAWSFROST.AuraDescription.enGB.set('Frozen in place by jaws of Frost.')
+JAWSFROST.Description.enGB.set('Freeze the ground beneath your enemy, causing jaws of frost to rip out of the ground and root the target in place for 5 seconds.')
+JAWSFROST.Icon.set('Interface\\Icons\\spell_fire_blueimmolation.blp')
+
+JAWSFROST.Effects.clearAll()
+JAWSFROST.Effects.add()
+JAWSFROST.Visual.cloneFrom(6131,true)
+JAWSFROST.Range.set(0,40,0,40)
+    JAWSFROST.Duration.set(5000,0,5000)
+    JAWSFROST.Effects.get(0).EffectType.setSchoolDamage()
+    JAWSFROST.Effects.get(0).BasePoints.set(15)
+    JAWSFROST.Effects.get(0).DieSides.set(5)
+    JAWSFROST.Effects.get(0).PointsPerLevel.set(1)
+    JAWSFROST.Effects.get(0).ImplicitTargetA.setDestTargetEnemy()
+    JAWSFROST.Effects.get(0).BonusMultiplier.set(1)
+    JAWSFROST.Effects.get(0).BonusMultiplier.set(1)
+
+JAWSFROST.Effects.add()
+    JAWSFROST.Effects.get(1).EffectType.setApplyAura()
+    JAWSFROST.Effects.get(1).AuraType.setModRoot()
