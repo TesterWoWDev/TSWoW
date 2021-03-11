303d5cd87ed51f1f6cf915d212071a4095b30d2e
Paxonym <rezamzinn@gmail.com>
2021-03-11 09:44:50 -0500

    Frost Spell Updates

---
+++ modules/classes-mod/data/Caster/Spells/Frost/Fracture.ts
@@ -0,0 +1,32 @@
+import { std } from "tswow-stdlib";
+import { MODNAME } from "../../../moduleName";
+import { FROST_SKILL } from "../../Caster";
+import { CASTER_SKILL_TRAINER_HORDE } from "../CasterTrainer";
+
+const FRACTURE = std.Spells.create (MODNAME,'Fracture',11).SkillLines.add(FROST_SKILL.ID).setAutolearn().end; //Frostbolt casts will add stacks of frostbite.
+FRACTURE.Name.enGB.set('Fracture')
+FRACTURE.Subtext.enGB.set('Crowd Control')
+FRACTURE.AuraDescription.enGB.set('Suffering from the effects of Fracture, slowly freezing solid.')
+FRACTURE.Description.enGB.set('Summon a massive spike from the ground, dealing $s1 damage, and $s2 damage per second.')
+FRACTURE.Icon.set('Interface\\Icons\\spell_fire_bluehellfire.blp')
+
+FRACTURE.Effects.clearAll()
+FRACTURE.CastTime.set(1500,0,1500)
+FRACTURE.Range.set(0,35,0,35)
+FRACTURE.Effects.add()
+    FRACTURE.Visual.cloneFrom(50597,true)
+    FRACTURE.Effects.get(0).EffectType.setSchoolDamage()
+    FRACTURE.Effects.get(0).BasePoints.set(80)
+    FRACTURE.Effects.get(0).DieSides.set(15)
+    FRACTURE.Effects.get(0).PointsPerLevel.set(1)
+    FRACTURE.Effects.get(0).ImplicitTargetA.setDestTargetEnemy()
+    FRACTURE.Effects.get(0).BonusMultiplier.set(1)
+
+FRACTURE.Effects.add()
+    FRACTURE.Effects.get(1).EffectType.setApplyAura()
+    FRACTURE.Effects.get(1).BasePoints.set(150)
+    FRACTURE.Effects.get(1).DieSides.set(10)
+    FRACTURE.Effects.get(1).AuraType.setPeriodicDamage()
+    FRACTURE.Effects.get(1).ImplicitTargetA.setUnitDestAreaEnemy()
+    FRACTURE.Effects.get(1).BonusMultiplier.set(1)
+    FRACTURE.Effects.get(1).AuraPeriod.set(1000)
