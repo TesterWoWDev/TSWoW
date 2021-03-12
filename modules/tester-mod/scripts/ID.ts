8219b5392a8a54e8fd52f28ada9e931000c86aec
drugsthedeveloper <76750305+drugsthedeveloper@users.noreply.github.com>
2021-03-12 18:43:18 -0500

    Donte Connection Fix

--- modules/tester-mod/scripts/ID.ts
+++ modules/tester-mod/scripts/ID.ts
@@ -2,10 +2,4 @@ export namespace ID {
     export const TESTER_MOD_SKILLSHOT_PROC : uint32 = GetID("Spell","tester-mod","skillshot-proc");
     export const TESTER_MOD_SKILLSHOT_CAST : uint32 = GetID("Spell","tester-mod","skillshot-cast");
     export const TESTER_MOD_SKILLSHOT_CREATURE : uint32 = GetID("creature_template","tester-mod","skillshot-creature");
-    export const TESTER_MOD_TESTMESSAGE : uint32 = GetID("Messages","tester-mod","testMessage");
-    export const TESTER_MOD_WRAPPERMESSAGE : uint32 = GetID("Messages","tester-mod","WrapperMessage");
-    export const TESTER_MOD_BUTTONIDMESSAGE : uint32 = GetID("Messages","tester-mod","buttonIDMessage");
-    export const TESTER_MOD_SHOWFRAMEMESSAGE : uint32 = GetID("Messages","tester-mod","showFrameMessage");
-    export const TESTER_MOD_ITEMMESSAGE : uint32 = GetID("Messages","tester-mod","itemMessage");
-    export const TESTER_MOD_CURRENCYMESSAGE : uint32 = GetID("Messages","tester-mod","currencyMessage");
 };
