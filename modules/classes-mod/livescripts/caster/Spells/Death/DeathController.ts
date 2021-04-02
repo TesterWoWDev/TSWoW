9ab672319761d39d35dc5c1cbd1bde939bce1c84
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-04-02 18:06:47 -0400

    Cache refresh

--- modules/classes-mod/livescripts/caster/Spells/Death/DeathController.ts
+++
@@ -1,9 +0,0 @@
-import { HealthLink } from "./HealthLink";
-import { HealthSwap } from "./HealthSwap";
-import { MultiPetAttack } from "./MultiPetAttack";
-
-export function DeathController(events: TSEventHandlers) {
-    HealthLink(events)
-    HealthSwap(events)
-    MultiPetAttack(events)
-}
