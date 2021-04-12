c0064a35912dc1365c0ebd0a480fd4225c60dced
Connor <connorkilbourne@gmail.com>
2021-04-12 19:07:08 -0400

    auotlearn + corect naming

--- modules/classes-mod/datascripts/Ranged/Spells/Trapper/FrostGrenade.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Trapper/FrostGrenade.ts
@@ -2,7 +2,7 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { TRAPPER_SKILL } from "../../Ranged";
 
-const ERUPTION = std.Spells.create (MODNAME,'exploding-shot',39965).SkillLines.add(TRAPPER_SKILL.ID).AcquireMethod.set(0).end;
+const ERUPTION = std.Spells.create (MODNAME,'frost-grenade',39965).SkillLines.add(TRAPPER_SKILL.ID).setAutolearn().end;
 ERUPTION.Name.enGB.set('Frost Grenade')
 ERUPTION.Subtext.enGB.set('')
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
