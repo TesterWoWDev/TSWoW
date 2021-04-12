d0199a6e51346613536e08a5d120c880f6724d9c
Connor <connorkilbourne@gmail.com>
2021-04-12 19:27:25 -0400

    descriptions update

--- modules/classes-mod/datascripts/Melee/Spells/Juggernaut/Ferocity.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Juggernaut/Ferocity.ts
@@ -7,4 +7,4 @@ FEROCITY.Name.enGB.set('Ferocity')
 FEROCITY.Subtext.enGB.set('')
 //VOIDSTRIKE.Range.set(5,30,5,30)
 FEROCITY.Cooldown.set(90000,0,0,0)
-//FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
+FEROCITY.Description.enGB.set('Go into a frezy gaining 50% haste but taking 60 more damage from any attacks for 8 seconds.') //can change the %
--- modules/classes-mod/datascripts/Melee/Spells/Juggernaut/HeroicLeap.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Juggernaut/HeroicLeap.ts
@@ -7,4 +7,4 @@ HEROICLEAP.Name.enGB.set('Heroic Leap')
 HEROICLEAP.Subtext.enGB.set('')
 //VOIDSTRIKE.Range.set(5,30,5,30)
 HEROICLEAP.Cooldown.set(22000,0,0,0)
-//FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
+HEROICLEAP.Description.enGB.set('Leap forward slamming down, dealing 50% weapon damage and stunning all enemies caught underfoot for 2 seconds. This ability will remove any immobilization effects when used.')
