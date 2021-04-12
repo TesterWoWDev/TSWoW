b426bc180a93eadc9806f50231b19082d7dea414
Connor <connorkilbourne@gmail.com>
2021-04-12 19:07:08 -0400

    auotlearn + corect naming

--- modules/classes-mod/datascripts/Ranged/Spells/Amazonian/FrostSpear.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Amazonian/FrostSpear.ts
@@ -2,7 +2,7 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { AMAZONIAN_SKILL } from "../../Ranged";
 
-const FROSTSPEAR = std.Spells.create (MODNAME,'frost-spear',63111).SkillLines.add(AMAZONIAN_SKILL.ID).AcquireMethod.set(0).end;
+const FROSTSPEAR = std.Spells.create (MODNAME,'frost-spear',63111).SkillLines.add(AMAZONIAN_SKILL.ID).setAutolearn().end;
 FROSTSPEAR.Name.enGB.set('Frost Spear')
 FROSTSPEAR.Subtext.enGB.set('')
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
