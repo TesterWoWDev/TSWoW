9839701e50c425ad73417184119c79b466ac75a7
Connor <connorkilbourne@gmail.com>
2021-04-12 19:17:59 -0400

    void strike

---
+++ modules/classes-mod/datascripts/Melee/Spells/Assassination/VoidStrike.ts
@@ -0,0 +1,10 @@
+import { std } from "tswow-stdlib";
+import { MODNAME } from "../../../moduleName";
+import { ASSASSINATION_SKILL } from "../../Melee";
+
+const VOIDSTRIKE = std.Spells.create (MODNAME,'frost-grenade',52633).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
+VOIDSTRIKE.Name.enGB.set('Void Strike')
+VOIDSTRIKE.Subtext.enGB.set('')
+//VOIDSTRIKE.Range.set(5,30,5,30)
+VOIDSTRIKE.Cooldown.set(3000,0,0,0)
+//FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
