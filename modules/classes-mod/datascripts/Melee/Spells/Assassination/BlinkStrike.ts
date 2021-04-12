e172afa7534067de7f96ea6619fb9aedc740a935
Connor <connorkilbourne@gmail.com>
2021-04-12 19:19:02 -0400

    blink strike

--- modules/classes-mod/datascripts/Melee/Spells/Assassination/BlinkStrike.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Assassination/BlinkStrike.ts
@@ -2,9 +2,9 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { ASSASSINATION_SKILL } from "../../Melee";
 
-const VOIDSTRIKE = std.Spells.create (MODNAME,'void-strike',52633).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
-VOIDSTRIKE.Name.enGB.set('Void Strike')
-VOIDSTRIKE.Subtext.enGB.set('')
+const BLINKSTRIKE = std.Spells.create (MODNAME,'blink-strike',49961).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
+BLINKSTRIKE.Name.enGB.set('Blink Strike')
+BLINKSTRIKE.Subtext.enGB.set('')
 //VOIDSTRIKE.Range.set(5,30,5,30)
-VOIDSTRIKE.Cooldown.set(3000,0,0,0)
+BLINKSTRIKE.Cooldown.set(13000,0,0,0)
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
