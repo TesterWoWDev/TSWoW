d8aefb38d4cfc9d16b94f5557c801eb8ea1ca449
Connor <connorkilbourne@gmail.com>
2021-04-12 19:21:38 -0400

    cd change heroic leap

--- modules/classes-mod/datascripts/Melee/Spells/Juggernaut/HeroicLeap.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Juggernaut/HeroicLeap.ts
@@ -6,5 +6,5 @@ const HEROICLEAP = std.Spells.create (MODNAME,'heroic-leap',6544).SkillLines.add
 HEROICLEAP.Name.enGB.set('Heroic Leap')
 HEROICLEAP.Subtext.enGB.set('')
 //VOIDSTRIKE.Range.set(5,30,5,30)
-HEROICLEAP.Cooldown.set(90000,0,0,0)
+HEROICLEAP.Cooldown.set(22000,0,0,0)
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
