997edfa6ae1321f554b999979776f22f62adcf7b
Connor <connorkilbourne@gmail.com>
2021-04-12 19:14:14 -0400

    fix ranges and cooldowns of eruption, frost grenade and frost spear

--- modules/classes-mod/datascripts/Ranged/Spells/Marksman/ExplodingShot.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Marksman/ExplodingShot.ts
@@ -5,4 +5,4 @@ import { MARKSMAN_SKILL } from "../../Ranged";
 const EXPLODINGSHOT = std.Spells.create (MODNAME,'exploding-shot',7896).SkillLines.add(MARKSMAN_SKILL.ID).setAutolearn().end;
 EXPLODINGSHOT.Name.enGB.set('Exploding Shot')
 EXPLODINGSHOT.Subtext.enGB.set('')
-//FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
+EXPLODINGSHOT.Description.enGB.set('Inflicts normal damage plus $s1 to an enemy and any of its nearby allies. All damage is dealt as fire damage.') //can change the %
--- modules/classes-mod/datascripts/Ranged/Spells/Trapper/FrostGrenade.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Trapper/FrostGrenade.ts
@@ -2,7 +2,9 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { TRAPPER_SKILL } from "../../Ranged";
 
-const ERUPTION = std.Spells.create (MODNAME,'frost-grenade',39965).SkillLines.add(TRAPPER_SKILL.ID).setAutolearn().end;
-ERUPTION.Name.enGB.set('Frost Grenade')
-ERUPTION.Subtext.enGB.set('')
+const FROSTGRENADE = std.Spells.create (MODNAME,'frost-grenade',39965).SkillLines.add(TRAPPER_SKILL.ID).setAutolearn().end;
+FROSTGRENADE.Name.enGB.set('Frost Grenade')
+FROSTGRENADE.Subtext.enGB.set('')
+FROSTGRENADE.Range.set(5,30,5,30)
+FROSTGRENADE.Cooldown.set(18000,0,0,0)
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
