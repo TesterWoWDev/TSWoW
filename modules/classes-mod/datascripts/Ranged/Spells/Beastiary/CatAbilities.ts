7b0bb6c0a16d78095db49b09813a46d8d94a95ea
Connor <connorkilbourne@gmail.com>
2021-04-14 10:26:39 -0400

    rename cat spirit to Feral Affinity

--- modules/classes-mod/datascripts/Ranged/Spells/Beastiary/CatAbilities.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Beastiary/CatAbilities.ts
@@ -4,11 +4,11 @@ import { BEAST_SKILL } from "../../Ranged";
 
 const HARSHCLAW = std.Spells.create (MODNAME,'harsh-claw',48572).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
 HARSHCLAW.Name.enGB.set('Harsh Claw')
-HARSHCLAW.Subtext.enGB.set('Cat Spirit')
+HARSHCLAW.Subtext.enGB.set('Feral Affinity')
 HARSHCLAW.Description.enGB.set('Claw the target, causing 225% damage + 666 to the target. Must be behind the target. Effects which increase Bleed damage also increase Harsh Claw damage.')
 
 
 const RAKE = std.Spells.create (MODNAME,'rake',48574).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
 RAKE.Name.enGB.set('Rake')
-RAKE.Subtext.enGB.set('Cat Spirit')
+RAKE.Subtext.enGB.set('Feral Affinity')
 RAKE.Description.enGB.set('Rake the target for ${$AP/100+$m1} bleed damage and an additional ${$m2*3+$AP*0.18} damage over $d.')
--- modules/classes-mod/datascripts/Ranged/Spells/Beastiary/FeralAffinity.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Beastiary/FeralAffinity.ts
@@ -3,7 +3,7 @@ import { MODNAME } from "../../../moduleName";
 import { BEAST_SKILL } from "../../Ranged";
 
 const CATFORM = std.Spells.create (MODNAME,'cat-form',768).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
-CATFORM.Name.enGB.set('Cat Spirit')
+CATFORM.Name.enGB.set('Feral Affinity')
 CATFORM.Subtext.enGB.set('')
 CATFORM.Cooldown.set(40000,0,0,0)
-CATFORM.Description.enGB.set('Take up the Spirit of the Cat, shapeshifting for 20 seconds increasing melee attack power by 40 plus Agility. Also protects the caster from Polymorph effects and allows use of various cat abilities.')
+CATFORM.Description.enGB.set('Take up the Feral Affinity, shapeshifting for 20 seconds increasing melee attack power by 40 plus Agility. Also protects the caster from Polymorph effects and allows use of various cat abilities.')
