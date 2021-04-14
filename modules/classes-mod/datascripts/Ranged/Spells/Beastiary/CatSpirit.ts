dbe06f98ec05ee984c077d3c2d8f222d1037d6cc
Connor <connorkilbourne@gmail.com>
2021-04-14 10:24:34 -0400

    update cat spirit cooldown

--- modules/classes-mod/datascripts/Ranged/Spells/Beastiary/CatSpirit.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Beastiary/CatSpirit.ts
@@ -5,5 +5,5 @@ import { BEAST_SKILL } from "../../Ranged";
 const CATFORM = std.Spells.create (MODNAME,'cat-form',768).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
 CATFORM.Name.enGB.set('Cat Spirit')
 CATFORM.Subtext.enGB.set('')
-CATFORM.Cooldown.set(45000,0,0,0)
+CATFORM.Cooldown.set(40000,0,0,0)
 CATFORM.Description.enGB.set('Take up the Spirit of the Cat, shapeshifting for 20 seconds increasing melee attack power by 40 plus Agility. Also protects the caster from Polymorph effects and allows use of various cat abilities.')
