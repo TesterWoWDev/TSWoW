e5c547c474bf3a2da7f1d33ebfc43a502a8efd3f
Connor <connorkilbourne@gmail.com>
2021-04-14 10:20:12 -0400

    update spell inheritence for bear spirit

--- modules/classes-mod/datascripts/Ranged/Spells/Beastiary/BearSpirit.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Beastiary/BearSpirit.ts
@@ -2,7 +2,8 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { BEAST_SKILL } from "../../Ranged";
 
-const BEARFORM = std.Spells.create (MODNAME,'bear-form',7090).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
+const BEARFORM = std.Spells.create (MODNAME,'bear-form',4948).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
 BEARFORM.Name.enGB.set('Bear Spirit')
 BEARFORM.Subtext.enGB.set('')
+BEARFORM.Duration.set(30000,0,30000)
 BEARFORM.Description.enGB.set('Take up the Spirit of the Bear, shapeshifting for 30 seconds increasing armor and health. Also protects the caster from Polymorph effects and allows use of various cat abilities.')
