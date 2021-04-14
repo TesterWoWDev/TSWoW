0175630d303c9323c9c38fa2996e464d0aa0906a
Connor <connorkilbourne@gmail.com>
2021-04-14 10:17:08 -0400

    cat spirit spells

--- modules/classes-mod/datascripts/Ranged/Spells/Beastiary/BearForm.ts
+++
@@ -1,8 +0,0 @@
-import { std } from "tswow-stdlib";
-import { MODNAME } from "../../../moduleName";
-import { BEAST_SKILL } from "../../Ranged";
-
-const CATFORM = std.Spells.create (MODNAME,'cat-form',768).SkillLines.add(BEAST_SKILL.ID).setAutolearn().end;
-CATFORM.Name.enGB.set('Cat Spirit')
-CATFORM.Subtext.enGB.set('')
-CATFORM.Description.enGB.set('Take up the Spirit of the Cat, shapeshifting for 20 seconds increasing melee attack power by 40 plus Agility. Also protects the caster from Polymorph effects and allows use of various cat abilities.')
