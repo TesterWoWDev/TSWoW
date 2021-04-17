a9ff1cc5524d7e668e0787ecab4ce286d0ea99b6
Connor <connorkilbourne@gmail.com>
2021-04-17 09:14:09 -0400

    fix hidden amongst trees

--- modules/classes-mod/datascripts/Ranged/Spells/Amazonian/TreeHide.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Amazonian/TreeHide.ts
@@ -3,7 +3,7 @@ import { MODNAME } from "../../../moduleName";
 import { AMAZONIAN_SKILL } from "../../Ranged";
 
 //spawns a box, fix model in it
-const TREEHIDE = std.Spells.create (MODNAME,'tree-hide',2983).SkillLines.add(AMAZONIAN_SKILL.ID).setAutolearn().end;
+const TREEHIDE = std.Spells.create (MODNAME,'tree-hide',42943).SkillLines.add(AMAZONIAN_SKILL.ID).setAutolearn().end;
 TREEHIDE.Name.enGB.set('Hidden Amongst Trees')
 TREEHIDE.Subtext.enGB.set('')
 TREEHIDE.Cooldown.set(25000,0,0,0)
