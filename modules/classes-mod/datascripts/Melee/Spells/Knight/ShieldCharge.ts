d7369c823f6abaf4bb22445c68749eff95fe0dff
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-04-17 00:54:00 -0400

    made Shield Charge cost rage

--- modules/classes-mod/datascripts/Melee/Spells/Knight/ShieldCharge.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Knight/ShieldCharge.ts
@@ -7,4 +7,5 @@ SHIELDCHARGE.Name.enGB.set('Shield Charge')
 SHIELDCHARGE.Subtext.enGB.set('')
 //VOIDSTRIKE.Range.set(5,30,5,30)
 SHIELDCHARGE.Cooldown.set(11000,0,0,0)
+SHIELDCHARGE.Power.setRage(15)//adjust later?
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
