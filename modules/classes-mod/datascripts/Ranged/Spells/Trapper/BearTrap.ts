15b4f41d374e5a4d832b74d2e992f651328a0c30
Connor <connorkilbourne@gmail.com>
2021-04-16 21:36:07 -0400

    bear trap

---
+++ modules/classes-mod/datascripts/Ranged/Spells/Trapper/BearTrap.ts
@@ -0,0 +1,9 @@
+import { std } from "tswow-stdlib";
+import { MODNAME } from "../../../moduleName";
+import { TRAPPER_SKILL } from "../../Ranged";
+
+const ERUPTION = std.Spells.create (MODNAME,'bear-trap',61385).SkillLines.add(TRAPPER_SKILL.ID).setAutolearn().end;
+ERUPTION.Name.enGB.set('Bear Trao')
+ERUPTION.Subtext.enGB.set('')
+ERUPTION.Cooldown.set(25000,0,0,0)
+//FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
