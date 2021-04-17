75e1f0ca5d9950eeb881afdb2ccf2d958b5d9954
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-04-16 22:51:01 -0400

    Named AOE Taunt

--- modules/classes-mod/datascripts/Melee/Spells/Defender/AOETaunt.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Defender/AOETaunt.ts
@@ -3,9 +3,9 @@ import { MODNAME } from "../../../moduleName";
 import { DEFENDER_SKILL } from "../../Melee";
 import { MELEE_SKILL_TRAINER } from "../MeleeTrainer";
 
-const AOETAUNT = std.Spells.create(MODNAME, 'AOE TAUNT NAME HERE', 1161).SkillLines.add(DEFENDER_SKILL.ID).AcquireMethod.set(0).end;
+const AOETAUNT = std.Spells.create(MODNAME, 'Defender\'s Shout', 1161).SkillLines.add(DEFENDER_SKILL.ID).AcquireMethod.set(0).end;
     MELEE_SKILL_TRAINER.Trainer.addSpell(AOETAUNT.ID,10,19)//CHANGE LATER
-AOETAUNT.Name.enGB.set('AOE TAUNT NAME HERE')
+AOETAUNT.Name.enGB.set('Defender\'s Shout')
 AOETAUNT.Subtext.enGB.set("")
 AOETAUNT.Description.enGB.set('Shout to defend your allies, forcing all enemies within $a1 yards to attack you for $d.')
 AOETAUNT.AuraDescription.enGB.set('Taunted by the Defender.')
