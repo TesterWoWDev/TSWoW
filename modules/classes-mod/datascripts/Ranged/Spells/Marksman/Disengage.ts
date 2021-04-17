f6fd76af778897c42e59388e25d344d324934bfe
Connor <connorkilbourne@gmail.com>
2021-04-17 09:15:16 -0400

    fix disengage

--- modules/classes-mod/datascripts/Ranged/Spells/Marksman/Disengage.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Marksman/Disengage.ts
@@ -2,5 +2,5 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { MARKSMAN_SKILL } from "../../Ranged";
 
-const DISENGAGE = std.Spells.create (MODNAME,'disengage',781).SkillLines.add(MARKSMAN_SKILL.ID).setAutolearn().end;
+const DISENGAGE = std.Spells.create (MODNAME,'disengage',65870).SkillLines.add(MARKSMAN_SKILL.ID).setAutolearn().end;
 DISENGAGE.Subtext.enGB.set('')
