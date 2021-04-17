c7b55fc213b962c5565a0bce15fff7ddbc265ea7
Connor <connorkilbourne@gmail.com>
2021-04-16 21:36:47 -0400

    update bear trap

--- modules/classes-mod/datascripts/Ranged/Spells/Trapper/BearTrap.ts
+++ modules/classes-mod/datascripts/Ranged/Spells/Trapper/BearTrap.ts
@@ -2,7 +2,7 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { TRAPPER_SKILL } from "../../Ranged";
 
-const ERUPTION = std.Spells.create (MODNAME,'bear-trap',61385).SkillLines.add(TRAPPER_SKILL.ID).setAutolearn().end;
+const ERUPTION = std.Spells.create (MODNAME,'bear-trap',53432).SkillLines.add(TRAPPER_SKILL.ID).setAutolearn().end;
 ERUPTION.Name.enGB.set('Bear Trao')
 ERUPTION.Subtext.enGB.set('')
 ERUPTION.Cooldown.set(25000,0,0,0)
