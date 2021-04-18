777b6e2a99cef5eb365f8bf36a108988847cb49d
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-04-17 21:48:59 -0400

    Fixed current spells GCDs (melee only)

--- modules/classes-mod/datascripts/Melee/Spells/Assassination/VoidStrike.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Assassination/VoidStrike.ts
@@ -7,4 +7,5 @@ VOIDSTRIKE.Name.enGB.set('Void Strike')
 VOIDSTRIKE.Subtext.enGB.set('')
 //VOIDSTRIKE.Range.set(5,30,5,30)
 VOIDSTRIKE.Cooldown.set(3000,0,0,0)
+VOIDSTRIKE.Cooldown.StartTime.set(1500)
 //FROSTSPEAR.Description.enGB.set('Increases Critical Strike Chance of all Fire Spells by $s1% and Fire Spell casting time by $s2% for $d seconds.') //can change the %
