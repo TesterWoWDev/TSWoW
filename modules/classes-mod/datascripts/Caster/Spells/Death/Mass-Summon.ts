d8ac1eaf8e47de70350218179ab80a7b78f4693f
Connor <connorkilbourne@gmail.com>
2021-04-17 20:21:05 -0400

    remove death bolt from summon

--- modules/classes-mod/datascripts/Caster/Spells/Death/Mass-Summon.ts
+++ modules/classes-mod/datascripts/Caster/Spells/Death/Mass-Summon.ts
@@ -1,11 +1,6 @@
 import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import {DEATH_SKILL} from "../../Caster"
-export const DEATH_BOLT = std.Spells.create(MODNAME,'death-bolt',686)
-	DEATH_BOLT.Name.enGB.set('Death Bolt')
-	DEATH_BOLT.Description.enGB.set('A bolt of death!');
-	DEATH_BOLT.SkillLines.add(DEATH_SKILL.ID).setAutolearn();
-	//DEATH_BOLT.SkillLines.add(DEATH_SKILL.ID).AcquireMethod.set(0)//learn by trainer
 
 export const MINION_SUMMON = std.Spells.create(MODNAME,'minion-summon',66842)
 	MINION_SUMMON.Name.enGB.set('Mass Reanimation')
