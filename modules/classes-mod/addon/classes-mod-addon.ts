ca0b3435896d50220f309d5079afb6d1dcbcefc7
Connor <connorkilbourne@gmail.com>
2021-04-18 09:43:09 -0400

    statusbar added to addon functionality

--- modules/classes-mod/addon/classes-mod-addon.ts
+++ modules/classes-mod/addon/classes-mod-addon.ts
@@ -1,5 +1,5 @@
 import { Events, SendToServer } from "./lib/Events";
-import { powerUpdate } from "../shared/Messages";
+import { powerUpdate, resetFrame } from "../shared/Messages";
 
 //life
 const lifeItem = 100001
@@ -7,20 +7,10 @@ const maxLifeResource = 5
 //death
 const deathItem = 1
 const maxDeathResource = 5
-
+let frames = []
 let allButtons = []
+let setup = false
 let mframe = CreateFrame('Frame','test',UIParent);
-    mframe.SetWidth(240)
-    mframe.SetHeight(70)
-    mframe.SetBackdrop({bgFile : "Interface/TutorialFrame/TutorialFrameBackground", 
-        edgeFile : "Interface/DialogFrame/UI-DialogBox-Border", 
-        tile : true, tileSize : 22, edgeSize : 22, 
-        insets : { left : 4, right : 4, top : 4, bottom : 4 }});
-    mframe.SetBackdropColor(0,0,0,1);
-    mframe.SetPoint("TOPLEFT",50,-100)
-
-    mframe.SetMovable(true)
-	mframe.EnableMouse(true)
     mframe.Hide()
     // mframe.RegisterForDrag("LeftButton")
     // mframe.SetScript("OnDragStart", mframe.StartMoving)
@@ -31,20 +21,50 @@ Events.AddOns.OnMessage(mframe,powerUpdate,(msg)=>{
     chooseUpdateFunction(msg)
 })
 
+Events.AddOns.OnMessage(mframe,resetFrame,(msg)=>{
+    setup = false
+    for (let i=0;i<frames.length;i++){
+        frames[i].Hide()
+    }
+    frames = []
+})
+
 function chooseUpdateFunction(msg){
     for(let i=0;i<allButtons.length;i++){
         allButtons[i].Hide()
     }
     allButtons = []
     if(GetItemCount(lifeItem,true,false) > 0){//life
+        makeLifeFrame()
         updateLeaves(msg.powerCnt)
     }else if(GetItemCount(deathItem,true,false) > 0){//death
+        makeDeathFrame()
         updateSouls(msg.powerCnt)
     }else{
         mframe.Hide()
     }
 }
+
 //life
+function makeLifeFrame(){
+    if(!setup){
+        let lifeFrame = CreateFrame('Frame','life',mframe)
+        lifeFrame.SetWidth(240)
+        lifeFrame.SetHeight(70)
+        lifeFrame.SetBackdrop({bgFile : "Interface/TutorialFrame/TutorialFrameBackground", 
+            edgeFile : "Interface/DialogFrame/UI-DialogBox-Border", 
+            tile : true, tileSize : 22, edgeSize : 22, 
+            insets : { left : 4, right : 4, top : 4, bottom : 4 }});
+        lifeFrame.SetBackdropColor(0,0,0,1);
+        lifeFrame.SetPoint("TOPLEFT",50,-100)
+
+        lifeFrame.SetMovable(true)
+        lifeFrame.EnableMouse(true)
+        setup = true
+        frames.push(lifeFrame)
+    }
+}
+
 function updateLeaves(count){
     mframe.SetWidth(130)
     mframe.SetHeight(35)
@@ -63,9 +83,36 @@ function updateLeaves(count){
                 count--
             }else{
                 tex.SetTexture("Interface\\BUTTONS\\UI-CheckBox-Check-Disabled.blp")
-            }
-                
+            }  
         allButtons.push(button)
+    }
+}
+
+function makeDeathFrame(){
+    if(!setup){
+        let deathFrame = CreateFrame('StatusBar','death',mframe)
+        deathFrame.SetPoint("CENTER", UIParent, "CENTER", 0, 100)
+        deathFrame.SetWidth(200)
+        deathFrame.SetHeight(20)
+        deathFrame.SetStatusBarTexture("Interface\\TARGETINGFRAME\\UI-StatusBar")
+        deathFrame.GetStatusBarTexture()
+        deathFrame.GetStatusBarTexture()
+        deathFrame.SetStatusBarColor(0, 0.65, 0,1)
+        
+        let bg = deathFrame.CreateTexture(null, "BACKGROUND")
+        bg.SetTexture("Interface\\TARGETINGFRAME\\UI-StatusBar")
+        bg.SetAllPoints()
+        bg.SetVertexColor(0, 0.35, 0)
+        
+        let value = deathFrame.CreateFontString(null, "OVERLAY")
+        value.SetPoint("LEFT", deathFrame, "LEFT", 4, 0)
+        value.SetFont("Fonts\\FRIZQT__.TTF", 16, "OUTLINE")
+        value.SetJustifyH("LEFT")
+        value.SetShadowOffset(1, -1)
+        value.SetTextColor(0, 1, 0)
+        value.SetText("100%")
+        setup = true
+        frames.push(deathFrame)
     }
 }
 
--- modules/classes-mod/shared/Messages.ts
+++ modules/classes-mod/shared/Messages.ts
@@ -3,3 +3,8 @@ export class powerUpdate {
     @MsgPrimitive
         powerCnt: uint32 = 0;
 }
+
+export class resetFrame {
+    @MsgPrimitive
+        powerCnt: uint32 = 0;
+}
