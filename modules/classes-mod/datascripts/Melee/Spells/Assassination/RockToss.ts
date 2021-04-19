e33e18b3158a5bbab7dcf9452cae04d7436285e7
Connor <connorkilbourne@gmail.com>
2021-04-19 15:43:32 -0400

    spell fixes - thanks will

--- modules/classes-mod/datascripts/Melee/Spells/Assassination/RockToss.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Assassination/RockToss.ts
@@ -2,11 +2,9 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { ASSASSINATION_SKILL } from "../../Melee";
 
-const ROCKTOSS = std.Spells.create (MODNAME,'rock-toss',69989).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
-ROCKTOSS.Name.enGB.set('Rock Toss')
-ROCKTOSS.Subtext.enGB.set('')
-ROCKTOSS.Cooldown.StartTime.set(1500)
-ROCKTOSS.Power.setEnergy(0)
-ROCKTOSS.Power.setRage(10)
-ROCKTOSS.Range.set(0,20,0,20)
-ROCKTOSS.Description.enGB.set('Toss a rock at a nearby enemy for $s1 Arcane damage.') //can change the %
+const TOSS = std.Spells.create (MODNAME,'thrown-toss',39129).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
+TOSS.Name.enGB.set('Rock Toss')
+TOSS.Subtext.enGB.set('')
+TOSS.Cooldown.StartTime.set(1500)
+TOSS.Power.setRage(10)
+TOSS.Description.enGB.set('Toss a thrown weapon at a nearby enemy dealing Physical damage.') //can change the %
--- modules/classes-mod/datascripts/Melee/Spells/Assassination/Stealth.ts
+++ modules/classes-mod/datascripts/Melee/Spells/Assassination/Stealth.ts
@@ -2,7 +2,7 @@ import { std } from "tswow-stdlib";
 import { MODNAME } from "../../../moduleName";
 import { ASSASSINATION_SKILL } from "../../Melee";
 
-const STEALTH = std.Spells.create (MODNAME,'stealth',69989).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
+const STEALTH = std.Spells.create (MODNAME,'stealth',58506).SkillLines.add(ASSASSINATION_SKILL.ID).setAutolearn().end;
 STEALTH.Name.enGB.set('Stealth')
 STEALTH.Subtext.enGB.set('')
 STEALTH.Cooldown.set(60000,0,0,0)
