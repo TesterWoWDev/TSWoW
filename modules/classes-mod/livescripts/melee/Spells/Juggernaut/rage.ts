302e5c3be1b0727e44ca3941f6ec4822039ec6d6
Connor <connorkilbourne@gmail.com>
2021-05-29 18:02:53 -0400

    comment out dumb shit

--- modules/classes-mod/livescripts/melee/Spells/Juggernaut/rage.ts
+++ modules/classes-mod/livescripts/melee/Spells/Juggernaut/rage.ts
@@ -1,10 +1,10 @@
 export function rageSpell(events: TSEventHandlers) {
-    events.SpellID.OnCast(ID.rage,spell=>{
-        let caster = spell.GetCaster()
-        if(caster.GetHealthPct() <= 50){
-            spell.Finish()
-        }else{
-            spell.Cancel()
-        }
-    })
+    // events.SpellID.OnCast(ID.rage,spell=>{
+    //     let caster = spell.GetCaster()
+    //     if(caster.GetHealthPct() <= 50){
+    //         spell.Finish()
+    //     }else{
+    //         spell.Cancel()
+    //     }
+    // })
 }
