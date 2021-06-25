16e99b8a4843979dbb2b7d40af622c765b9648ee
TesterWoWDev <connorkilbourne@gmail.com>
2021-06-25 08:27:15 -0400

    more crafting work

---
+++ modules/minecraft-mod/datascripts/recipe-creation.ts
@@ -0,0 +1,19 @@
+import { std } from "tswow-stdlib";
+import { SQL } from "wotlkdata/sql/SQLFiles";
+import { createBaseResources, generateGearRecipes, generateWeaponRecipes } from "./recipe-functions";
+let MODNAME = 'minecraft-mod'
+
+SQL.Databases.world_dest.write('DROP TABLE IF EXISTS `minecraft_recipes`;')
+SQL.Databases.world_dest.write('CREATE TABLE `minecraft_recipes` (`pos1` int(11) DEFAULT NULL,`pos2` int(11) DEFAULT NULL,`pos3` int(11) DEFAULT NULL,`pos4` int(11) DEFAULT NULL,`pos5` int(11) DEFAULT NULL,`pos6` int(11) DEFAULT NULL,`pos7` int(11) DEFAULT NULL,`pos8` int(11) DEFAULT NULL,`pos9` int(11) DEFAULT NULL,`req1` int(11) DEFAULT NULL,`cnt1` int(11) DEFAULT NULL,`req2` int(11) DEFAULT NULL,`cnt2` int(11) DEFAULT NULL,`req3` int(11) DEFAULT NULL,`cnt3` int(11) DEFAULT NULL,`req4` int(11) DEFAULT NULL,`cnt4` int(11) DEFAULT NULL,`req5` int(11) DEFAULT NULL,`cnt5` int(11) DEFAULT NULL,`req6` int(11) DEFAULT NULL,`cnt6` int(11) DEFAULT NULL,`req7` int(11) DEFAULT NULL,`cnt7` int(11) DEFAULT NULL,`req8` int(11) DEFAULT NULL,`cnt8` int(11) DEFAULT NULL,`req9` int(11) DEFAULT NULL,`cnt9` int(11) DEFAULT NULL,`craftID` int(11) DEFAULT NULL, `craftCount` int(11) DEFAULT NULL)')
+
+let tierOneStrResources = createBaseResources('tier1-str',2934,'scrappy stout',['gem','material','epulet','chain','metal','reinforced metal','string'])
+generateGearRecipes(tierOneStrResources[0],tierOneStrResources[1],tierOneStrResources[2],tierOneStrResources[3],[39,39,39,39,39,39,39,39,39,39,39,39])
+generateWeaponRecipes(tierOneStrResources[0],tierOneStrResources[1],tierOneStrResources[4],tierOneStrResources[5],tierOneStrResources[6],[39,39,39,39,39,39,39,39,39,39,39,39,39,39])
+
+let tierOneAgiResources = createBaseResources('tier1-agi',2934,'scrappy agile',['gem','material','epulet','chain','metal','reinforced metal','string'])
+generateGearRecipes(tierOneAgiResources[0],tierOneAgiResources[1],tierOneAgiResources[2],tierOneAgiResources[3],[39,39,39,39,39,39,39,39,39,39,39,39])
+generateWeaponRecipes(tierOneAgiResources[0],tierOneAgiResources[1],tierOneAgiResources[4],tierOneAgiResources[5],tierOneAgiResources[6],[39,39,39,39,39,39,39,39,39,39,39,39,39,39])
+
+let tierOneIntResources = createBaseResources('tier1-str',2934,'scrappy intelligent',['gem','material','epulet','chain','metal','reinforced metal','string'])
+generateGearRecipes(tierOneIntResources[0],tierOneIntResources[1],tierOneIntResources[2],tierOneIntResources[3],[39,39,39,39,39,39,39,39,39,39,39,39])
+generateWeaponRecipes(tierOneIntResources[0],tierOneIntResources[1],tierOneIntResources[4],tierOneIntResources[5],tierOneIntResources[6],[39,39,39,39,39,39,39,39,39,39,39,39,39,39])
--- modules/minecraft-mod/datascripts/recipe-functions.ts
+++ modules/minecraft-mod/datascripts/recipe-functions.ts
@@ -1,12 +1,27 @@
+import { MODNAME } from "quests-mod";
 import { std } from "tswow-stdlib";
 import { SQL } from "wotlkdata/sql/SQLFiles";
-SQL.Databases.world_dest.write('DROP TABLE IF EXISTS `minecraft_recipes`;')
-SQL.Databases.world_dest.write('CREATE TABLE `minecraft_recipes` (`pos1` int(11) DEFAULT NULL,`pos2` int(11) DEFAULT NULL,`pos3` int(11) DEFAULT NULL,`pos4` int(11) DEFAULT NULL,`pos5` int(11) DEFAULT NULL,`pos6` int(11) DEFAULT NULL,`pos7` int(11) DEFAULT NULL,`pos8` int(11) DEFAULT NULL,`pos9` int(11) DEFAULT NULL,`req1` int(11) DEFAULT NULL,`cnt1` int(11) DEFAULT NULL,`req2` int(11) DEFAULT NULL,`cnt2` int(11) DEFAULT NULL,`req3` int(11) DEFAULT NULL,`cnt3` int(11) DEFAULT NULL,`req4` int(11) DEFAULT NULL,`cnt4` int(11) DEFAULT NULL,`req5` int(11) DEFAULT NULL,`cnt5` int(11) DEFAULT NULL,`req6` int(11) DEFAULT NULL,`cnt6` int(11) DEFAULT NULL,`req7` int(11) DEFAULT NULL,`cnt7` int(11) DEFAULT NULL,`req8` int(11) DEFAULT NULL,`cnt8` int(11) DEFAULT NULL,`req9` int(11) DEFAULT NULL,`cnt9` int(11) DEFAULT NULL,`craftID` int(11) DEFAULT NULL, `craftCount` int(11) DEFAULT NULL)')
-//[head,neck,shoulder,back,chest,bracer,hand,belt,leg,boot,ring,trink]
-generateGearItems(39,39,39,39,[39,39,39,39,39,39,39,39,39,39,39,39])
-//[ohs,ohm,oha,dag,ths,thm,tha,book,pole,bow,staff,wand,shield,fist]
-generateWeaponItems(39,39,39,39,39,[39,39,39,39,39,39,39,39,39,39,39,39,39,39])
-function generateGearItems(gem: Number, material: Number, epulet: Number, chain: Number,itemID: Number[]) {
+
+//tiername,parent,prefix,[itemnames]
+//let returnItems = createBaseResources('tier1',2934,'prefix',['moonstone','leather','epulet','chain','steel','reinforced steel','string'])
+//gem,material,epulet,chain[head,neck,shoulder,back,chest,bracer,hand,belt,leg,boot,ring,trink]
+//generateGearRecipes(returnItems[0],returnItems[1],returnItems[2],returnItems[3],[39,39,39,39,39,39,39,39,39,39,39,39])
+//material,metal,reinforced,string,chain,[ohs,ohm,oha,dag,ths,thm,tha,book,pole,bow,staff,wand,shield,fist]
+//generateWeaponRecipes(returnItems[0],returnItems[1],returnItems[4],returnItems[5],returnItems[6],[39,39,39,39,39,39,39,39,39,39,39,39,39,39])
+
+
+export function createBaseResources(tierPrefix:string,parentItem:number,prefix:string,itemNames:string[]):number[]{
+    let test = ['gem','material','epulet','chain','metal','reinforced','string']
+    let allItemIDs = []
+    for(let i=0;i<test.length;i++){
+        let item = std.Items.create(MODNAME, tierPrefix + '-' + test[0], parentItem)
+        item.Name.enGB.set(prefix + ' ' + itemNames[0])
+        allItemIDs.push(item.ID)
+    }
+    return allItemIDs
+}
+
+export function generateGearRecipes(gem: Number, material: Number, epulet: Number, chain: Number,itemID: Number[]) {
     let zero = 0
     SQL.Databases.world_dest.write('INSERT INTO `minecraft_recipes` VALUES(' + material + ',' + material + ',' + material + ',' + material + ',' + zero + ',' + material + ',' + zero + ',' + zero + ',' + zero + ',' +
     '' + material + ',' + 5 + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' +
@@ -57,7 +72,7 @@ function generateGearItems(gem: Number, material: Number, epulet: Number, chain:
     '' + itemID[11] + ',' + 1 + ')')
 }
 
-function generateWeaponItems(material: number,metal: number,reinforced: number,string: number,gem: number,itemID: number[]){
+export function generateWeaponRecipes(gem: number,material: number,metal: number,reinforced: number,string: number,itemID: number[]){
     let zero = 0
     SQL.Databases.world_dest.write('INSERT INTO `minecraft_recipes` VALUES(' + zero + ',' + metal + ',' + zero + ',' + zero + ',' + metal + ',' + zero + ',' + zero + ',' + material + ',' + zero + ',' +
     '' + metal + ',' + 2 + ',' + material + ',' + 1 + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' + zero + ',' +
