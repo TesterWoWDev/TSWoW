808cd4bc8eaf2ab72ea4b3434ba34419528f86be
TesterWoWDev <connorkilbourne@gmail.com>
2021-06-28 12:04:53 -0400

    auto learn craft + remove global from livescripts

--- modules/addn-mod/livescripts/ID.ts
+++
@@ -1,6 +0,0 @@
-export namespace ID {
-    export const ATLAS_ADDON_CREATURENAMEMESSAGE : uint32 = GetID("Messages","addn-mod","creatureNameMessage");
-    export const ATLAS_ADDON_ITEMLOOTMESSAGE : uint32 = GetID("Messages","addn-mod","itemLootMessage");
-    export const ATLAS_ADDON_ITEMLOOTFINISHMESSAGE : uint32 = GetID("Messages","addn-mod","itemLootFinishMessage");
-    export const ATLAS_ADDON_CREATURENOEXISTMESSAGE : uint32 = GetID("Messages","addn-mod","creatureNoExistMessage");
-};
--- modules/tester-mod/livescripts/skillshot.ts
+++ modules/tester-mod/livescripts/skillshot.ts
@@ -1,13 +1,11 @@
-import { ID } from "./ID";
-
 export function Skillshot(events: TSEventHandlers) {
 		//test build out for summon creature that goes forward 20 yards
 	events.SpellID.OnCast(53307,(spell)=>{
 		const player = spell.GetCaster().ToPlayer()
 		const TSPosition = player.GetRelativePoint(20,0)
-		const creature = player.SpawnCreature(ID.TESTER_MOD_SKILLSHOT_CREATURE,player.GetX(),player.GetY(),player.GetZ(),player.GetO(),3,3000)
+		const creature = player.SpawnCreature(GetID("creature_template","tester-mod","skillshot-creature"),player.GetX(),player.GetY(),player.GetZ(),player.GetO(),3,3000)
 			creature.SetLevel(player.GetLevel())
-			creature.CastSpell(creature,ID.TESTER_MOD_SKILLSHOT_CAST,true)
+			creature.CastSpell(creature,GetID("Spell","tester-mod","skillshot-cast"),true)
 			creature.CastSpell(creature,51019,true)
 			creature.MoveTo(1,TSPosition.x,TSPosition.y,TSPosition.z,true)
 			creature.SetDisplayId(11686)
