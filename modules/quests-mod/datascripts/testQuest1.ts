dda7087548ff644daed7ef3280b909fac5c3a192
TesterWoWDev <connorkilbourne@gmail.com>
2021-06-30 11:24:18 -0400

    add PoC item and creature for ghost

--- modules/quests-mod/datascripts/testQuest1.ts
+++ modules/quests-mod/datascripts/testQuest1.ts
@@ -1,6 +1,17 @@
 import { MODNAME } from "./quests-mod-data"
 import { std } from "tswow-stdlib"
-let quest = std.Quests.create(MODNAME,'quest1')
+
+let mob = std.CreatureTemplates.create(MODNAME,'quest1-mob',2934)//replace 2934 with parentCreature
+mob.Name.enGB.set('eh')
+mob.Subname.enGB.set('eh')
+mob.Level.set(1,3)
+
+let item = std.Items.create(MODNAME,'quest1-item',2934)//replace 2934 with parentItem
+item.Name.enGB.set('eej')
+item.Description.enGB.set('asdasd')
+item.Bonding.setQuestItem()
+
+let quest = std.Quests.create(MODNAME,'quest1-quest')
 //option 1
 quest.Flags.set(1)
 //option 2
@@ -12,8 +23,8 @@ quest.QuestLevel.set(1)
 
 // quest.Objectives.Entity.add(creatureid,count)//kill quest
 // quest.Objectives.Item.add(itemid,itemcount)//return item quest
-quest.Objectives.Entity.add(121,1)//kill quest
-quest.Objectives.Item.add(133,1)//return item quest
+quest.Objectives.Entity.add(mob.ID,1)//kill quest
+quest.Objectives.Item.add(item.ID,1)//return item quest
 //option 1
 quest.Questgiver.addStarter(131)
 quest.Questgiver.addEnder(122)
