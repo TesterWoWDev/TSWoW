c6ed0e8328da4beef6eba4f85ceda4d05a7702ab
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-01 13:05:59 -0400

    move enchant + put in mining node functions

---
+++ modules/minecraft-mod/datascripts/mining-nodes/mining-node-functions.ts
@@ -0,0 +1,39 @@
+import { std } from "tswow-stdlib"
+import { GameObjectChest } from "tswow-stdlib/GameObject/Types/GameObjectChest"
+import { MODNAME } from "../database-setup"
+//lockID-mining skill required
+// 38-0
+// 18-25
+// 19-50
+// 20-75
+// 21-100
+// 22-125
+// 25-150
+// 379-175
+// 380-205
+// 400-230
+// 939-255
+// 1649-275
+// 1632-305
+// 1650-325
+// 1652-350
+// 1651-375
+
+//setupMiningNode(makeMiningNode(name,displayID,lockID,modPrefix),metalID,refinedMetalID,[gem1,gem2,gem3])
+
+function makeMiningNode(name:string,displayID:number,lockID:number,modPrefix:string):GameObjectChest{
+    let miningNode = std.GameObjectTemplates.create(MODNAME,modPrefix,1731).setChest()
+    miningNode.Name.enGB.set(name)
+    miningNode.Display.setID(displayID)//currently copper vein
+    miningNode.Lock.setID(lockID)
+    return miningNode
+}
+
+function setupMiningNode(miningNode: GameObjectChest, metalID: number, refinedMetalID: number, gemIDArray: number[]):number {
+    miningNode.Loot.addItem(metalID,100,1,2)
+    miningNode.Loot.addItem(refinedMetalID,50,1,2)
+    gemIDArray.forEach((value,index,arr)=>{
+        miningNode.Loot.addItem(value,10,1,1)
+    })
+    return miningNode.ID
+}   
