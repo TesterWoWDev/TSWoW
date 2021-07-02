221e8dc3ee027216197e8ce36b12298921367ba2
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-02 12:18:09 -0400

    move axepickvel to items

--- modules/minecraft-mod/datascripts/items/crafting-item.ts
+++ modules/minecraft-mod/datascripts/items/crafting-item.ts
@@ -1,5 +1,5 @@
 import { std } from "tswow-stdlib"
-import { MODNAME } from "./database-setup"
+import { MODNAME } from "../database-setup"
 let item = std.Items.create(MODNAME,'multitool',40772)
 item.Name.enGB.set('Axepickvel')
 item.Description.enGB.set('This amazing creation by an old man is all the tools one could ever need and more.')
