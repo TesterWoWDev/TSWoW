96f4ae4c2284cd2ce80444ebab86167ef00db3fc
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-05 11:15:18 -0400

    remove useless enchant test

--- modules/minecraft-mod/datascripts/items/enchants/enchant-test.ts
+++
@@ -1,3 +0,0 @@
-import { createRecipe } from "../../functions/recipe-functions";
-//position, requirement, requirement count, rewarded item, how many rewarded
-createRecipe([0,1210,0,0,-1,0,0,0,0],[1210,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0],1047,1)
