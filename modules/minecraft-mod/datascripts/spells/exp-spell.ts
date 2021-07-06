1fbf2a458e64269fce359871a3199d2d03c6c585
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-07-06 13:22:13 -0400

    exportable exp spell

--- modules/minecraft-mod/datascripts/spells/exp-spell.ts
+++ modules/minecraft-mod/datascripts/spells/exp-spell.ts
@@ -1,6 +1,8 @@
 import { std } from "tswow-stdlib"
 import { MODNAME } from "../modname"
-let expSpell = std.Spells.create(MODNAME,'exp-spell',57353)
+
+
+export let expSpell = std.Spells.create(MODNAME,'exp-spell',57353)
 expSpell.Name.enGB.set('Increased Experience')
 expSpell.Description.enGB.set('Experience gained is increased by $s1%.')
 expSpell.Effects.get(0).BasePoints.set(0)
