73e4b1d890e4f8b9526de34456fbb0429dc84a78
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-06 12:13:29 -0400

    add class spawn file

---
+++ modules/minecraft-mod/datascripts/class-changes/clas-spawn.ts
@@ -0,0 +1,10 @@
+import { SQL } from "wotlkdata/sql/SQLFiles"
+
+// SQL.playercreateinfo.filter({}).forEach((value,index,arr)=>{
+//     if(value.class.get() == classID){
+//         value.map.set()
+//         value.position_x.set()
+//         value.position_y.set()
+//         value.position_z.set()
+//     }
+// })
