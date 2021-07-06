4189a7be4f93364c25d3c94990f91cf3c0a5df64
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-07-06 13:16:11 -0400

    NOW spell changes

--- modules/minecraft-mod/datascripts/spells/exp-spell.ts
+++ modules/minecraft-mod/datascripts/spells/exp-spell.ts
@@ -1,7 +1,13 @@
 import { std } from "tswow-stdlib"
 import { MODNAME } from "../modname"
 let expSpell = std.Spells.create(MODNAME,'exp-spell',57353)
-expSpell.Name.enGB.set('xp spell for dad')
-expSpell.Description.enGB.set('description of dad spell')
+expSpell.Name.enGB.set('Increased Experience')
+expSpell.Description.enGB.set('Experience gained is increased by $s1%.')
 expSpell.Effects.get(0).BasePoints.set(0)
-expSpell.Effects.get(1).BasePoints.set(0)
+    expSpell.Effects.get(0).EffectType.setApplyAura()
+    expSpell.Effects.get(0).BasePoints.set(0)
+    expSpell.Effects.get(0).DieSides.set(1)
+    expSpell.Effects.get(0).ImplicitTargetA.setDestCaster()
+    expSpell.Effects.get(0).AuraType.setModXpPct()
+//expSpell.Effects.get(1).BasePoints.set(0)
+
