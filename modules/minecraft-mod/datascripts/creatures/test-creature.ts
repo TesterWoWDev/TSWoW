6c4d2760beb7446f40321504c9a3cedb02ebb52d
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-06 14:59:26 -0400

    add example waypoint

--- modules/minecraft-mod/datascripts/creatures/test-creature.ts
+++ modules/minecraft-mod/datascripts/creatures/test-creature.ts
@@ -1,4 +1,5 @@
 import { std } from "tswow-stdlib"
+import { SQL } from "../../../../bin/scripts/tswow/wotlkdata/sql/SQLFiles"
 import { MODNAME } from "../modname"
 
 let creature1 = std.CreatureTemplates.create(MODNAME,'example-creature1',299)
@@ -6,4 +7,5 @@ creature1.MovementType.setRandomMovement()
 
 let creature2 = std.CreatureTemplates.create(MODNAME,'example-creature1',299)
 creature2.MovementType.setWaypoint()
-
+SQL.waypoints.add(creature2.ID,1).position_x.set(1).position_y.set(1).position_z.set(1)
+SQL.waypoints.add(creature2.ID,2).position_x.set(1).position_y.set(2).position_z.set(1)
