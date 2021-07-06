15685739698ff78d3dbc85d2c9158c1862c305e7
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-06 18:57:57 -0400

    xx

--- modules/minecraft-mod/datascripts/items/RandomProperties/tier1-properties.ts
+++ modules/minecraft-mod/datascripts/items/RandomProperties/tier1-properties.ts
@@ -9,19 +9,28 @@ let PropertyIndex = 2500
 let SpellItemEnchantmentIndex = 6000
 export let clothTierOneItemEnchantmentIndex = 15000
 
-DBC_SpellItemEnchantment.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
-DBC_SpellItemEnchantment.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
-DBC_SpellItemEnchantment.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+int.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+spirit.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+power.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+haste.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+hit.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+crit.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
+mp5.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
 
-DBC_SpellItemEnchantment.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
-DBC_SpellItemEnchantment.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
-DBC_SpellItemEnchantment.add(SpellItemEnchantmentIndex++).Effect.setIndex(0,5).EffectArg.setIndex(0,3).EffectPointsMax.setIndex(0,3).EffectPointsMin.setIndex(0,3).Name.enGB.set('+3 Agility').ItemVisual.set(0).Flags.set(0)
 
-DBC_ItemRandomProperties.add(PropertyIndex).Name2.enGB.set('of the Whale').Enchantment.setIndex(0,SpellItemEnchantmentIndex-4).Enchantment.setIndex(1,SpellItemEnchantmentIndex-3).Enchantment.setIndex(2,SpellItemEnchantmentIndex-2)
+DBC_ItemRandomProperties.add(PropertyIndex).Name2.enGB.set('of the Whale').Enchantment.setIndex(0,SpellItemEnchantmentIndex-7).Enchantment.setIndex(1,SpellItemEnchantmentIndex-6).Enchantment.setIndex(2,SpellItemEnchantmentIndex-2)
 SQL.item_enchantment_template.add(clothTierOneItemEnchantmentIndex,PropertyIndex++).chance.set(10)
-DBC_ItemRandomProperties.add(PropertyIndex).Name2.enGB.set('of the Boar').Enchantment.setIndex(0,SpellItemEnchantmentIndex-4).Enchantment.setIndex(1,SpellItemEnchantmentIndex-3).Enchantment.setIndex(2,SpellItemEnchantmentIndex-2)
+
+
+DBC_ItemRandomProperties.add(PropertyIndex).Name2.enGB.set('of the Boar').Enchantment.setIndex(0,SpellItemEnchantmentIndex-6).Enchantment.setIndex(1,SpellItemEnchantmentIndex-1).Enchantment.setIndex(2,SpellItemEnchantmentIndex-2)
 SQL.item_enchantment_template.add(clothTierOneItemEnchantmentIndex,PropertyIndex).chance.set(10)
 
 /*
-dd
+int
+spirit
+spell pwoer
+haste
+hit
+crit
+mp5
 */
