7f12e831e3aebdfc6fc2962405806cfc4e624f8e
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-06 15:09:07 -0400

    comment out grouping

--- modules/tester-mod/livescripts/rangeLoot.ts
+++ modules/tester-mod/livescripts/rangeLoot.ts
@@ -16,17 +16,17 @@ export function rangeLoot(events: TSEventHandlers) {
                 //make curCorpse unlootable  
             }
         }
-        for(let i=0;i<corpseLoot.GetItemCount();i++){
-            let curItem = corpseLoot.GetItem(i)
-            for(let j=i+1;j<corpseLoot.GetItemCount();j++){
-                let cItem = corpseLoot.GetItem(j)
-                if(cItem.GetCount() > 0){
-                    if(curItem.GetItemID() == cItem.GetItemID()){
-                        curItem.SetCount(curItem.GetCount() + cItem.GetCount())
-                        cItem.SetCount(0)
-                    }
-                }
-            }
-        }
+        // for(let i=0;i<corpseLoot.GetItemCount();i++){
+        //     let curItem = corpseLoot.GetItem(i)
+        //     for(let j=i+1;j<corpseLoot.GetItemCount();j++){
+        //         let cItem = corpseLoot.GetItem(j)
+        //         if(cItem.GetCount() > 0){
+        //             if(curItem.GetItemID() == cItem.GetItemID()){
+        //                 curItem.SetCount(curItem.GetCount() + cItem.GetCount())
+        //                 cItem.SetCount(0)
+        //             }
+        //         }
+        //     }
+        // }
     })
 }
