9984c22e2748bcaac5f1444548c022d2f30bd9d8
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-06 22:34:22 -0400

    example creature updated

--- modules/minecraft-mod/datascripts/creatures/test-creature.ts
+++ modules/minecraft-mod/datascripts/creatures/test-creature.ts
@@ -4,6 +4,13 @@ import { MODNAME } from "../modname"
 
 let creature1 = std.CreatureTemplates.create(MODNAME,'example-creature1',299)
 creature1.MovementType.setRandomMovement()
+creature1.Level.set(1,1)
+creature1.DamageSchool.setNormal()
+creature1.Stats.ArmorMod.set(1)
+creature1.Stats.DamageMod.set(1)
+creature1.Stats.ExperienceMod.set(1)
+creature1.Stats.HealthMod.set(1)
+creature1.Stats.ManaMod.set(1)
 
 let creature2 = std.CreatureTemplates.create(MODNAME,'example-creature1',299)
 creature2.MovementType.setWaypoint()
