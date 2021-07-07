e3652d78af6461e0e12251d00bfa0a42edca9bbb
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-07 15:42:48 -0400

    onCommand transmog

--- modules/tester-mod/livescripts/tester-mod-scripts.ts
+++ modules/tester-mod/livescripts/tester-mod-scripts.ts
@@ -12,4 +12,16 @@ export function Main(events: TSEventHandlers) {
 	onDeath(events)
     manaSystem(events)
     rangeLoot(events)
+    events.Player.OnCommand((player,com,found)=>{
+        if(com.get().startsWith("transmog")){
+            let vis = com.get().substr(8)
+            let visSplit = vis.split(' ')//itemID,enchantID,slotID
+            if(!player.GetItemByPos(255,ToUInt32(visSplit[2])).IsNull()){
+                player.SetUInt16Value((0x0087+0x008E+0x0006)+(ToUInt32(visSplit[2])*2),0,ToUInt32(visSplit[0]))//item visual
+                player.SetUInt16Value((0x0088+0x008E+0x0006)+(ToUInt32(visSplit[2])*2),0,ToUInt32(visSplit[1]))//enchantment visual
+            } 
+            //replace visSplit[2] with itemSlot for mass mod
+            found.set(true)
+        }
+    })
 }
