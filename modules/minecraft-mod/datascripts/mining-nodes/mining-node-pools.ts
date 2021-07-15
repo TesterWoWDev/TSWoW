bfb7bff4bc370ab9f0f0f7c8219e1ad9685e19e2
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-14 22:55:13 -0400

    spawn2 + decending pool values

--- modules/minecraft-mod/datascripts/mining-nodes/mining-node-pools.ts
+++ modules/minecraft-mod/datascripts/mining-nodes/mining-node-pools.ts
@@ -5,7 +5,7 @@ import { node1, node2, node3, node4 } from "./mining-nodes";
 let poolEntry = 100000
 let respawnRate = 300
 
-makePool(poolEntry++, node1, 30, respawnRate, spawn1)
+makePool(poolEntry++, node1, 50, respawnRate, spawn1)
 makePool(poolEntry++, node2, 30, respawnRate, spawn2)
-makePool(poolEntry++, node3, 30, respawnRate, spawn3)
-makePool(poolEntry++, node4, 30, respawnRate, spawn4)
+makePool(poolEntry++, node3, 20, respawnRate, spawn3)
+makePool(poolEntry++, node4, 15, respawnRate, spawn4)
