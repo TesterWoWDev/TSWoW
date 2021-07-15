2f6c6f0e581f1902131adc1433dfeb47e35fdfd9
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-15 15:03:52 -0400

    spawn changes for equipmentIDs

--- modules/minecraft-mod/datascripts/functions/spawning-functions.ts
+++ modules/minecraft-mod/datascripts/functions/spawning-functions.ts
@@ -2,12 +2,16 @@ import { std } from "tswow-stdlib";
 import { Pos } from "tswow-stdlib/Misc/Position";
 import { MODNAME } from "../modname";
 
-export function spawnNPC(id: number, wanderDistance:number, position:number[], index?:number) {
+export function spawnNPC(id: number, wanderDistance:number, equipmentID?:number, position:number[], index?:number) {
     if(index == null){
         index = 0
     }
+    if(equipmentID == null){
+        equipmentID = 0
+    }
     let npc = std.CreatureTemplates.load(id);
     let spawn = npc.spawn(MODNAME,id+"creature-spawn"+index,Pos(723,position[0],position[1],position[2],position[3]))
+    spawn.row.equipment_id.set(equipmentID)
     if(wanderDistance > 0){
         spawn.WanderDistance.set(wanderDistance);
         spawn.MovementType.setRandomMovement()
@@ -15,9 +19,9 @@ export function spawnNPC(id: number, wanderDistance:number, position:number[], i
 
 }
 
-export function spawnMultipleNPCs(id:number,wanderDistance:number, positions:number[][]){
+export function spawnMultipleNPCs(id:number,wanderDistance:number, equipmentID?:number, positions:number[][]){
     for (let i = 0; i < positions.length; i++) {
-        spawnNPC(id,wanderDistance,positions[i],i);
+        spawnNPC(id,wanderDistance,equipmentID,positions[i],i);
     }
 }
 
