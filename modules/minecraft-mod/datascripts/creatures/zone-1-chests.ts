a68255e34f15d653e1640863cfdc47455981c89d
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-17 17:45:50 -0400

    addloot for gameobject chests

--- modules/minecraft-mod/datascripts/creatures/zone-1-chests.ts
+++ modules/minecraft-mod/datascripts/creatures/zone-1-chests.ts
@@ -1,14 +1,38 @@
 import { std } from "tswow-stdlib"
+import { GameObjectChest } from "../../../tswow-stdlib/datascripts/build/GameObject/Types/GameObjectChest"
 import { tierOneClothMaterial, tierOneLeatherMaterial, tierOneMailMaterial } from "../items/armor/tier1-set"
 import { MODNAME } from "../modname"
 
 let Chest1 = std.GameObjectTemplates.create(MODNAME,'test-Chest1-chest',2843).setChest()
-let Chest1Loot = Chest1.Loot
+export let Chest1Loot = Chest1.Loot
 Chest1.Name.enGB.set('Battered Chest')
 Chest1.Display.setID(259)
 Chest1.Loot.addItem(tierOneMailMaterial,10,1,2)//id,chance,min,max
 Chest1.Loot.addItem(tierOneLeatherMaterial,10,1,2)//id,chance,min,max
 Chest1.Loot.addItem(tierOneClothMaterial,10,1,2)//id,chance,min,max
+addLoot(Chest1,[1,1],[1,1])
+addLootSingleChance(Chest1,[1,1],1)
 
 
-//gob.Loot.addItem(1,1,1,1,true)//id,chance,min,max,questRequired
+function addLoot(chest: GameObjectChest, items: number[], chances: number[],groupID?:number) {
+    if(groupID == null){
+        groupID = 0
+    }
+    items.forEach((value,index)=>{
+        if(chances[index] > 0) {
+            chest.Loot.addItem(value,chances[index],1,1,false,groupID,0)
+        }
+    })
+
+}
+
+function addLootSingleChance(chest: GameObjectChest,items:number[],chance:number,groupID?:number) {
+    if(groupID == null){
+        groupID = 0
+    }
+    items.forEach((value,index)=>{
+        if(chance > 0) {
+            chest.Loot.addItem(value,chance,1,1,false,groupID,0)
+        }
+    })
+}
