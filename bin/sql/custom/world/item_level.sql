49cd3365a967946ec701424375743e9052979d64
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-18 11:09:04 -0400

    clear faction requirements

--- bin/sql/custom/world/item_level.sql
+++ bin/sql/custom/world/item_level.sql
@@ -1,3 +1,8 @@
+UPDATE item_template SET RequiredSkill = 0;
+UPDATE item_template SET RequiredSkillRank = 0;
+UPDATE item_template SET RequiredReputationFaction = 0;
+UPDATE item_template SET RequiredReputationRank = 0;
+
 UPDATE item_template SET RequiredLevel = 10 WHERE RequiredLevel > 20 AND RequiredLevel < 60;
 UPDATE item_template SET RequiredLevel = 15 WHERE RequiredLevel >= 60 AND RequiredLevel < 70;
 UPDATE item_template SET RequiredLevel = 20 WHERE RequiredLevel >= 70 AND RequiredLevel < 80;
