009880cb1ead5a706e371868653bbc844c3e6037
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-18 10:41:25 -0400

    setup herb pool spawns

--- modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-node-pools.ts
+++ modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-node-pools.ts
@@ -1,23 +1,12 @@
-// import { retardMaudan, spawn1Tier1, spawn2Tier1, spawn2Tier2, spawn3Tier1, spawn3Tier2, spawn3Tier3, spawn4Tier1, spawn4Tier2, spawn4Tier3, spawn4Tier4 } from "./herb-node-spawns";
-// import { makePool } from "../../functions/resource-node-functions";
-// import { node1, node2, node3, node4 } from "./herb-nodes";
+import { makePool } from "../../functions/resource-node-functions";
+import { herbSpawn1Tier1, herbSpawn2Tier1, herbSpawn3Tier1, herbSpawn4Tier1, herbSpawn5Tier1 } from "./herb-node-spawns";
+import { herbNode1, herbNode2, herbNode3, herbNode4, herbNode5 } from "./herb-nodes";
 
-// let poolEntry = 100100
-// let respawnRate = 300
+let poolEntry = 100100
+let respawnRate = 300
 //tier 1 area
-makePool(poolEntry++, node1, 30, respawnRate, spawn1Tier1)
-
-//tier2 area
-makePool(poolEntry++, node1, 5, respawnRate, spawn2Tier1)
-makePool(poolEntry++, node2, 25, respawnRate, spawn2Tier2)
-
-//tier3 area
-makePool(poolEntry++, node1, 5, respawnRate, spawn3Tier1)
-makePool(poolEntry++, node2, 10, respawnRate, spawn3Tier2)
-makePool(poolEntry++, node3, 20, respawnRate, spawn3Tier3)
-
-//tier4 area
-makePool(poolEntry++, node1, 3, respawnRate, spawn4Tier1)
-makePool(poolEntry++, node2, 5, respawnRate, spawn4Tier2)
-makePool(poolEntry++, node3, 5, respawnRate, spawn4Tier3)
-makePool(poolEntry++, node4, 15, respawnRate, spawn4Tier4)
+makePool(poolEntry++, herbNode1, 30, respawnRate, herbSpawn1Tier1)
+makePool(poolEntry++, herbNode2, 30, respawnRate, herbSpawn2Tier1)
+makePool(poolEntry++, herbNode3, 30, respawnRate, herbSpawn3Tier1)
+makePool(poolEntry++, herbNode4, 30, respawnRate, herbSpawn4Tier1)
+makePool(poolEntry++, herbNode5, 30, respawnRate, herbSpawn5Tier1)
---
+++ modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-node-spawns.ts
@@ -0,0 +1,15 @@
+export let herbSpawn1Tier1 = [
+    []
+]
+export let herbSpawn2Tier1 = [
+    []
+]
+export let herbSpawn3Tier1 = [
+    []
+]
+export let herbSpawn4Tier1 = [
+    []
+]
+export let herbSpawn5Tier1 = [
+    []
+]
