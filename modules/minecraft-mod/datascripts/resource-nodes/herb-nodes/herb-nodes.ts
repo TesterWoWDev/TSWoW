6c3bb42bcb9116d938daa3c00c24cf0701554629
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-18 10:33:57 -0400

    start work on herbalism

--- modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-nodes.ts
+++ modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-nodes.ts
@@ -1,11 +1,8 @@
-// export let node1 = makeResourceNode('Copper Vein 1',310,38,'tier1')
-// //setupherbNode(node1,tierOneBaseResources[3],tierOneBaseResources[4],tierOneBaseResources[0],[undiscoveredReds,undiscoveredGreens,undiscoveredPurples,undiscoveredYellows])
+import { makeResourceNode } from "../../functions/resource-node-functions"
 
-// export let node2 = makeResourceNode('Copper Vein 2',310,18,'tier2')
-// //setupherbNode(node2,tierTwoBaseResources[3],tierTwoBaseResources[4],tierTwoBaseResources[0],[undiscoveredReds,undiscoveredGreens,undiscoveredPurples,undiscoveredYellows])
-
-// export let node3 = makeResourceNode('Copper Vein 3',310,19,'tier3')
-// //setupherbNode(node3,tierThreeBaseResources[3],tierThreeBaseResources[4],tierThreeBaseResources[0],[undiscoveredReds,undiscoveredGreens,undiscoveredPurples,undiscoveredYellows])
-
-// export let node4 = makeResourceNode('Copper Vein 4',310,20,'tier4')
-// //setupherbNode(node4,tierFourBaseResources[3],tierFourBaseResources[4],tierFourBaseResources[0],[undiscoveredReds,undiscoveredGreens,undiscoveredPurples,undiscoveredYellows])
+export let herbNode1 = makeResourceNode('herb Vein 1',310,29,'tier1')
+    herbNode1.Loot.addItem(item,chance,1,3)
+export let herbNode2 = makeResourceNode('herb Vein 2',310,29,'tier1')
+export let herbNode3 = makeResourceNode('herb Vein 3',310,29,'tier1')
+export let herbNode4 = makeResourceNode('herb Vein 4',310,29,'tier1')
+export let herbNode5 = makeResourceNode('herb Vein 5',310,29,'tier1')
--- modules/minecraft-mod/datascripts/resource-nodes/mining-nodes/mining-node-pools.ts
+++ modules/minecraft-mod/datascripts/resource-nodes/mining-nodes/mining-node-pools.ts
@@ -1,26 +1,26 @@
 import { retardMaudan, spawn1Tier1, spawn2Tier1, spawn2Tier2, spawn3Tier1, spawn3Tier2, spawn3Tier3, spawn4Tier1, spawn4Tier2, spawn4Tier3, spawn4Tier4 } from "./mining-node-spawns";
 import { makePool } from "../../functions/resource-node-functions";
-import { node1, node2, node3, node4 } from "./mining-nodes";
+import { miningNode1, miningNode2, miningNode3, miningNode4 } from "./mining-nodes";
 
 let poolEntry = 100000
 let respawnRate = 300
 //tier 1 area
-makePool(poolEntry++, node1, 30, respawnRate, spawn1Tier1)
+makePool(poolEntry++, miningNode1, 30, respawnRate, spawn1Tier1)
 
 //tier2 area
-makePool(poolEntry++, node1, 5, respawnRate, spawn2Tier1)
-makePool(poolEntry++, node2, 25, respawnRate, spawn2Tier2)
+makePool(poolEntry++, miningNode1, 5, respawnRate, spawn2Tier1)
+makePool(poolEntry++, miningNode2, 25, respawnRate, spawn2Tier2)
 
 //tier3 area
-makePool(poolEntry++, node1, 5, respawnRate, spawn3Tier1)
-makePool(poolEntry++, node2, 10, respawnRate, spawn3Tier2)
-makePool(poolEntry++, node3, 20, respawnRate, spawn3Tier3)
+makePool(poolEntry++, miningNode1, 5, respawnRate, spawn3Tier1)
+makePool(poolEntry++, miningNode2, 10, respawnRate, spawn3Tier2)
+makePool(poolEntry++, miningNode3, 20, respawnRate, spawn3Tier3)
 
 //...
-makePool(poolEntry++, node3, 10, respawnRate, retardMaudan)//mauradon spawns
+makePool(poolEntry++, miningNode3, 10, respawnRate, retardMaudan)//mauradon spawns
 
 //tier4 area
-makePool(poolEntry++, node1, 3, respawnRate, spawn4Tier1)
-makePool(poolEntry++, node2, 5, respawnRate, spawn4Tier2)
-makePool(poolEntry++, node3, 5, respawnRate, spawn4Tier3)
-makePool(poolEntry++, node4, 15, respawnRate, spawn4Tier4)
+makePool(poolEntry++, miningNode1, 3, respawnRate, spawn4Tier1)
+makePool(poolEntry++, miningNode2, 5, respawnRate, spawn4Tier2)
+makePool(poolEntry++, miningNode3, 5, respawnRate, spawn4Tier3)
+makePool(poolEntry++, miningNode4, 15, respawnRate, spawn4Tier4)
