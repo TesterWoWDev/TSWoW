a60f371cdf71b48649887620d970a4be4e1082f0
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-18 10:38:55 -0400

    rename mining node spawns + start herb node pool

--- modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-node-pools.ts
+++ modules/minecraft-mod/datascripts/resource-nodes/herb-nodes/herb-node-pools.ts
@@ -2,25 +2,22 @@
 // import { makePool } from "../../functions/resource-node-functions";
 // import { node1, node2, node3, node4 } from "./herb-nodes";
 
-// let poolEntry = 100000
+// let poolEntry = 100100
 // let respawnRate = 300
-// //tier 1 area
-// makePool(poolEntry++, node1, 30, respawnRate, spawn1Tier1)
+//tier 1 area
+makePool(poolEntry++, node1, 30, respawnRate, spawn1Tier1)
 
-// //tier2 area
-// makePool(poolEntry++, node1, 5, respawnRate, spawn2Tier1)
-// makePool(poolEntry++, node2, 25, respawnRate, spawn2Tier2)
+//tier2 area
+makePool(poolEntry++, node1, 5, respawnRate, spawn2Tier1)
+makePool(poolEntry++, node2, 25, respawnRate, spawn2Tier2)
 
-// //tier3 area
-// makePool(poolEntry++, node1, 5, respawnRate, spawn3Tier1)
-// makePool(poolEntry++, node2, 10, respawnRate, spawn3Tier2)
-// makePool(poolEntry++, node3, 20, respawnRate, spawn3Tier3)
+//tier3 area
+makePool(poolEntry++, node1, 5, respawnRate, spawn3Tier1)
+makePool(poolEntry++, node2, 10, respawnRate, spawn3Tier2)
+makePool(poolEntry++, node3, 20, respawnRate, spawn3Tier3)
 
-// //...
-// makePool(poolEntry++, node3, 10, respawnRate, retardMaudan)//mauradon spawns
-
-// //tier4 area
-// makePool(poolEntry++, node1, 3, respawnRate, spawn4Tier1)
-// makePool(poolEntry++, node2, 5, respawnRate, spawn4Tier2)
-// makePool(poolEntry++, node3, 5, respawnRate, spawn4Tier3)
-// makePool(poolEntry++, node4, 15, respawnRate, spawn4Tier4)
+//tier4 area
+makePool(poolEntry++, node1, 3, respawnRate, spawn4Tier1)
+makePool(poolEntry++, node2, 5, respawnRate, spawn4Tier2)
+makePool(poolEntry++, node3, 5, respawnRate, spawn4Tier3)
+makePool(poolEntry++, node4, 15, respawnRate, spawn4Tier4)
--- modules/minecraft-mod/datascripts/resource-nodes/mining-nodes/mining-node-pools.ts
+++ modules/minecraft-mod/datascripts/resource-nodes/mining-nodes/mining-node-pools.ts
@@ -1,26 +1,26 @@
-import { retardMaudan, spawn1Tier1, spawn2Tier1, spawn2Tier2, spawn3Tier1, spawn3Tier2, spawn3Tier3, spawn4Tier1, spawn4Tier2, spawn4Tier3, spawn4Tier4 } from "./mining-node-spawns";
+import { retardMaudan, miningSpawn1Tier1, miningSpawn2Tier1, miningSpawn2Tier2, miningSpawn3Tier1, miningSpawn3Tier2, miningSpawn3Tier3, miningSpawn4Tier1, miningSpawn4Tier2, miningSpawn4Tier3, miningSpawn4Tier4 } from "./mining-node-spawns";
 import { makePool } from "../../functions/resource-node-functions";
 import { miningNode1, miningNode2, miningNode3, miningNode4 } from "./mining-nodes";
 
 let poolEntry = 100000
 let respawnRate = 300
 //tier 1 area
-makePool(poolEntry++, miningNode1, 30, respawnRate, spawn1Tier1)
+makePool(poolEntry++, miningNode1, 30, respawnRate, miningSpawn1Tier1)
 
 //tier2 area
-makePool(poolEntry++, miningNode1, 5, respawnRate, spawn2Tier1)
-makePool(poolEntry++, miningNode2, 25, respawnRate, spawn2Tier2)
+makePool(poolEntry++, miningNode1, 5, respawnRate, miningSpawn2Tier1)
+makePool(poolEntry++, miningNode2, 25, respawnRate, miningSpawn2Tier2)
 
 //tier3 area
-makePool(poolEntry++, miningNode1, 5, respawnRate, spawn3Tier1)
-makePool(poolEntry++, miningNode2, 10, respawnRate, spawn3Tier2)
-makePool(poolEntry++, miningNode3, 20, respawnRate, spawn3Tier3)
+makePool(poolEntry++, miningNode1, 5, respawnRate, miningSpawn3Tier1)
+makePool(poolEntry++, miningNode2, 10, respawnRate, miningSpawn3Tier2)
+makePool(poolEntry++, miningNode3, 20, respawnRate, miningSpawn3Tier3)
 
 //...
 makePool(poolEntry++, miningNode3, 10, respawnRate, retardMaudan)//mauradon spawns
 
 //tier4 area
-makePool(poolEntry++, miningNode1, 3, respawnRate, spawn4Tier1)
-makePool(poolEntry++, miningNode2, 5, respawnRate, spawn4Tier2)
-makePool(poolEntry++, miningNode3, 5, respawnRate, spawn4Tier3)
-makePool(poolEntry++, miningNode4, 15, respawnRate, spawn4Tier4)
+makePool(poolEntry++, miningNode1, 3, respawnRate, miningSpawn4Tier1)
+makePool(poolEntry++, miningNode2, 5, respawnRate, miningSpawn4Tier2)
+makePool(poolEntry++, miningNode3, 5, respawnRate, miningSpawn4Tier3)
+makePool(poolEntry++, miningNode4, 15, respawnRate, miningSpawn4Tier4)
