3084d9c2db451629378c966cab8cc7d02d77c2c2
Flannel <54649204+FlannelDev@users.noreply.github.com>
2021-07-19 02:59:24 -0400

    added Horde/Alliance to chat

--- modules/minecraft-mod/livescripts/world-chat.ts
+++ modules/minecraft-mod/livescripts/world-chat.ts
@@ -1,6 +1,7 @@
 export function worldChat(events:TSEventHandlers){
     events.Player.OnCommand((player,command,found)=>{
         if(command.get().startsWith("wc") || command.get().startsWith("chat") || command.get().startsWith("world")){
+            let team = ["[|TInterface\\icons\\Inv_Misc_Tournaments_banner_Human.png:13|t]","[|TInterface\\icons\\Inv_Misc_Tournaments_banner_Orc.png:13|t]"]
             let colors = ["C79C6E","F58CBA","ABD473","FFF569","FFFFFF","C41F3B","0070DE","69CCF0","9482C9","","FF7D0A"]
             found.set(true)
             let text = command.get().split(" ")
@@ -8,7 +9,7 @@ export function worldChat(events:TSEventHandlers){
             for(let i = 1; i < text.length; i++){
                 message += text[i] + " "
             }
-            SendWorldMessage("[World] [|cff"+colors[player.GetClass()]+ "|Hplayer:" + player.GetName() + "|h" + player.GetName() + "|h|r]: |cffffffff"+message+"|r")
+            SendWorldMessage("[World] [|cff" + team[player.GetFaction()] + colors[player.GetClass()]+ "|Hplayer:" + player.GetName() + "|h" + player.GetName() + "|h|r]: |cffffffff"+message+"|r")
         }
     }) 
 }
