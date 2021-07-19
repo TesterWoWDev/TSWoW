79c503c8ee2f29d772234e28f53089daacdd4932
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-19 13:03:12 -0400

    unlearn the initla find X material

--- modules/minecraft-mod/livescripts/onLogin.ts
+++ modules/minecraft-mod/livescripts/onLogin.ts
@@ -1,14 +1,12 @@
 export function onLoginEvents(events:TSEventHandlers){
     events.Player.OnLogin((player,first)=>{
         if(first){
-            //learn skinning
-            //learn herb
-            //learn mining
-            //player.LearnClassSpells(true,false)
             player.LearnSpell(50305)//skin
             player.LearnSpell(65288)//herb
             player.LearnSpell(50310)//mining
             player.LearnSpell(13262)//disenchant
+            player.RemoveSpell(2383,false,false)//remove find herb
+            player.RemoveSpell(2580,false,false)//remove find mineral
             player.AddItem(GetID("item_template","minecraft-mod","multitool"),1)//multitool
         }
     })
