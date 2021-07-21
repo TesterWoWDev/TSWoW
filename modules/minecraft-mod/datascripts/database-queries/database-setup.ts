ccc27b60ccfa5b9ebbac89b34802a231e3a6b914
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-21 14:06:58 -0400

    puuuush

--- modules/minecraft-mod/datascripts/database-queries/database-setup.ts
+++ modules/minecraft-mod/datascripts/database-queries/database-setup.ts
@@ -3,7 +3,10 @@ import { SQL } from "wotlkdata/sql/SQLFiles"
 SQL.Databases.world_dest.writeEarly('DROP TABLE IF EXISTS `minecraft_recipes`')
 SQL.Databases.world_dest.writeEarly('CREATE TABLE `minecraft_recipes` (`pos1` int(11) DEFAULT NULL,`pos2` int(11) DEFAULT NULL,`pos3` int(11) DEFAULT NULL,`pos4` int(11) DEFAULT NULL,`pos5` int(11) DEFAULT NULL,`pos6` int(11) DEFAULT NULL,`pos7` int(11) DEFAULT NULL,`pos8` int(11) DEFAULT NULL,`pos9` int(11) DEFAULT NULL,`req1` int(11) DEFAULT NULL,`cnt1` int(11) DEFAULT NULL,`req2` int(11) DEFAULT NULL,`cnt2` int(11) DEFAULT NULL,`req3` int(11) DEFAULT NULL,`cnt3` int(11) DEFAULT NULL,`req4` int(11) DEFAULT NULL,`cnt4` int(11) DEFAULT NULL,`req5` int(11) DEFAULT NULL,`cnt5` int(11) DEFAULT NULL,`req6` int(11) DEFAULT NULL,`cnt6` int(11) DEFAULT NULL,`req7` int(11) DEFAULT NULL,`cnt7` int(11) DEFAULT NULL,`req8` int(11) DEFAULT NULL,`cnt8` int(11) DEFAULT NULL,`req9` int(11) DEFAULT NULL,`cnt9` int(11) DEFAULT NULL,`craftID` int(11) DEFAULT NULL, `craftCount` int(11) DEFAULT NULL)')
 SQL.Databases.world_dest.writeEarly('INSERT INTO `disables` VALUES (6, 723, 2, "723", "", "Disable Vmap - Creatures on Map 723");');
-SQL.disables.add(6,723).flags.set(2).params_0.set("723").params_1.set("").comment.set('disable vmap 723')
+
+SQL.Databases.world_dest.writeEarly("INSERT INTO `disables` VALUES (6, 723, 14, '0', '0', 'Disable SW vMaps');");
+SQL.Databases.world_dest.writeEarly("INSERT INTO `disables` VALUES (7, 723, 14, '0', '0', 'Disable SW vMaps');");
+
 SQL.Databases.world_dest.writeEarly('DELETE FROM `item_enchantment_template` WHERE entry >= 8700')
 
 SQL.Databases.world_dest.writeEarly('DELETE FROM `creature_equip_template` WHERE creatureID >= 100000')
