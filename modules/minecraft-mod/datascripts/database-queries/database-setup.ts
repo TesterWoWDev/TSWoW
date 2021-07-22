053dcff6cf705fe02288f26f0e9691d6bc8259c9
Connor Kilbourne <connorkilbourne@gmail.com>
2021-07-22 14:12:26 -0400

    Loot

--- modules/minecraft-mod/datascripts/database-queries/database-setup.ts
+++ modules/minecraft-mod/datascripts/database-queries/database-setup.ts
@@ -14,9 +14,8 @@ SQL.Databases.world_dest.writeEarly('CREATE TABLE `invasions_bosses` (`tier` int
 SQL.Databases.world_dest.writeEarly('DROP TABLE IF EXISTS `invasions_spawners`')
 SQL.Databases.world_dest.writeEarly('CREATE TABLE `invasions_spawners` (`entry` int(11) DEFAULT NULL,`x` float DEFAULT NULL,`y` float DEFAULT NULL,`z` float DEFAULT NULL,`o` float DEFAULT NULL)')
 
-SQL.creature_loot_template.filter({Entry:299}).forEach((value)=>{
-    value.Chance.set(0)
-})
+SQL.Databases.world_source.writeEarly('DELETE FROM `creature_loot_template` WHERE Entry = 299')
+
 SQL.game_tele.add(1450).position_x.set(-8750.45).position_y.set(-74.6418).position_z.set(31.1351).map.set(725).name.set('start')
 //in bin/sql/custom/world/item_level setup is a auto-set required level sql
 //as well as item required rep and skills
