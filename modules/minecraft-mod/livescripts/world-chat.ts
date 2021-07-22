0f23149c604761e435203a3c26e5ca0b8e97567a
Connor Kilbourne <connorkilbourne@gmail.com>
2021-07-22 16:33:07 -0400

    fix worldchat

--- modules/minecraft-mod/livescripts/world-chat.ts
+++ modules/minecraft-mod/livescripts/world-chat.ts
@@ -1,7 +1,9 @@
 export function worldChat(events:TSEventHandlers){
     events.Player.OnCommand((player,command,found)=>{
         if(command.get().startsWith("wc") || command.get().startsWith("chat") || command.get().startsWith("world")){
-            let team = ["[|TInterface/icons/Inv_Misc_Tournaments_banner_Human.png:13|t]","[|TInterface/icons/Inv_Misc_Tournaments_banner_Orc.png:13|t]"]
+            let h = "|TInterface/icons/Inv_Misc_Tournaments_banner_Orc:20|t"
+            let a = "|TInterface/icons/Inv_Misc_Tournaments_banner_Human:20|t"
+            let g = "|TInterface/CHATFRAME/UI-ChatIcon-Blizz:20|t"
             let colors = ["","C79C6E","F58CBA","ABD473","FFF569","FFFFFF","C41F3B","0070DE","69CCF0","9482C9","","FF7D0A"]
             found.set(true)
             let text = command.get().split(" ")
@@ -9,7 +11,18 @@ export function worldChat(events:TSEventHandlers){
             for(let i = 1; i < text.length; i++){
                 message += text[i] + " "
             }
-            SendWorldMessage("[World] [|cff" + team[player.GetFaction()] + colors[player.GetClass()]+ "|Hplayer:" + player.GetName() + "|h" + player.GetName() + "|h|r]: |cffffffff"+message+"|r")
+            let teamIcon = ""
+            let faction = player.GetFaction()
+            if(faction == 0){
+                teamIcon = h
+            }
+            else if(faction == 1){
+                teamIcon = a
+            }
+            else{
+                teamIcon = g
+            }
+            SendWorldMessage("[World] [" + teamIcon + "|cff" + colors[player.GetClass()]+ "|Hplayer:" + player.GetName() + "|h" + player.GetName() + "|h|r]: |cffffffff"+message+"|r")
         }
     }) 
 }
