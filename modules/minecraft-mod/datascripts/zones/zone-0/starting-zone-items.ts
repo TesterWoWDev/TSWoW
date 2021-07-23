9c9edb034594a567496073a36dfe423df9181995
Paxonym <rezamzinn@gmail.com>
2021-07-23 15:39:54 -0400

    Update Agathas Box

--- modules/minecraft-mod/datascripts/zones/zone-0/starting-zone-items.ts
+++ modules/minecraft-mod/datascripts/zones/zone-0/starting-zone-items.ts
@@ -1,6 +1,8 @@
 import { MODNAME } from "quests-mod"
 import { std } from "tswow-stdlib"
 
-export let AgathasLockbox = std.Items.create(MODNAME,'agathaslockbox',159)
+export let AgathasLockbox = std.Items.create(MODNAME,'agathaslockbox',4632)
+AgathasLockbox.Name.enGB.set('Agatha\'s Lockbox')
 AgathasLockbox.Quality.setBlue()
 AgathasLockbox.Bonding.setQuestItem()
+AgathasLockbox.Spells.clearAll()
