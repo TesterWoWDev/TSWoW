f80b972ba14c0772e81b919160c913d1396b30c8
Connor Kilbourne <connorkilbourne@gmail.com>
2021-07-22 21:04:22 -0400

    solve range loot .die

--- modules/tester-mod/livescripts/rangeLoot.ts
+++ modules/tester-mod/livescripts/rangeLoot.ts
@@ -1,11 +1,9 @@
 export function rangeLoot(events: TSEventHandlers) {
     events.Player.OnCreatureKill((player,corpse)=>{
-        if(player.IsGM())
-        return
-        
         let inRange = corpse.GetCreaturesInRange(10,0,0,2)
         let corpseLoot = corpse.GetLoot()
         for(let i=0;i<inRange.length;i++){
+        if(!inRange[i].GetLootRecipient().IsNull())
             if(inRange[i].GetLootRecipient().GetGUIDLow() == player.GetGUIDLow() || inRange[i].GetLootRecipientGroup().GetGUID() == player.GetGroup().GetGUID()){
                 let curCorpse = inRange[i].GetLoot()
                 corpse.GetLoot().SetMoney(corpse.GetLoot().GetMoney() + curCorpse.GetMoney())
@@ -19,17 +17,5 @@ export function rangeLoot(events: TSEventHandlers) {
                 //make curCorpse unlootable  
             }
         }
-        // for(let i=0;i<corpseLoot.GetItemCount();i++){
-        //     let curItem = corpseLoot.GetItem(i)
-        //     for(let j=i+1;j<corpseLoot.GetItemCount();j++){
-        //         let cItem = corpseLoot.GetItem(j)
-        //         if(cItem.GetCount() > 0){
-        //             if(curItem.GetItemID() == cItem.GetItemID()){
-        //                 curItem.SetCount(curItem.GetCount() + cItem.GetCount())
-        //                 cItem.SetCount(0)
-        //             }
-        //         }
-        //     }
-        // }
     })
 }
