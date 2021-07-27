e79705cdb47bfcf2eff2e88c6aecf3529e89a19f
Paxonym <rezamzinn@gmail.com>
2021-07-27 11:44:07 -0400

    Starting Quest 04

--- modules/minecraft-mod/datascripts/zones/zone-0/starting-zone-items.ts
+++ modules/minecraft-mod/datascripts/zones/zone-0/starting-zone-items.ts
@@ -1,11 +1,21 @@
 import { MODNAME } from "quests-mod"
 import { std } from "tswow-stdlib"
+import { SQL } from "wotlkdata/sql/SQLFiles"
+import { tierOneBaseResources, tierOneClothMaterial } from "../../items/armor/tier1-set"
 
 export let AgathasLockbox = std.Items.create(MODNAME,'agathaslockbox',4632)
 AgathasLockbox.Name.enGB.set('Agatha\'s Lockbox')
 AgathasLockbox.Quality.setBlue()
 AgathasLockbox.Bonding.setQuestItem()
 AgathasLockbox.Spells.clearAll()
+
+export let SatchelofResources = std.Items.create(MODNAME,'resourcecache',4632)
+SatchelofResources.Name.enGB.set('Fractal\'s Resource Cache')
+SatchelofResources.Quality.setBlue()
+SatchelofResources.Bonding.setQuestItem()
+SatchelofResources.Spells.clearAll()
+SQL.item_loot_template.add(SatchelofResources.ID,tierOneClothMaterial).Chance.set(100).MinCount.set(3).MaxCount.set(3)
+SQL.item_loot_template.add(SatchelofResources.ID,tierOneBaseResources[0]).Chance.set(100).MinCount.set(1).MaxCount.set(1)
 
 export let startingResource01 = std.Items.create(MODNAME,'startingresource01',765)
 startingResource01.Name.enGB.set('Everlilly')
