5c046858cf758e32039dc2141ecda6584d16e42d
TesterWoWDev <connorkilbourne@gmail.com>
2021-07-28 12:48:16 -0400

    update quest func

--- modules/minecraft-mod/datascripts/functions/quest-functions.ts
+++ modules/minecraft-mod/datascripts/functions/quest-functions.ts
@@ -1,19 +1,13 @@
 import { SQL } from "wotlkdata/sql/SQLFiles"
 
 export function previousQuestID(questID:number,prev:number){
-    SQL.quest_template_addon.filter({ID:questID}).forEach((value)=>{
-        value.PrevQuestID.set(prev)
-    })
+    SQL.quest_template_addon.add(questID).PrevQuestID.set(prev)
 }
 
 export function nextQuestID(questID:number,next:number){
-    SQL.quest_template_addon.filter({ID:questID}).forEach((value)=>{
-        value.NextQuestID.set(next)
-    })
+    SQL.quest_template_addon.add(questID).NextQuestID.set(next)
 }
 
 export function prevAndNextQuestID(questID:number,prev:number,next:number){
-    SQL.quest_template_addon.filter({ID:questID}).forEach((value)=>{
-        value.PrevQuestID.set(prev).NextQuestID.set(next)
-    })
+    SQL.quest_template_addon.add(questID).PrevQuestID.set(prev).NextQuestID.set(next)
 }
