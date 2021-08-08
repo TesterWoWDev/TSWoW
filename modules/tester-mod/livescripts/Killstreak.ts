db68274d185daf69a63d40ecec9cc58eec63f3f7
TesterWoWDev <connorkilbourne@gmail.com>
2021-08-08 11:54:18 -0400

    fix logic in killstreak

--- modules/tester-mod/livescripts/Killstreak.ts
+++ modules/tester-mod/livescripts/Killstreak.ts
@@ -15,10 +15,7 @@ class PlayerKillstreak extends DBTable {
 export function Killstreaks(events: TSEventHandlers) {
 	events.Player.OnPVPKill((killer,killed)=>{
 		let killCount = killer.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killer.GetGUIDLow())).killCount++		
-		let pastKills = killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killed.GetGUIDLow())).killCount = 0
-		SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+(killCount+1))
-		killer.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killer.GetGUIDLow())).save();
-		killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killed.GetGUIDLow())).save();
+		let pastKills = killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killed.GetGUIDLow())).killCount
         if(killCount%5 == 0){
             let val = Math.floor(killCount/5.0)
             if(val <= auraSpells.length){
@@ -28,9 +25,15 @@ export function Killstreaks(events: TSEventHandlers) {
                 killer.AddAura(auraSpells[val],killer)
             }
         }
-
         if(pastKills > 5){
             killer.ModifyHonorPoints(pastKills*50)
+            SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has ENDED |cffff0000"+killed.GetName() + "|r's killstreak of "+(pastKills+1))
+        }else{
+            SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Now on a killstreak of "+(killCount+1))
         }
+
+        killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killed.GetGUIDLow())).killCount = 0
+        killer.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killer.GetGUIDLow())).save();
+		killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killed.GetGUIDLow())).save();
 	});
 }
