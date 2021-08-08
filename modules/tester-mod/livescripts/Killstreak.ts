1f4a1f4361e316b54c23d30608e2cf3390fc883a
TesterWoWDev <connorkilbourne@gmail.com>
2021-08-08 11:49:15 -0400

    extend killstreak system

--- modules/tester-mod/livescripts/Killstreak.ts
+++ modules/tester-mod/livescripts/Killstreak.ts
@@ -1,5 +1,5 @@
 const TABLE_NAME_KILLSTREAK = "playerkillstreak";
-
+const auraSpells:TSArray<uint32> = [1901,1901,1901]
 @CharactersTable
 class PlayerKillstreak extends DBTable {
 	constructor(playerGUID: uint32) {
@@ -15,9 +15,22 @@ class PlayerKillstreak extends DBTable {
 export function Killstreaks(events: TSEventHandlers) {
 	events.Player.OnPVPKill((killer,killed)=>{
 		let killCount = killer.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killer.GetGUIDLow())).killCount++		
-		killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killed.GetGUIDLow())).killCount = 0
-			SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+(killCount+1))
-			killer.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killer.GetGUIDLow())).save();
-			killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killed.GetGUIDLow())).save();
+		let pastKills = killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK,new PlayerKillstreak(killed.GetGUIDLow())).killCount = 0
+		SendWorldMessage("|cffff0000[KillTracker] " + killer.GetName() + "|r Has Murdered |cffff0000"+killed.GetName() + "|r In Cold Blood. Current killstreak of "+(killCount+1))
+		killer.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killer.GetGUIDLow())).save();
+		killed.GetData().GetObject<PlayerKillstreak>(TABLE_NAME_KILLSTREAK, new PlayerKillstreak(killed.GetGUIDLow())).save();
+        if(killCount%5 == 0){
+            let val = Math.floor(killCount/5.0)
+            if(val <= auraSpells.length){
+                if(val > 0){
+                    killer.RemoveAura(auraSpells[val-1])
+                }
+                killer.AddAura(auraSpells[val],killer)
+            }
+        }
+
+        if(pastKills > 5){
+            killer.ModifyHonorPoints(pastKills*50)
+        }
 	});
 }
