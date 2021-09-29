2c2d8c6ff464d0cde452ae7426e5e97dac2db6ce
Paxonym <rezamzinn@gmail.com>
2021-09-29 11:30:20 -0400

    PlayerSpells

--- modules/minecraft-mod/livescripts/item-learn-spell.ts
+++ modules/minecraft-mod/livescripts/item-learn-spell.ts
@@ -1,9 +1,11 @@
 export function itemLearnSpell(events: TSEventHandlers) {
     //add some sort of loop
-    events.ItemID.OnEquipLate(13,(item,player,result)=>{
-        player.LearnSpell(133)
+    events.ItemID.OnEquipLate(2092,(item,player,result)=>{
+        player.LearnSpell(51723)
+        player.SendAreaTriggerMessage("You feel a surge of energy enter your mind. You have learned how to use Fan of Knives.")
     })
-    events.ItemID.OnUnequip(13,(item,player,isSwap,result)=>{
-        player.RemoveSpell(133,false,false)
+    events.ItemID.OnUnequip(2092,(item,player,isSwap,result)=>{
+        player.RemoveSpell(51723,false,false)
+        player.SendAreaTriggerMessage("A draining feeling washes over you and you lose the ability to use Fan of Knives.")
     })
 }
