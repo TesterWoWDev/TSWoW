56faf157e5bbe79ed719a187a0d8f2b1ee323ee5
TesterWoWDev <connorkilbourne@gmail.com>
2021-09-29 14:54:04 -0400

    test

--- modules/minecraft-mod/livescripts/creature-death-test.ts
+++ modules/minecraft-mod/livescripts/creature-death-test.ts
@@ -1,10 +1,17 @@
-export function creatureDeathTest(events: TSEventHandlers) {
-    events.CreatureID.OnDeath(GetID("creature_template","minecraft-mod","worgchampioncreature-creature"),
+let table:TSArray<uint32> = [
+    GetID("creature_template","minecraft-mod","worgchampioncreature-creature"),
     
-    (creature,killer)=>{
-        let players = creature.GetPlayersInRange(50,0,0)
-        for(let i=0;i<players.length;i++){
-            players[i].AddAura(133,players[i])
-        }
-    })  
+]
+
+
+export function creatureDeathTest(events: TSEventHandlers) {
+    for(let i=0;i<table.length;i++){
+        events.CreatureID.OnDeath(table[i], (creature,killer)=>{
+            let players = creature.GetPlayersInRange(50,0,0)
+            for(let i=0;i<players.length;i++){
+                players[i].AddAura(133,players[i])
+            }
+        })  
+    }
+    
 }
