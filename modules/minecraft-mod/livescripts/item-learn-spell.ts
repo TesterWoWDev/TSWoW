7bd03ce796de7c2eb5c0e0ac0a1ced046f8504d0
TesterWoWDev <connorkilbourne@gmail.com>
2021-09-29 12:21:37 -0400

    fix

--- modules/minecraft-mod/livescripts/item-learn-spell.ts
+++ modules/minecraft-mod/livescripts/item-learn-spell.ts
@@ -16,12 +16,12 @@ export function itemLearnSpell(events: TSEventHandlers) {
         events.ItemID.OnEquipEarly(itemIDs[i],(item,player,result)=>{
             let entry = item.GetEntry()
             player.LearnSpell(itemIDToSpellID[entry])
-            player.SendBroadcastMessage("You feel a surge of energy enter your mind. You have learned how to use" + "|Hspell:" + 51723 + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
+            player.SendBroadcastMessage("You feel a surge of energy enter your mind. You have learned how to use |Hspell:" + 51723 + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
         })
         events.ItemID.OnUnequip(itemIDs[i],(item,player,isSwap,result)=>{
             let entry = item.GetEntry()
             player.RemoveSpell(itemIDToSpellID[entry],false,false)
-            player.SendBroadcastMessage("A draining feeling washes over you and you lose the ability to use + |Hspell:" + 51723 + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
+            player.SendBroadcastMessage("A draining feeling washes over you and you lose the ability to use |Hspell:" + 51723 + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
         })
     }
 }  
