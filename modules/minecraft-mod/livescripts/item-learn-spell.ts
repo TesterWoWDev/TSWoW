cf6601bfd45714e7a542b4c4cfee814c949ea536
Paxonym <rezamzinn@gmail.com>
2021-09-29 13:00:48 -0400

    itemlearnspellchanges

--- modules/minecraft-mod/livescripts/item-learn-spell.ts
+++ modules/minecraft-mod/livescripts/item-learn-spell.ts
@@ -3,12 +3,12 @@ let itemIDs:TSArray<uint64> = [
     2092
 ]
 let spellIDs = [
-    13,
-    13
+    39139,
+    51723
 ]
 let spellNames = [
-    "name1",
-    "name2"
+    "Flamestrike",
+    "Fan of Knives"
 ]
 
 
@@ -29,12 +29,12 @@ export function itemLearnSpell(events: TSEventHandlers) {
         events.ItemID.OnEquipEarly(itemIDs[i],(item,player,result)=>{
             let entry = item.GetEntry()
             player.LearnSpell(itemIDToSpellID[entry])
-            player.SendBroadcastMessage("You feel a surge of energy enter your mind. You have learned how to use |Hspell:" + 51723 + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
+            player.SendBroadcastMessage("You feel a surge of energy enter your mind. You have learned how to use |Hspell:" + itemIDToSpellID[entry] + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
         })
         events.ItemID.OnUnequip(itemIDs[i],(item,player,isSwap,result)=>{
             let entry = item.GetEntry()
             player.RemoveSpell(itemIDToSpellID[entry],false,false)
-            player.SendBroadcastMessage("A draining feeling washes over you and you lose the ability to use |Hspell:" + 51723 + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
+            player.SendBroadcastMessage("A draining feeling washes over you and you lose the ability to use |Hspell:" + itemIDToSpellID[entry] + "|h|r|cff71d5ff[" + itemIDToSpellName[entry] + "]|r|h")
         })
     }
 }
