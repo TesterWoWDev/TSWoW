ce3cf1728262a0a52b0cf616e5d2a5aacf96eb33
Paxonym <rezamzinn@gmail.com>
2021-10-03 13:35:51 -0400

    Mall Spawns, Vendor Spawns, Etc

---
+++ modules/minecraft-mod/datascripts/zones/(Zone-5)Mall/vendor-items.ts
@@ -0,0 +1,60 @@
+import { std } from "tswow-stdlib"
+import { MODNAME } from "../../modname"
+import { ClothArmorVendor01 } from "./creature-mall-spawns"
+
+
+//Cloth
+export let ClothItem01 = std.Items.create(MODNAME,'clothitem01',12608)
+ClothItem01.Quality.setGreen()
+ClothItem01.Price.set(27893,148576,1)
+export let ClothItem02 = std.Items.create(MODNAME,'clothitem02',13170)
+ClothItem02.Quality.setGreen()
+ClothItem02.Price.set(31893,229563,1)
+export let ClothItem03 = std.Items.create(MODNAME,'clothitem03',13282)
+ClothItem03.Quality.setGreen()
+ClothItem03.Price.set(45893,315191,1)
+export let ClothItem04 = std.Items.create(MODNAME,'clothitem04',13283)
+ClothItem04.Quality.setGreen()
+ClothItem04.Price.set(66893,274512,1)
+export let ClothItem05 = std.Items.create(MODNAME,'clothitem05',22319)
+ClothItem05.Quality.setGreen()
+ClothItem05.Price.set(52893,179562,1)
+export let ClothItem06 = std.Items.create(MODNAME,'clothitem06',13185)
+ClothItem06.Quality.setGreen()
+ClothItem06.Price.set(14893,492252,1)
+export let ClothItem07 = std.Items.create(MODNAME,'clothitem07',13178)
+ClothItem07.Quality.setGreen()
+ClothItem07.Price.set(19893,216621,1)
+export let ClothItem08 = std.Items.create(MODNAME,'clothitem08',13253)
+ClothItem08.Quality.setGreen()
+ClothItem08.Price.set(28893,175544,1)
+export let ClothItem09 = std.Items.create(MODNAME,'clothitem09',13206)
+ClothItem09.Quality.setGreen()
+ClothItem09.Price.set(24893,539623,1)
+export let ClothItem10 = std.Items.create(MODNAME,'clothitem10',13203)
+ClothItem10.Quality.setGreen()
+ClothItem10.Price.set(29892,274545,1)
+export let ClothItem11 = std.Items.create(MODNAME,'clothitem11',13161)
+ClothItem11.Quality.setGreen()
+ClothItem11.Price.set(11843,1019563,1)
+export let ClothItem12 = std.Items.create(MODNAME,'clothitem12',22268)
+ClothItem12.Quality.setGreen()
+ClothItem12.Price.set(19893,947513,1)
+
+ClothArmorVendor01.addVendorItem(ClothItem01.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem02.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem03.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem04.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem05.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem06.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem07.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem08.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem09.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem10.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem11.ID,0,0,0)
+ClothArmorVendor01.addVendorItem(ClothItem12.ID,0,0,0)
+
+//Plate
+
+//Leather
+//Mail
