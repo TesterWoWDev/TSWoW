a13631e47f74d4fb8eada08fdfb984bbb0437df2
TesterWoWDev <connorkilbourne@gmail.com>
2021-10-03 22:17:58 -0400

    more livescript refactor

--- modules/minecraft-mod/livescripts/minecraft-mod-scripts.ts
+++ modules/minecraft-mod/livescripts/minecraft-mod-scripts.ts
@@ -1,13 +1,13 @@
-import { handleCraftMessages } from "./crafting-messages"
-import { creature1Script } from "./creature-scripts/creature1-test"
+import { handleCraftMessages } from "./craftingSystem"
+import { creature1Script } from "./creature/creature1-test"
 import { onLevelup } from "./QoL/onLevelup"
-import { worldChat } from "./world-chat"
+import { worldChat } from "./QoL/world-chat"
 import { Dueling } from "./QoL/onDuel"
 import { Killstreaks } from "./QoL/Killstreak"
 import { onLoginEvents } from "./QoL/onLogin"
-import { itemLearnSpell } from "./item-learn-spell"
-import { creatureDeathTest } from "./creature-death-test"
-import { reviveFix } from "./revival"
+import { itemLearnSpell } from "./item/item-learn-spell"
+import { creatureDeathTest } from "./creature/creature-death-test"
+import { reviveFix } from "./QoL/revive"
 
 export function Main(events: TSEventHandlers) {
     handleCraftMessages(events)
