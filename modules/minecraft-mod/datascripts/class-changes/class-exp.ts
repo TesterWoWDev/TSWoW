09fafa0808cdff0ed82d7c2b846e93c5069e72c7
Paxonym <rezamzinn@gmail.com>
2021-10-03 22:41:26 -0400

    Adding loot to bandits and other creatures

--- modules/minecraft-mod/datascripts/class-changes/class-exp.ts
+++ modules/minecraft-mod/datascripts/class-changes/class-exp.ts
@@ -2,5 +2,5 @@ import { SQL } from "wotlkdata/sql/SQLFiles"
 
 SQL.player_xp_for_level.filter({}).forEach((value,index,array)=>{
     if(index <= 20)
-    value.Experience.set(value.Experience.get() * 6.25)
+    value.Experience.set(value.Experience.get() * 3.25)
 })
