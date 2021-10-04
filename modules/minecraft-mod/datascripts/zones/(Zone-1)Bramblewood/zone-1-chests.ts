146eb21269afebf0b590fba002df40f742ce8c02
Connor Kilbourne <connorkilbourne@gmail.com>
2021-10-04 02:05:12 -0400

    chests now have a respawn timer that isnt 10 seconds...

--- modules/minecraft-mod/datascripts/zones/(Zone-1)Bramblewood/zone-1-chests.ts
+++ modules/minecraft-mod/datascripts/zones/(Zone-1)Bramblewood/zone-1-chests.ts
@@ -1,14 +1,26 @@
 import { GameObjectChest } from "tswow-stdlib/GameObject/Types/GameObjectChest"
 import { addLootToGobChest, addLootToGobChestSingleChance, addLootToGobChestSingleChanceMultiGroup } from "../../functions/gob-functions"
 import { makeResourceNode } from "../../functions/resource-node-functions"
-import { spawnMultipleGobs } from "../../functions/spawning-functions"
+import { spawnMultiGobTimer, spawnMultipleGobs } from "../../functions/spawning-functions"
 import { tierOneBaseResources, tierOneClothMaterial, tierOneLeatherMaterial, tierOneMailMaterial } from "../../items/armor/tier1-set"
 import { SmallSackofCoins, LargeSackofCoins } from "../../items/currencies"
 import { BlazingRapier, WickedMithrilBlade, SearingGoldenBlade, BuzzSaw, BuzzerBlade, Rockslicer, RhahkZorsHammer, GoldplatedBuckler, EmberstoneStaff, ImpalingHarpoon, ThiefsBlade, SmitesReaver, SmitesMightyHammer, CookiesTenderizer, CookiesStirringRod, IronKnuckles, PrisonShank, KamsWalkingStick, SerpentsKiss, KreshsBack, WornTurtleShellShield, SerpentsShoulders, BoahnsFang, ArmoroftheFang, LeggingsoftheFang, FootpadsoftheFang, BeltoftheFang, GlovesoftheFang, StaffofNobles, BladeofEternalDarkness, BladeoftheWretched, FrightskullShaft, GatorbiteAxe, InfernalTricksterLeggings, PhytoskinSpaulders, RunebladeofBaronRivendare, SkullforgeReaver, BiteofSerrakis, BurstofKnowledge, CircleofFlame, DarkIronPulverizer, DarkwaterTalwar, DeadmansHand, FangoftheCrystalSpider, FlameWrath, ForceofWill, GalgannsFireblaster, GalgannsFirehammer, GoblinDragonGun, Grimclaw, HerodsShoulder, IronweaveBelt, IronweaveBoots, IronweaveBracers, IronweaveCowl, IronweaveGloves, IronweaveMantle, IronweavePants, IronweaveRobe, LordGeneralsSword, MantleofDoan, Naglering, RagingBerserkersHelm, RobeofDoan, SavageGladiatorChain, SavageGladiatorGreaves, SavageGladiatorGrips, SavageGladiatorHelm, SavageGladiatorLeggings, ScarletLeggings, Shadowfang, StrikeoftheHydra, WraithScythe, ArdentCustodian, BowofSearingArrows, DazzlingLongsword, EskhandarsLeftClaw, EskhandarsRightClaw, FieryWarAxe, GutRipper, Ironfoe, Nightblade, StaffofJordan, TheGreenTower, UnderworldBand, AzureSilkBelt, AzureSilkCloak, AzureSilkGloves, AzureSilkHood, AzureSilkPants, AzureSilkVest, BarbaricIronBoots, BarbaricIronBreastplate, BarbaricIronGloves, BarbaricIronHelm, BarbaricIronShoulders, CrimsonSilkBelt, CrimsonSilkCloak, CrimsonSilkGloves, CrimsonSilkPantaloons, CrimsonSilkRobe, CrimsonSilkShoulders, CrimsonSilkVest, EmbossedLeatherBoots, EmbossedLeatherCloak, EmbossedLeatherGloves, EmbossedLeatherPants, EmbossedLeatherVest, FineLeatherBelt, FineLeatherBoots, FineLeatherCloak, FineLeatherGloves, FineLeatherTunic, SilveredBronzeBoots, SilveredBronzeBreastplate, SilveredBronzeGauntlets, SilveredBronzeShoulders, DiscoloredHealingPotion, LesserHealingPotion, LesserManaPotion, MinorHealingPotion, MinorManaPotion, MinorRejuvenationPotion, RagePotion, SwiftnessPotion, SwimSpeedPotion, ScrollofAgilityVIII, ScrollofIntellectVIII, ScrollofProtectionVIII, ScrollofSpiritVIII, ScrollofStaminaVIII, ScrollofStrengthVIII } from "./zone-1-items"
 
 
 export let Zone1Chest1 = makeResourceNode('Small Chest',259,57,'zone1chest1-chest')
-spawnMultipleGobs(Zone1Chest1.ID,[[-8334.177,110.985,18.796,3.147],[-8028.486,101.582,1.089,4.817],[-7896.042,138.932,-26.418,1.171],[-8246.397,280.637,3.401,2.344],[-7963.068,232.766,-27.964,0.902],[-7927.933,119.273,-50.434,6.027],[-7935.968,179.275,-45.210,5.896],[-7938.499,244.016,-35.589,2.280],[-8052.747,40.852,1.877,5.17],[-7969.805,-19.939,0.453,2.319],[-8227.125,270.125,3.976,1.564],[-8048.143,465.894,-4.900,4.711]])
+spawnMultiGobTimer(Zone1Chest1.ID,[
+    [-8334.177,110.985,18.796,3.147],
+    [-8028.486,101.582,1.089,4.817],
+    [-7896.042,138.932,-26.418,1.171],
+    [-8246.397,280.637,3.401,2.344],
+    [-7963.068,232.766,-27.964,0.902],
+    [-7927.933,119.273,-50.434,6.027],
+    [-7935.968,179.275,-45.210,5.896],
+    [-7938.499,244.016,-35.589,2.280],
+    [-8052.747,40.852,1.877,5.17],
+    [-7969.805,-19.939,0.453,2.319],
+    [-8227.125,270.125,3.976,1.564],
+    [-8048.143,465.894,-4.900,4.711]],600)
 Zone1Chest1.Size.set(0.75)
 addLootToGobChest(Zone1Chest1,tierOneBaseResources,[4,3,1,2,2,5,3])
 /*Base Resources - Group 5*/  
