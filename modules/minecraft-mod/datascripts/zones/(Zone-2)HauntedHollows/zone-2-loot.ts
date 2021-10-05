46778c7b024b003d8531b0f9d58b1ead6779b9cd
Paxonym <rezamzinn@gmail.com>
2021-10-05 10:00:51 -0400

    Add Zone 2 Loot

--- modules/minecraft-mod/datascripts/zones/(Zone-2)HauntedHollows/zone-2-loot.ts
+++ modules/minecraft-mod/datascripts/zones/(Zone-2)HauntedHollows/zone-2-loot.ts
@@ -1,5 +1,6 @@
-import { addLootToCreature } from "../../functions/npc-functions"
+import { addLootToCreature, addLootToCreatureSingleChance } from "../../functions/npc-functions"
 import { tierTwoBaseResources, tierTwoClothMaterial, tierTwoLeatherMaterial, tierTwoMailMaterial } from "../../items/armor/tier2-set"
+import { undiscoveredBlues, undiscoveredGreens, undiscoveredOranges, undiscoveredPurples, undiscoveredReds, undiscoveredYellows } from "../../items/gems/tier1-gem"
 import { stringItem } from "../../items/string"
 import { Zone2Creature1Loot } from "./zone-2-creatures"
 import { BloodiedPouch, SilkyCape, Carapace, GluttonCleaver, DeathmageSash, MordeshEye, FleshhideShoulders, AwarenessHelm, CleverHat, FerventHelm, GuileRing, PotencyRing, SatyrBow, ShadowMistGloves } from "./zone-2-items"
@@ -20,4 +21,19 @@ addLootToCreature(Zone2Creature1Loot,[
     ShadowMistGloves.ID,            CleverHat.ID,                   PotencyRing.ID,
     GuileRing.ID
 ],[1,1,1,1,1,1,1],1)
+addLootToCreatureSingleChance(Zone2Creature1Loot,[
+    undiscoveredReds[0],            undiscoveredReds[1],            undiscoveredReds[2],
+    undiscoveredReds[3],            undiscoveredReds[4],            undiscoveredReds[5],
+    undiscoveredReds[6],            undiscoveredReds[7],            undiscoveredGreens[0],
+    undiscoveredGreens[1],          undiscoveredGreens[2],          undiscoveredGreens[3],
+    undiscoveredGreens[4],          undiscoveredGreens[5],          undiscoveredGreens[6],
+    undiscoveredPurples[0],         undiscoveredPurples[1],         undiscoveredPurples[2],
+    undiscoveredPurples[3],         undiscoveredPurples[4],         undiscoveredPurples[5],
+    undiscoveredPurples[6],         undiscoveredYellows[0],         undiscoveredYellows[1],
+    undiscoveredYellows[2],         undiscoveredYellows[3],         undiscoveredYellows[4],
+    undiscoveredBlues[0],           undiscoveredBlues[1],           undiscoveredBlues[2],
+    undiscoveredBlues[3],           undiscoveredOranges[0],         undiscoveredOranges[1],
+    undiscoveredOranges[2],         undiscoveredOranges[3],         undiscoveredOranges[4],
+    undiscoveredOranges[5],
+],0.1,2)
 
