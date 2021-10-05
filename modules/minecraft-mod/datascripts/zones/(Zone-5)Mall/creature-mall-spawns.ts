6bdb05288f10f276d53c6c448a806320f717919c
Paxonym <rezamzinn@gmail.com>
2021-10-05 12:07:49 -0400

    remove random arena and honor point requirement for buying items...?

--- modules/minecraft-mod/datascripts/zones/(Zone-5)Mall/creature-mall-spawns.ts
+++ modules/minecraft-mod/datascripts/zones/(Zone-5)Mall/creature-mall-spawns.ts
@@ -133,7 +133,7 @@ OrbOfPowerVendor.Models.addIds(21891)
 OrbOfPowerVendor.FactionTemplate.set(35)
 spawnMultipleNPCWithTimer(OrbOfPowerVendor.ID,0,0,[
     [-8302.053711,-267.736298,17.274397,6.243299]],10)
-DBC_ItemExtendedCost.add(5000).ItemID.set([OrbofPower.ID]).ItemCount.set([10])
+DBC_ItemExtendedCost.add(5000).ItemID.set([OrbofPower.ID]).ItemCount.set([10]).HonorPoints.set(0).ArenaPoints.set(0)
 OrbOfPowerVendor.addVendorItem(34170,0,0,5000)
 
 export let OrganicMatterVendor = std.CreatureTemplates.create(MODNAME,'organicmob-creature',3562)
@@ -143,7 +143,7 @@ OrganicMatterVendor.Models.addIds(21891)
 OrganicMatterVendor.FactionTemplate.set(35)
 spawnMultipleNPCWithTimer(OrganicMatterVendor.ID,0,0,[
     [-8166.573,-2.096447,0.082,2.345]],10)
-DBC_ItemExtendedCost.add(5001).ItemID.set([OrganicMatter.ID]).ItemCount.set([1])
+DBC_ItemExtendedCost.add(5001).ItemID.set([OrganicMatter.ID]).ItemCount.set([1]).HonorPoints.set(0).ArenaPoints.set(0)
 OrganicMatterVendor.addVendorItem(tierOneClothMaterial,0,0,5001)
 OrganicMatterVendor.addVendorItem(tierOneMailMaterial,0,0,5001)
 OrganicMatterVendor.addVendorItem(tierOneLeatherMaterial,0,0,5001)
