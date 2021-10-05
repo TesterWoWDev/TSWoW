48c223b00f9bf278799eeaf42c94b1626c58f264
Paxonym <rezamzinn@gmail.com>
2021-10-05 11:03:45 -0400

    update spawn timers of zone2 creatures

--- modules/minecraft-mod/datascripts/zones/(Zone-2)HauntedHollows/zone-2-creatures.ts
+++ modules/minecraft-mod/datascripts/zones/(Zone-2)HauntedHollows/zone-2-creatures.ts
@@ -108,7 +108,7 @@ spawnMultipleNPCWithTimer(Zone2Creature1.ID,5,0,[
     [-8956.194336,-494.921509,11.799611,3.375770],
     [-8966.097656,-485.147491,11.982493,1.125604],
     [-8956.512695,-475.510529,7.917710,2.107352],
-    [-8710.021484,-341.958008,14.851360,0.838924],],300)
+    [-8710.021484,-341.958008,14.851360,0.838924],],120)
 
 export let SmallSpider = std.CreatureTemplates.create(MODNAME,'smallspider-creature',721)
 SmallSpider.Name.enGB.set('Skitterer')
