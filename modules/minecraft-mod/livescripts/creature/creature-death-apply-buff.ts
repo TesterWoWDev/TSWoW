95ef74ef9786ae7f5807fd7f15c69db820b2dd79
Connor Kilbourne <connorkilbourne@gmail.com>
2021-10-06 10:59:55 -0400

    changes

--- modules/minecraft-mod/livescripts/creature/creature-death-apply-buff.ts
+++ modules/minecraft-mod/livescripts/creature/creature-death-apply-buff.ts
@@ -4,7 +4,7 @@ let table:TSArray<uint32> = [
     GetID("creature_template","minecraft-mod","murlocchampioncreature-creature"),
 ]
 
-export function creatureDeathTest(events: TSEventHandlers) {
+export function creatureDeathApplyBuff(events: TSEventHandlers) {
     for(let i=0;i<table.length;i++){
         events.CreatureID.OnDeath(table[i], (creature,killer)=>{
             let players = creature.GetPlayersInRange(50,0,0)
--- modules/minecraft-mod/livescripts/creature/creature-death-summon.ts
+++ modules/minecraft-mod/livescripts/creature/creature-death-summon.ts
@@ -2,8 +2,8 @@ let tableOfSpawns:TSArray<uint32> = [
     1,
 ]
 
-export function creatureDeathTest(events: TSEventHandlers) {
-    events.CreatureID.OnDeath(creatureToDie, (creature,killer)=>{
+export function creatureDeathSummon(events: TSEventHandlers) {
+    events.CreatureID.OnDeath(1, (creature,killer)=>{
         creature.SpawnCreature(tableOfSpawns[randNum(tableOfSpawns.length)],creature.GetX(),creature.GetY(),creature.GetZ(),0,2,120)
     })  
 }
