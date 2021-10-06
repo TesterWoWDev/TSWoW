208d946988e49654832b13d9821d537be383bb97
Connor Kilbourne <connorkilbourne@gmail.com>
2021-10-06 11:09:28 -0400

    update

--- modules/minecraft-mod/livescripts/creature/creature-death-summon.ts
+++ modules/minecraft-mod/livescripts/creature/creature-death-summon.ts
@@ -4,7 +4,7 @@ let tableOfSpawns:TSArray<uint32> = [
 
 export function creatureDeathSummon(events: TSEventHandlers) {
     events.CreatureID.OnDeath(GetID("creature_template","minecraft-mod","webbed-zone2"), (creature,killer)=>{
-        creature.SpawnCreature(tableOfSpawns[randNum(tableOfSpawns.length)],creature.GetX(),creature.GetY(),creature.GetZ(),0,2,120)
+        creature.SpawnCreature(tableOfSpawns[randNum(tableOfSpawns.length)],creature.GetX(),creature.GetY(),creature.GetZ(),0,2,60000)
     })  
 }
 
