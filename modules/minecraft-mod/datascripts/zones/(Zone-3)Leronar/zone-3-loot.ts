cdf9c4af682b9bcff56f1299d4e3e441f280af30
Paxonym <rezamzinn@gmail.com>
2021-10-06 17:03:57 -0400

    Add remaining loot to creatures and bosses for champion scrolls

--- modules/minecraft-mod/datascripts/zones/(Zone-3)Leronar/zone-3-loot.ts
+++ modules/minecraft-mod/datascripts/zones/(Zone-3)Leronar/zone-3-loot.ts
@@ -6,7 +6,10 @@ import { addLootToCreature, addLootToCreatureSingleChance } from "../../function
 import { tierTwoClothMaterial, tierTwoLeatherMaterial, tierTwoMailMaterial, tierTwoBaseResources } from "../../items/armor/tier2-set"
 import { undiscoveredReds, undiscoveredGreens, undiscoveredPurples, undiscoveredYellows, undiscoveredBlues, undiscoveredOranges } from "../../items/gems/tier1-gem"
 import { stringItem } from "../../items/string"
-import { Zone3Creature2Loot, Zone3Creature3Loot, Zone3RareCreature1Loot } from "./zone-3-creatures"
+import { harpyScroll } from "../champion-scrolls/harpy-champion-scroll"
+import { quailboarScroll } from "../champion-scrolls/quailboar-champion-scroll"
+import { WorgScroll } from "../champion-scrolls/ravaging-worg-champion-scroll"
+import { SavageWolfLoot, Zone3Creature2Loot, Zone3Creature3Loot, Zone3RareCreature1Loot } from "./zone-3-creatures"
 import { Commendation, Contempt, DragonscaleBlade, FocusingCrystal, Gliderboots, Glidergreaves, Glidersabatons, Gliderwrap, HeartPit, LostTreads, LurkerBelt, LurkerCord, LurkerGirdle, LurkerGrasp, MiseryMace, Ravagerband, Ravagerbracer, RavagerCuff, RavagerWrap, ShivanFeather, SunwellVial, VolatilePower } from "./zone-3-items"
 
 
@@ -16,8 +19,8 @@ addLootToCreature(Zone3Creature1Loot,tierTwoBaseResources,[6,6,6,6,6,6,6])
 addLootToCreature(Zone3Creature1Loot,[
     BloodiedPouch.ID,               SilkyCape.ID,                   Carapace.ID,
     GluttonCleaver.ID,              DeathmageSash.ID,               MordeshEye.ID,
-    FleshhideShoulders.ID,          stringItem.ID
-],[2,2,2,2,2,2,2,1],0)
+    FleshhideShoulders.ID,          stringItem.ID,                  quailboarScroll.ID
+],[2,2,2,2,2,2,2,1,1],0)
 /*Weapons and Misc Drops - Group 1*/
 addLootToCreature(Zone3Creature1Loot,[
     FerventHelm.ID,                 AwarenessHelm.ID,               SatyrBow.ID,
@@ -47,8 +50,8 @@ addLootToCreature(Zone3Creature3Loot,tierTwoBaseResources,[6,6,6,6,6,6,6])
 addLootToCreature(Zone3Creature3Loot,[
     BloodiedPouch.ID,               SilkyCape.ID,                   Carapace.ID,
     GluttonCleaver.ID,              DeathmageSash.ID,               MordeshEye.ID,
-    FleshhideShoulders.ID,          stringItem.ID
-],[2,2,2,2,2,2,2,1],0)
+    FleshhideShoulders.ID,          stringItem.ID,                  harpyScroll.ID
+],[2,2,2,2,2,2,2,1,1],0)
 /*Weapons and Misc Drops - Group 1*/
 addLootToCreature(Zone3Creature3Loot,[
     FerventHelm.ID,                 AwarenessHelm.ID,               SatyrBow.ID,
@@ -77,8 +80,8 @@ addLootToCreature(Zone3Creature2Loot,tierTwoBaseResources,[6,6,6,6,6,6,6])
 addLootToCreature(Zone3Creature2Loot,[
     BloodiedPouch.ID,               SilkyCape.ID,                   Carapace.ID,
     GluttonCleaver.ID,              DeathmageSash.ID,               MordeshEye.ID,
-    FleshhideShoulders.ID,          stringItem.ID
-],[2,2,2,2,2,2,2,1],0)
+    FleshhideShoulders.ID,          stringItem.ID,                  quailboarScroll.ID
+],[2,2,2,2,2,2,2,1,1],0)
 /*Weapons and Misc Drops - Group 1*/
 addLootToCreature(Zone3Creature2Loot,[
     FerventHelm.ID,                 AwarenessHelm.ID,               SatyrBow.ID,
@@ -109,8 +112,9 @@ addLootToCreature(Zone3RareCreature1Loot,tierTwoBaseResources,[6,6,6,6,6,6,6])
 addLootToCreature(Zone3RareCreature1Loot,[
     BloodiedPouch.ID,               SilkyCape.ID,                   Carapace.ID,
     GluttonCleaver.ID,              DeathmageSash.ID,               MordeshEye.ID,
-    FleshhideShoulders.ID,          stringItem.ID,                  ShivanFeather.ID
-],[2,2,2,2,2,2,2,1,20],0)
+    FleshhideShoulders.ID,          stringItem.ID,                  ShivanFeather.ID,
+    harpyScroll.ID
+],[2,2,2,2,2,2,2,1,5],0)
 /*Weapons and Misc Drops - Group 1*/
 addLootToCreature(Zone3RareCreature1Loot,[
     FerventHelm.ID,                 AwarenessHelm.ID,               SatyrBow.ID,
@@ -128,3 +132,33 @@ addLootToCreatureSingleChance(Zone3RareCreature1Loot,[
 addLootToCreatureSingleChance(Zone3RareCreature1Loot,[
     OrbofPower.ID
 ],5,3)
+
+addLootToCreature(SavageWolfLoot,[tierTwoClothMaterial,tierTwoLeatherMaterial,tierTwoMailMaterial],[10,10,10])
+addLootToCreature(SavageWolfLoot,tierTwoBaseResources,[6,6,6,6,6,6,6])
+/*Bags and Armor*/
+addLootToCreature(SavageWolfLoot,[
+    BloodiedPouch.ID,               SilkyCape.ID,                   Carapace.ID,
+    GluttonCleaver.ID,              DeathmageSash.ID,               MordeshEye.ID,
+    FleshhideShoulders.ID,          stringItem.ID,                  WorgScroll.ID
+],[2,2,2,2,2,2,2,1,1],0)
+/*Weapons and Misc Drops - Group 1*/
+addLootToCreature(SavageWolfLoot,[
+    FerventHelm.ID,                 AwarenessHelm.ID,               SatyrBow.ID,
+    ShadowMistGloves.ID,            CleverHat.ID,                   PotencyRing.ID,
+    GuileRing.ID,                   ExpScroll.ID
+],[2,2,2,2,2,2,2,2],1)
+addLootToCreatureSingleChance(SavageWolfLoot,[
+    undiscoveredReds[0],            undiscoveredReds[1],            undiscoveredReds[2],
+    undiscoveredReds[3],            undiscoveredReds[4],            undiscoveredReds[5],
+    undiscoveredReds[6],            undiscoveredReds[7],            undiscoveredGreens[0],
+    undiscoveredGreens[1],          undiscoveredGreens[2],          undiscoveredGreens[3],
+    undiscoveredGreens[4],          undiscoveredGreens[5],          undiscoveredGreens[6],
+    undiscoveredPurples[0],         undiscoveredPurples[1],         undiscoveredPurples[2],
+    undiscoveredPurples[3],         undiscoveredPurples[4],         undiscoveredPurples[5],
+    undiscoveredPurples[6],         undiscoveredYellows[0],         undiscoveredYellows[1],
+    undiscoveredYellows[2],         undiscoveredYellows[3],         undiscoveredYellows[4],
+    undiscoveredBlues[0],           undiscoveredBlues[1],           undiscoveredBlues[2],
+    undiscoveredBlues[3],           undiscoveredOranges[0],         undiscoveredOranges[1],
+    undiscoveredOranges[2],         undiscoveredOranges[3],         undiscoveredOranges[4],
+    undiscoveredOranges[5],
+],2,2)
