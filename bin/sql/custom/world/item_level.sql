7b12c6ab0a7d2b64678c2e537b66e80f0f459090
TesterWoWDev <connorkilbourne@gmail.com>
2021-10-07 19:20:50 -0400

    update sql for ; endlines

--- bin/sql/custom/world/item_level.sql
+++ bin/sql/custom/world/item_level.sql
@@ -9,7 +9,7 @@ UPDATE item_template SET RequiredLevel = 20 WHERE RequiredLevel >= 70 AND Requir
 UPDATE item_template SET RequiredLevel = 30 WHERE RequiredLevel >= 80;
 
 
-UPDATE `item_template` SET `armor` = (armor * 0.25) AND `itemlevel` = (requiredlevel * 8.759) AND `socketcolor_1` = (0) AND `socketcolor_2` = (0) AND `socketcolor_3` = (0) AND `socketbonus` = (0) WHERE `entry` > 0
-UPDATE `item_template` SET `ammo_type` = (2) AND `spellid_5` = (46699) AND `spelltrigger_5` = (1) WHERE `entry` > 0 AND `class` = (2) AND `subclass` = (2)
-UPDATE `item_template` SET `ammo_type` = (2) AND `spellid_5` = (46699) AND `spelltrigger_5` = (1)  WHERE `entry` > 0 AND `class` = (2) AND `subclass` = (3)
-UPDATE `item_template` SET `ammo_type` = (2) AND `spellid_5` = (46699) AND `spelltrigger_5` = (1)  WHERE `entry` > 0 AND `class` = (2) AND `subclass` = (18)
+UPDATE `item_template` SET `armor` = (armor * 0.25) AND `itemlevel` = (requiredlevel * 8.759) AND `socketcolor_1` = (0) AND `socketcolor_2` = (0) AND `socketcolor_3` = (0) AND `socketbonus` = (0) WHERE `entry` > 0;
+UPDATE `item_template` SET `ammo_type` = (2) AND `spellid_5` = (46699) AND `spelltrigger_5` = (1) WHERE `entry` > 0 AND `class` = (2) AND `subclass` = (2);
+UPDATE `item_template` SET `ammo_type` = (2) AND `spellid_5` = (46699) AND `spelltrigger_5` = (1)  WHERE `entry` > 0 AND `class` = (2) AND `subclass` = (3);
+UPDATE `item_template` SET `ammo_type` = (2) AND `spellid_5` = (46699) AND `spelltrigger_5` = (1)  WHERE `entry` > 0 AND `class` = (2) AND `subclass` = (18);
