896207f7f7fa27bc14cc47955c9da00fc48d2fdb
Paxonym <rezamzinn@gmail.com>
2021-10-07 11:14:32 -0400

    Update Armor Values Query

--- modules/minecraft-mod/datascripts/items/string.ts
+++ modules/minecraft-mod/datascripts/items/string.ts
@@ -1,7 +1,12 @@
 import { std } from "tswow-stdlib"
+import { SQL } from "wotlkdata"
 import { MODNAME } from "../modname"
 
 export let stringItem = std.Items.create(MODNAME,'string', 2934)
     stringItem.Name.enGB.set('String')
     stringItem.DisplayInfo.setID(58198)
     stringItem.Description.enGB.set('A unique piece of string.')
+
+
+
+SQL.Databases.world_dest.write('UPDATE `item_template` SET `armor` = (armor * 0.1) WHERE `entry` > 65000)')
