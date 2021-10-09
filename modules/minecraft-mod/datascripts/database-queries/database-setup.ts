effc400fcc864253b8214e48b1f4f5fa97d32f42
TesterWoWDev <connorkilbourne@gmail.com>
2021-10-09 13:58:08 -0400

    more database error corrections

--- modules/minecraft-mod/datascripts/database-queries/database-setup.ts
+++ modules/minecraft-mod/datascripts/database-queries/database-setup.ts
@@ -1,3 +1,4 @@
+import { DBC } from "wotlkdata/dbc/DBCFiles"
 import { SQL } from "wotlkdata/sql/SQLFiles"
 
 SQL.Databases.world_dest.writeEarly('TRUNCATE TABLE `minecraft_recipes`')
@@ -14,3 +15,4 @@ SQL.Databases.world_source.writeEarly('DELETE FROM `creature_loot_template` WHER
 SQL.game_tele.add(1450).position_x.set(-8750.45).position_y.set(-74.6418).position_z.set(31.1351).map.set(725).name.set('start')
 
 //2 custom sql files in bin/sql/custom/world
+DBC.MapDifficulty.add(1000).MapID.set(725).Difficulty.set(0).Message.enGB.set(' ').RaidDuration.set(0).MaxPlayers.set(0).Difficultystring.set(' ')
