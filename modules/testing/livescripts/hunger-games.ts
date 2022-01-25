9f239245e078a353ca1fe87f1b88850aa38c176a
TesterWoWDev <connorkilbourne@gmail.com>
2022-01-25 13:20:28 -0500

    pvp kill part of hunger games

--- modules/testing/livescripts/hunger-games.ts
+++ modules/testing/livescripts/hunger-games.ts
@@ -5,22 +5,30 @@ export function hungerGames(events: TSEventHandlers) {
     //new recipes for stuff
     //random spawned rares
     //closing zone of death
+    let winToken = 11
+    let killToken = 12
 
     events.MapID.OnPlayerEnter(1,(map,player)=>{
         //difficulty 1
         player.AddTimer('hungergames-1',3000,0,timer=>{
             console.log("hungergames-1 for " + player.GetName())
-            for(let i=0;i<getInt(5);i++){
+            for(let i=0;i<getInt(3);i++){
                 let spawn = player.SpawnCreature(1,player.GetX() + getInt(50),player.GetY() + getInt(50),player.GetZ(),0,4,300)
                 //spawn.Attack(player,true);
                 spawn.AttackStart(player)
             }
         })
+
         //difficulty 2
         player.AddTimer('hungergames-2-starting-call',10000,0,timer=>{
             console.log("hungergames-2-starting-call for " + player.GetName())
             player.AddTimer('hungergames-2',5000,0,timer=>{
                 console.log("hungergames-2 for " + player.GetName())
+                for(let i=0;i<getInt(2);i++){
+                    let spawn = player.SpawnCreature(2,player.GetX() + getInt(50),player.GetY() + getInt(50),player.GetZ(),0,4,300)
+                    //spawn.Attack(player,true);
+                    spawn.AttackStart(player)
+                }
             })
         })
 
@@ -29,7 +37,27 @@ export function hungerGames(events: TSEventHandlers) {
             console.log("hungergames-3-starting-call for " + player.GetName())
             player.AddTimer('hungergames-3',5000,0,timer=>{
                 console.log("hungergames-3 for " + player.GetName())
+                for(let i=0;i<getInt(1);i++){
+                    let spawn = player.SpawnCreature(3,player.GetX() + getInt(50),player.GetY() + getInt(50),player.GetZ(),0,4,300)
+                    //spawn.Attack(player,true);
+                    spawn.AttackStart(player)
+                }
             })
+        })
+
+        events.Player.OnPVPKill((killer,killed)=>{
+            teleportHome(killed)
+            killer.AddItem(killToken,1)
+            let pcount = killer.GetMap().GetPlayerCount();
+            if(pcount == 1){
+                SendWorldMessage('Hunger Games Ended! '+killer.GetName() + " wins!")
+                killer.AddItem(winToken,3)
+                killed.AddItem(winToken,2)
+                teleportHome(killer)
+            }
+            if(pcount == 2){
+                killed.AddItem(winToken,1)
+            }
         })
     })
 
@@ -37,4 +65,8 @@ export function hungerGames(events: TSEventHandlers) {
         return (Math.random() * (-max*2)) + max;
       }
 
+}
+
+function teleportHome(player: TSPlayer) {
+    player.Teleport(0,1,1,1,1)
 }
