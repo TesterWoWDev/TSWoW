19fc4ea8d2d9df147ce203cca315149fcfe2b4e3
TesterWoWDev <connorkilbourne@gmail.com>
2022-01-24 22:14:25 -0500

    added hunger-games livescript

--- modules/testing/livescripts/testing-scripts.ts
+++ modules/testing/livescripts/testing-scripts.ts
@@ -1,7 +1,9 @@
+import { hungerGames } from "./hunger-games";
 import { housing } from "./player_housing";
 import { torghastBuffSystem } from "./torghast";
 
 export function Main(events: TSEventHandlers) {
     housing(events)
     torghastBuffSystem(events)
+    hungerGames(events)
 }
