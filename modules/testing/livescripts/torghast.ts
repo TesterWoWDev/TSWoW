3d09d79cfec2a93d4725157df86bbe8cc2a03542
TesterWoWDev <connorkilbourne@gmail.com>
2022-01-26 16:59:14 -0500

    added buffcount

--- modules/testing/livescripts/torghast.ts
+++ modules/testing/livescripts/torghast.ts
@@ -1,12 +1,12 @@
-import { spellChoice, spellChoiceID, spellChoices, spellValuesMessage } from "../shared/Messages";
+import { spellChoice, spellChoiceID, spellChoices, spellValuesMessage } from "../shared/Messages"
 
-let spells: TSArray<int> = [7464, 7471, 7477, 7468, 7474];
-let buffChoiceCount = 3;
+let spells: TSArray<int> = [7464, 7471, 7477, 7468, 7474]
+let buffChoiceCount = 3
 
 class torghastBuffs extends TSClass {
-    currentBuffs: TSArray<int> = [];
-    currentBuffsCount: TSArray<int> = [];
-    currentChoiceBuffs: TSArray<int> = [];
+    currentBuffs: TSArray<int> = []
+    currentBuffsCount: TSArray<int> = []
+    currentChoiceBuffs: TSArray<int> = []
 }
 
 export function torghastBuffSystem(events: TSEventHandlers) {
@@ -20,13 +20,13 @@ export function torghastBuffSystem(events: TSEventHandlers) {
         removeBuffs(player)
     })
 
-    events.Player.OnLogout((player)=>{
+    events.Player.OnLogout((player) => {
         removeBuffs(player)
     })
 
     events.PacketID.OnCustom(spellChoiceID, (opcode, packet, player) => {
         let pkt = new spellChoice(0)
-        pkt.read(packet);
+        pkt.read(packet)
         chooseBuff(player, pkt.choice)
         applyBuffs(player)
         showBuffs(player)
@@ -34,43 +34,55 @@ export function torghastBuffSystem(events: TSEventHandlers) {
 }
 
 function buffChoice(player: TSPlayer) {
-    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs());
+    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs())
     for (let i = 1; i < buffChoiceCount; i++) {
         let chNum = Math.floor(Math.random() * spells.length)
         let choice = spells[chNum]
         charItems.currentChoiceBuffs.push(choice)
     }
-    let pkt = new spellChoices([0,0,0])
+    let pkt = new spellChoices([0, 0, 0])
     pkt.spellIDs = charItems.currentChoiceBuffs
-    pkt.write().SendToPlayer(player);
+    pkt.write().SendToPlayer(player)
 }
 
 function chooseBuff(player: TSPlayer, index: uint32) {
-    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs());
+    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs())
     if (charItems.currentChoiceBuffs.length >= 3) {
         let currentChoicesID = [charItems.currentChoiceBuffs.pop(), charItems.currentChoiceBuffs.pop(), charItems.currentChoiceBuffs.pop()]
         let choice: number = currentChoicesID[index]!
-        charItems.currentBuffs.push(choice)
+        let found = -1
+        for (let i = 0; i < charItems.currentBuffs.length; i++) {
+            if (charItems.currentBuffs[i] == choice) {
+                found = i
+                break
+            }
+        }
+        if (found == -1) {
+            charItems.currentBuffsCount[found]++
+        } else {
+            charItems.currentBuffs.push(choice)
+            charItems.currentBuffsCount.push(1)
+        }
+
     }
 }
 
 function showBuffs(player: TSPlayer) {
-    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs());
-    for (let i = 0; i < charItems.currentBuffs.length; i++) {
-        let pkt = new spellValuesMessage(charItems.currentBuffs.length, charItems.currentBuffs, charItems.currentBuffsCount)
-        pkt.write().SendToPlayer(player)
-    }
+    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs())
+    let pkt = new spellValuesMessage(charItems.currentBuffs.length, charItems.currentBuffs, charItems.currentBuffsCount)
+    pkt.write().SendToPlayer(player)
+
 }
 
 function applyBuffs(player: TSPlayer) {
-    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs());
+    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs())
     for (let i = 0; i < charItems.currentBuffs.length; i++) {
         player.AddAura(charItems.currentBuffs[i], player).SetStackAmount(charItems.currentBuffsCount[i])
     }
 }
 
 function removeBuffs(player: TSPlayer) {
-    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs());
+    let charItems = player.GetObject<torghastBuffs>("torghastBuffs", new torghastBuffs())
     for (let i = 0; i < charItems.currentBuffs.length; i++) {
         player.RemoveAura(charItems.currentBuffs[i])
     }
