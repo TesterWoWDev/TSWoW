c64d395fce7589581ec3bbc94853aebd332ef864
TesterWoWDev <connorkilbourne@gmail.com>
2022-01-27 20:28:03 -0500

    remove timers on death and reach home

--- modules/minecraft-mod/datascripts/testing/boss-scripts.ts
+++ modules/minecraft-mod/datascripts/testing/boss-scripts.ts
@@ -19,10 +19,24 @@ TorghastBoss1.InlineScripts.OnJustEnteredCombat((creature, target) => {
         let self = entity.ToCreature()
         let target = self.GetVictim()
         self.CastSpell(target, spellID3, false)//AoE Pull
-        creature.AddTimer('event3.1', 10000, 0, (timer, entity, del, can) => {
+        creature.AddTimer('event3.1', 2000, 0, (timer, entity, del, can) => {
             let self = entity.ToCreature()
             let target = self.GetVictim()
             self.CastSpell(target, spellID4, false)//AoE spell
         })
     })
+})
+
+TorghastBoss1.InlineScripts.OnDeath((creature,killer)=>{
+    creature.RemoveTimer('event1')
+    creature.RemoveTimer('event2')
+    creature.RemoveTimer('event3')
+    creature.RemoveTimer('event3.1')
+})
+
+TorghastBoss1.InlineScripts.OnReachedHome((creature)=>{
+    creature.RemoveTimer('event1')
+    creature.RemoveTimer('event2')
+    creature.RemoveTimer('event3')
+    creature.RemoveTimer('event3.1')
 })
