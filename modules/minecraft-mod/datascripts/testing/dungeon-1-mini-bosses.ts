75f1366c1822ffb65f172c4409019a4ac383630c
Paxonym <rezamzinn@gmail.com>
2022-02-08 16:48:22 -0500

    Torghast Mini Boss Update (1)

--- modules/minecraft-mod/datascripts/testing/dungeon-1-mini-bosses.ts
+++ modules/minecraft-mod/datascripts/testing/dungeon-1-mini-bosses.ts
@@ -1,48 +1,43 @@
 import { std } from "tswow-stdlib";
 import { MODNAME } from "../modname";
+import { mobOne } from "./dungeon-1-mobs";
 
 
-export let miniBoss1FlameShock = std.Spells.create(
-    MODNAME,
-    "miniboss1flameshock1-spell",
-    8050
-);
-miniBoss1FlameShock.Name.enGB.set("Crackling Flames");
-miniBoss1FlameShock.AuraDescription.enGB.set("$s2 Fire damage every $t2 seconds.");
-miniBoss1FlameShock.Description.enGB.set(
-    "Instantly sears the target with fire, causing $s1 Fire damage immediately and $o2 Fire damage over $d."
-);
-miniBoss1FlameShock.Effects.get(0).BasePoints.set(174);
-miniBoss1FlameShock.Effects.get(0).DieSides.set(1);
-miniBoss1FlameShock.Effects.get(0).PointsPerLevel.set(0);
-miniBoss1FlameShock.Effects.get(1).BasePoints.set(144);
-miniBoss1FlameShock.Effects.get(1).DieSides.set(1);
-miniBoss1FlameShock.Effects.get(1).ChainAmplitude.set(3000);
-miniBoss1FlameShock.Duration.setSimple(7000);
-miniBoss1FlameShock.Cooldown.set(7000, 0, 0, 0);
+export let MiniBossBurdenofKnowledge = std.Spells.create(MODNAME,"minibossburdenofknowledge-spell",23187);
+MiniBossBurdenofKnowledge.Name.enGB.set("Burden of Knowledge");
+MiniBossBurdenofKnowledge.AuraDescription.enGB.set("Increases the time between attacks by $s2%.");
+MiniBossBurdenofKnowledge.Description.enGB.set("Deals $s1 Frost damage to all enemies, drains $s3 mana and increases the time between attacks by $s2% for $d.");
+MiniBossBurdenofKnowledge.CastTime.setSimple(3000)
+MiniBossBurdenofKnowledge.Duration.setSimple(10000)
+MiniBossBurdenofKnowledge.Effects.get(0).BasePoints.set(649);
+MiniBossBurdenofKnowledge.Effects.get(0).DieSides.set(140);
+MiniBossBurdenofKnowledge.Effects.get(0).Radius.set(35);
+MiniBossBurdenofKnowledge.Effects.get(1).BasePoints.set(-401);
+MiniBossBurdenofKnowledge.Effects.get(1).DieSides.set(1);
+MiniBossBurdenofKnowledge.Effects.get(1).Radius.set(35)
 
-export let miniBoss1LightningBolt1 = std.Spells.create(
-    MODNAME,
-    "miniboss1lightningbolt1-spell",
-    8246
-);
-miniBoss1LightningBolt1.Name.enGB.set("Lightning Strike");
-miniBoss1LightningBolt1.Description.enGB.set(
-    "Blasts an enemy with lightning for $s1 Nature damage."
-);
-miniBoss1LightningBolt1.Effects.get(0).BasePoints.set(384);
-miniBoss1LightningBolt1.Effects.get(0).DieSides.set(1);
-miniBoss1LightningBolt1.Effects.get(0).PointsPerLevel.set(0);
-miniBoss1LightningBolt1.CastTime.setSimple(2000);
+export let MiniBossTouchofDeathDamage = std.Spells.load(57548);
+MiniBossTouchofDeathDamage.Name.enGB.set("Death Touch");
+MiniBossTouchofDeathDamage.Effects.get(0).BasePoints.set(199)
+MiniBossTouchofDeathDamage.Effects.get(0).DieSides.set(1)
+export let MiniBossTouchofDeath = std.Spells.create(MODNAME,"minibosstouchofdeath-spell",57547);
+MiniBossTouchofDeath.Name.enGB.set("Touch of Death");
+MiniBossTouchofDeath.Description.enGB.set("Inflicts $57548s1 Shadow damage every $t1 sec for $d.");
+MiniBossTouchofDeath.Stacks.set(100)
+MiniBossTouchofDeath.CastTime.setSimple(2000);
+MiniBossTouchofDeath.Duration.setSimple(7000)
 
-export let TorghastMiniBoss1 = std.CreatureTemplates.create(
-    MODNAME,
-    "torghastminiboss1",
-    3276
-);
-TorghastMiniBoss1.Models.clearAll();
-TorghastMiniBoss1.Models.addIds(19521);
-TorghastMiniBoss1.Name.enGB.set("Agnar Steelwinter");
+export let MiniBossSummonSpell = std.Spells.create(MODNAME,"minibosssummonspell-spell",66705);
+MiniBossSummonSpell.Name.enGB.set("Call Ally");
+MiniBossSummonSpell.Description.enGB.set("Summons miniature mobs to assist the caster.");
+MiniBossSummonSpell.Effects.get(0).BasePoints.set(2)
+MiniBossSummonSpell.Effects.get(0).DieSides.set(1)
+MiniBossSummonSpell.Effects.get(0).MiscValueA.set(mobOne.ID)
+MiniBossSummonSpell.Duration.setSimple(35000);
+MiniBossSummonSpell.CastTime.setSimple(2000);
+
+export let TorghastMiniBoss1 = std.CreatureTemplates.create(MODNAME,"torghastminiboss1",8912);
+TorghastMiniBoss1.Name.enGB.set("Volgathok the Acrhivist");
 TorghastMiniBoss1.Scale.set(1);
 TorghastMiniBoss1.FactionTemplate.set(48);
 TorghastMiniBoss1.Level.set(21, 21);
@@ -50,18 +45,13 @@ TorghastMiniBoss1.Rank.BOSS.set();
 TorghastMiniBoss1.UnitClass.PALADIN.set();
 TorghastMiniBoss1.DamageSchool.Normal.set();
 TorghastMiniBoss1.Stats.ArmorMod.set(30);
-TorghastMiniBoss1.Stats.DamageMod.set(20);
+TorghastMiniBoss1.Stats.DamageMod.set(4);
 TorghastMiniBoss1.Stats.HealthMod.set(20);
 TorghastMiniBoss1.Stats.ManaMod.set(50);
 TorghastMiniBoss1.Stats.ExperienceMod.set(5);
 
 TorghastMiniBoss1.InlineScripts.OnJustEnteredCombat((creature, target) => {
-    function attemptCast(
-        spellID: number,
-        self: TSCreature,
-        target: TSUnit,
-        force: boolean
-    ) {
+    function attemptCast(spellID: number,self: TSCreature,target: TSUnit,force: boolean) {
         if (!self.IsCasting() || force) {
             if (target.IsPlayer()) {
                 self.CastSpell(target, spellID, false);
@@ -71,28 +61,26 @@ TorghastMiniBoss1.InlineScripts.OnJustEnteredCombat((creature, target) => {
         }
     }
     //start of combat
-    creature.CastSpell(
-        target,
-        GetID("Spell", "minecraft-mod", "miniboss1flameshock1-spell"),
-        true
-    );
+    creature.CastSpell(target,GetID("Spell", "minecraft-mod", "minibossburdenofknowledge-spell"),true);
     //start of timers
     creature.AddTimer("event1", 3000, -1, (timer, entity, del, can) => {
         let self = entity.ToCreature();
         let target = self.GetVictim();
-        attemptCast(
-            GetID("Spell", "minecraft-mod", "miniboss1lightningbolt1-spell"),
-            self,
-            target,
-            false
-        );
+        attemptCast(GetID("Spell", "minecraft-mod", "minibosstouchofdeath-spell"),self,target,false);
+    });
+    creature.AddTimer("event2", 13000, -1, (timer, entity, del, can) => {
+        let self = entity.ToCreature();
+        let target = self.GetVictim();
+        attemptCast(GetID("Spell", "minecraft-mod", "minibosssummonspell-spell"),self,target,false);
     });
 });
 
 TorghastMiniBoss1.InlineScripts.OnDeath((creature, killer) => {
     creature.RemoveTimer("event1");
+    creature.RemoveTimer("event2");
 });
 
 TorghastMiniBoss1.InlineScripts.OnReachedHome((creature) => {
     creature.RemoveTimer("event1");
+    creature.RemoveTimer("event2");
 });
