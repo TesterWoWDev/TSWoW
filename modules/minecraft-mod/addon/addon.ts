7c4b4c43bd139353149d73b945ca4fd61a740a4a
TesterWoWDev <connorkilbourne@gmail.com>
2022-02-24 08:11:04 -0500

    add cache request to build

--- modules/minecraft-mod/addon/addon.ts
+++ modules/minecraft-mod/addon/addon.ts
@@ -1,5 +1,7 @@
 import { craftingMenu } from "./crafting-menu";
+import { itemCacheRequest } from "./item-cache-request";
 import { thorgast } from "./torghast";
 
 craftingMenu();
 thorgast();
+itemCacheRequest()
--- modules/minecraft-mod/addon/item-cache-request.ts
+++ modules/minecraft-mod/addon/item-cache-request.ts
@@ -4,7 +4,7 @@ export function itemCacheRequest()
 {
     GameTooltip.HookScript("OnTooltipSetItem", (self) => {
         const itemID = self.GetItem()[1].split(':')[1]
-        let p = new itemCache(Number(itemID),0)//eventually add a check if i want a template call?=
+        let p = new itemCache(Number(itemID),0)//eventually add a check if i want a template call?
         p.write().Send()
     });
 }
