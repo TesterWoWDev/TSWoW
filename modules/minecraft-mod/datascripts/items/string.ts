7fe6f43f250b1332e8afd7fc5c36fad0b167391e
TesterWoWDev <connorkilbourne@gmail.com>
2022-02-23 21:39:11 -0500

    force over ghost

--- modules/minecraft-mod/datascripts/items/string.ts
+++ modules/minecraft-mod/datascripts/items/string.ts
@@ -4,4 +4,4 @@ import { MODNAME } from "../modname";
 export let stringItem = std.Items.create(MODNAME, "string", 2934);
 stringItem.Name.enGB.set("String");
 stringItem.DisplayInfo.set(58198);
-stringItem.Description.enGB.set("A unique piece of string.");
+stringItem.Description.enGB.set("A unique piece of string.")
