e08fa6fcfe3d36df1248203473cd445ce1eb40c8
tester <connorkilbourne@gmail.com>
2022-02-27 21:16:55 -0500

    var to let

--- modules/minecraft-mod/datascripts/statTemplates.ts
+++ modules/minecraft-mod/datascripts/statTemplates.ts
@@ -2,22 +2,22 @@ import { std } from "wow/wotlk";
 import { ItemTemplate } from "wow/wotlk/std/Item/ItemTemplate";
 import { MODNAME } from "./modname";
 
-var manaTemplate = [0, 5];
-var healthTemplate = [1, 3];
-var agilityTemplate = [3, 1];
-var strengthTemplate = [4, 1];
-var intellectTemplate = [5, 1];
-var spiritTemplate = [6, 2];
-var staminaTemplate = [7, 2];
-var critTemplate = [32, 1];
-var hasteTemplate = [36, 1];
-var attackPowerTemplate = [38, 1];
-var spellPowerTemplate = [45, 1];
+let manaTemplate = [0, 5];
+let healthTemplate = [1, 3];
+let agilityTemplate = [3, 1];
+let strengthTemplate = [4, 1];
+let intellectTemplate = [5, 1];
+let spiritTemplate = [6, 2];
+let staminaTemplate = [7, 2];
+let critTemplate = [32, 1];
+let hasteTemplate = [36, 1];
+let attackPowerTemplate = [38, 1];
+let spellPowerTemplate = [45, 1];
 
-var stamIntTemplate = joinTemplates([staminaTemplate, intellectTemplate], 1);
-var stamAgiTemplate = joinTemplates([staminaTemplate, agilityTemplate], 1);
+let stamIntTemplate = joinTemplates([staminaTemplate, intellectTemplate], 1);
+let stamAgiTemplate = joinTemplates([staminaTemplate, agilityTemplate], 1);
 
-var item = std.Items.load(25);
+let item = std.Items.load(25);
 useTemplates(item, 1, [stamIntTemplate, stamAgiTemplate]);
 
 function joinTemplates(statTemplates: number[][], modifier: number) {
@@ -32,9 +32,9 @@ function useTemplates(
     modifier: number,
     templateArray: number[][][]
 ): number[] {
-    var IDs: number[] = [];
+    let IDs: number[] = [];
     templateArray.forEach((v, i, arr) => {
-        var curItem = std.Items.create(MODNAME, item.Name.enGB.get() + i, item.ID);
+        let curItem = std.Items.create(MODNAME, item.Name.enGB.get() + i, item.ID);
         curItem.Stats.clearAll();
         v.forEach((value) => curItem.Stats.add(value[0], value[1] * modifier));
         IDs.push(curItem.ID);
