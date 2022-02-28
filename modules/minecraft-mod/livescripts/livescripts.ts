1faf9837baa411d252a195f4bc03b91694ef3b87
tester <connorkilbourne@gmail.com>
2022-02-27 21:24:09 -0500

    remove more

--- modules/minecraft-mod/livescripts/livescripts.ts
+++ modules/minecraft-mod/livescripts/livescripts.ts
@@ -1,25 +1,25 @@
 import { handleCraftMessages } from "./craftingSystem";
-import { onLevelup } from "./QoL/onLevelup";
-import { worldChat } from "./QoL/world-chat";
+import { dungeon1 } from "./dungeon/dungeon-1";
+import { torghastBuffSystem } from "./dungeon/torghast-master";
+import { itemCacheSend } from "./item-cache";
+import { itemCreate } from "./item_create";
+import { itemReloading } from "./item_reloading";
 import { Killstreaks } from "./QoL/Killstreak";
+import { onLevelup } from "./QoL/onLevelup";
 import { onLoginEvents } from "./QoL/onLogin";
 import { reviveFix } from "./QoL/revive";
-import { torghastBuffSystem } from "./dungeon/torghast-master";
-import { dungeon1 } from "./dungeon/dungeon-1";
-import { itemReloading } from "./item_reloading";
-import { itemCreate } from "./item_create";
-import { itemCacheSend } from "./item-cache";
+import { worldChat } from "./QoL/world-chat";
 
 export function Main(events: TSEvents) {
-    handleCraftMessages(events);
-    onLoginEvents(events);
-    worldChat(events);
-    onLevelup(events);
-    Killstreaks(events);
-    reviveFix(events);
-    torghastBuffSystem(events);
-    dungeon1(events);
-    itemReloading(events);
-    itemCreate(events);
-    itemCacheSend(events);
+    handleCraftMessages(events)
+    dungeon1(events)
+    torghastBuffSystem(events)
+    itemCacheSend(events)
+    itemCreate(events)
+    itemReloading(events)
+    Killstreaks(events)
+    onLevelup(events)
+    onLoginEvents(events)
+    reviveFix(events)
+    worldChat(events)
 }
