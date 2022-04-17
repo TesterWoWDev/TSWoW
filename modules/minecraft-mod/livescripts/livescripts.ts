c8257ff4fdd15114a49020b4dcb4b1caedba54a2
tester <connorkilbourne@gmail.com>
2022-04-17 14:22:59 -0400

    remove comment block

--- modules/minecraft-mod/livescripts/livescripts.ts
+++ modules/minecraft-mod/livescripts/livescripts.ts
@@ -22,35 +22,4 @@ export function Main(events: TSEvents) {
     reviveFix(events)
     worldChat(events)
     easter(events)
-
-
-
-    // GetIDTag('minecraft-mod','tier1-normal-mob').forEach((val,i,arr) => {//iterate all tier1 mobs
-    //     events.CreatureID.OnGenerateLoot(val,(creature,killer)=>{
-    //         let cLoot = creature.GetLoot()
-    //         cLoot.AddItem(GetID('item_template','minecraft-mod','shazzian'),1,3)//shazz ID
-    //         cLoot.AddItem(GetID('item_template','minecraft-mod','other-drop'),1,1)//other ID
-    //         cLoot.AddItem(GetID('item_template','minecraft-mod','third-drop'),1,1)//other3 ID
-            
-    //         if(getRandomInt(100) == 1){//1 in 100 odds to drop a single tier1 green item
-    //             cLoot.AddItem(GetIDTag('minecraft-mod','tier1-green-item')[getRandomInt(GetIDTag('minecraft-mod','tier1-green-item').length)],1,1)
-    //         } 
-    //         if(getRandomInt(1000) == 1){//1 in 1000 odds to drop a single tier1 blue item
-    //             cLoot.AddItem(GetIDTag('minecraft-mod','tier1-blue-item')[getRandomInt(GetIDTag('minecraft-mod','tier1-blue-item').length)],1,1)
-    //         }
-    //         if(getRandomInt(10000) == 1){//1 in 10000 odds to drop a single tier1 purple item
-    //             cLoot.AddItem(GetIDTag('minecraft-mod','tier1-purple-item')[getRandomInt(GetIDTag('minecraft-mod','tier1-purple-item').length)],1,1)
-    //         }
-    //     })
-    // })
-
-    // events.ItemID.OnUse(GetID('item_template','minecraft-mod','item-clearer'),(item,player,reserved,cancel)=>{
-    //     if(item.GetTemplate().GetBonding() == 0 || item.GetTemplate().GetBonding() == 2)
-    //     {
-    //         for(let i=0;i<7;i++)
-    //             item.ClearEnchantment(i)
-    //         item.SetBinding(false)
-    //     }
-    // })
-
 }
