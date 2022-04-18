fbda70a52197fd7160f65b2cc3c5221cf57e1653
tester <connorkilbourne@gmail.com>
2022-04-18 01:49:02 -0400

    fix respawning

--- modules/minecraft-mod/livescripts/QoL/revive.ts
+++ modules/minecraft-mod/livescripts/QoL/revive.ts
@@ -1,10 +1,6 @@
 export function reviveFix(events: TSEvents) {
-    events.MapID.OnPlayerEnter(1, (map, player) => {
-        player.Teleport(725, -8749.424805, -77.516861, 31.135099, 0.923082);
-        player.ResurrectPlayer(15, false);
-    });
-    events.MapID.OnPlayerEnter(0, (map, player) => {
-        player.Teleport(725, -8749.424805, -77.516861, 31.135099, 0.923082);
-        player.ResurrectPlayer(15, false);
+    events.MapID.OnPlayerEnter(725, (map, player) => {
+        if(player.IsDead())
+        player.ResurrectPlayer(75, false);
     });
 }
